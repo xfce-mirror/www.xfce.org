de84dd57fc970e989f3d9092edb22c462c08057f
Nick Schermer <nick@xfce.org>
2006-01-17 18:41:22 +0000

    add index, remove test
    (Old svn revision: 39)

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	include_once ("includes/functions.php");
+	
+	include ("includes/header.php");
+	
+	/*
+	$url = $_SERVER["REQUEST_URI"];
+	$url = trim($url, '/');
+	*/
+	
+	include ("includes/footer.php");
+?>
--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$url = $_SERVER["REQUEST_URI"];
-	
-	// Trim leading and lagging slashes
-	$url = trim($url, '/');
-	
-	echo $url;
-?>
