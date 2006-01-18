f42e8dca36de83df509e0c319abff639d94f14a5
Nick Schermer <nick@xfce.org>
2006-01-18 19:34:20 +0000

    Recurive stripping explanation
    (Old svn revision: 63)

--- index.php
+++ index.php
@@ -31,6 +31,17 @@ else
 	}
 	else
 	{
+		/* Recursive stripping:
+			- explode uri
+			- start building the uri again while trying if the file exists
+			  - translation page and english one.
+			- if no file is found anymore: use last working uri.
+			
+			so projects/xfmedia/downloads/foo/foo/foo/foo/foo will be striped
+			to projects/xfmedia/downloads.
+			
+			Also names like projects/xfmedia/downloads.html will be projects/xfmedia
+		*/
 		print ("<h1>404, page not found</h1>");
 	}
 }
