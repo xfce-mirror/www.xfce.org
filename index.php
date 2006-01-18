83d6ead956628f0015c5100c015ee20abe233766
Nick Schermer <nick@xfce.org>
2006-01-18 18:27:07 +0000

    Remove benchamrks
    (Old svn revision: 58)

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-	/* Benchmark */
-	$startTime = array_sum(explode(" ",microtime()));
-	
 	include_once ("include/functions.php");
 	include_once ("include/header.php");
 	include_once ("include/footer.php");
-	echo ''. round((array_sum(explode(" ",microtime())) - $startTime),4).' seconds';
+
 	$lang = "en";
 	$uri = CleanupURI($_SERVER["REDIRECT_URL"]);
 
@@ -16,11 +13,8 @@
 	if ($file) {
 		include ($file);
 	} else {
-		#will be replaced by a 404 page or the index
 		include ("include/frontpage.php");
 	}
 	
 	PrintFooter ($lang);
-	
-	echo ''. round((array_sum(explode(" ",microtime())) - $startTime),4).' seconds';
 ?>
