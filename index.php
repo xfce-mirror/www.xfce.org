ab344ebd92c7faa499bb24b00d2cfc682df19e66
Nick Schermer <nick@xfce.org>
2006-01-19 15:21:31 +0000

    * Make content and navigation separate * Fix javascript * Make layout cookies work when style change in a directory
    (Old svn revision: 79)

--- index.php
+++ index.php
@@ -2,8 +2,21 @@
 include_once ("include/header.php");
 include_once ("include/footer.php");
 include_once ("include/frontpage.php");
+include_once ("include/content.php");
 
 $lang = "en";
+
+/* Recursive stripping idea:
+	- explode uri
+	- start building the uri again while trying if the file exists
+	  - translation page and english one.
+	- if no file is found anymore: use last working uri.
+	
+	so projects/xfmedia/downloads/foo/foo/foo/foo/foo will be striped
+	to projects/xfmedia/downloads.
+	
+	Also names like projects/xfmedia/downloads.html will be projects/xfmedia
+*/
 
 $uri = $_SERVER["REDIRECT_URL"];
 	$uri = trim($uri, '/');
@@ -13,36 +26,15 @@ PrintHeader ($uri, $lang);
 	
 if ($uri == "")
 {
-	PrintFrontpage ();
+	PrintFrontpage ($lang);
+}
+elseif ($content = GetContent ($uri, $lang))
+{
+	PrintContent ($content);
 }
 else
 {
-	$file = "i18n/" . $uri . "." . $lang . ".php";
-	$file_en = "i18n/" . $uri . ".en.php";
-	
-	if (is_file ($file))
-	{
-		include ($file);
-	}
-	else if (is_file ($file_en))
-	{
-		include ($file_en);
-	}
-	else
-	{
-		/* Recursive stripping:
-			- explode uri
-			- start building the uri again while trying if the file exists
-			  - translation page and english one.
-			- if no file is found anymore: use last working uri.
-			
-			so projects/xfmedia/downloads/foo/foo/foo/foo/foo will be striped
-			to projects/xfmedia/downloads.
-			
-			Also names like projects/xfmedia/downloads.html will be projects/xfmedia
-		*/
-		print ("<h1>404, page not found</h1>");
-	}
+	print ("<h1>404, page not found</h1>");
 }
 
 PrintFooter ($lang);
