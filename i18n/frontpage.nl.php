064b38996302b787079be2414700f3378a430021
Nick Schermer <nick@xfce.org>
2006-01-19 17:28:52 +0000

    Dutch frontapge, i love Babel Fish :P
    (Old svn revision: 88)

---
+++ i18n/frontpage.nl.php
@@ -0,0 +1,104 @@
+<div id="front-intro">
+	<div id="front-right">
+		<div id="front-download">
+			<div class="link">
+				<h2><a href="#" title="Downlaod Xfce 4.4">Download Xfce 4.4</a></h2>
+			</div>
+			<div class="notes">
+				<p><a href="#" title="Read the release notes">Release opmerkingen</a><span class="grey"> | </span><a href="#" title="What\'s new in Xfce 4.4">Rondleiding</a></p>
+			</div>
+		</div>
+		<div id="front-lang">
+			<h2 class="hidden">Site Language</h2>
+			<p class="langcolor">Language: <a href="?lang=en" title="English">en</a> | <a href="?lang=nl" title="Nederlands">nl</a></p>
+		</div>
+	</div>
+	<div id="front-intro-about">
+		<h1>Over Xfce</h1>
+		<p>Xfce is een lichte desktop omgeving voor verschillende *NIX systemen. Omdat het ontworpen is om snel mee te kunnen werken, laadt het snel de programma's, terwijl het niet het geheugen vol propt.</p>
+		<p>Xfce 4,2 neemt de traditionele filosofie van Unix van modulariteit en mogelijkheid om opnieuw gebruikt te worden op. Het bestaat uit een aantal componenten die samen de volledige functionaliteit van het Desktopmilieu verstrekken. Zij worden afzonderlijk verpakt en u kunt en van de beschikbare pakketten verkiezen om de beste persoonlijke werkomgeving plukken tot stand te brengen.</p>
+		<p class="readmore"><a href="#" title="Learn more about Xfce...">meer informatie</a></p>
+	</div>
+</div>
+<div class="clearboth"></div>
+<div id="front-container">
+	<div id="front-news">
+		<div id="front-news-content">
+			<h2><a href="#"><img src="/layout/images/feed.gif" alt="feed" title="Subscribe to 'Xfce News'..." width="12" height="12" border="0" align="right" /></a>Latest News</h2>
+			<ul>
+				<li><span class="grey">2006-12-06</span><br />
+					<a href="#">OSNews has a review of the new 4.4 release</a>
+				</li>
+				<li><span class="grey">2006-12-06</span><br />
+					<a href="#">Xfce 4.4 released</a>
+				</li>
+				<li><span class="grey">2006-12-06</span><br />
+					<a href="#">A brand new Xfce website</a>
+				</li>
+				<li><span class="grey">2006-12-06</span><br />
+					<a href="#">Xfce 4.2.3.2 released</a>
+				</li>
+				<li><span class="grey">2006-12-06</span><br />
+					<a href="#">Thunar, a new filemanager for the Xfce desktop</a>
+				</li>
+			</ul>
+			<div class="readmore">
+				<a href="#" title="View the news archive...">archive</a>
+			</div>
+		</div>
+	</div>
+	<div id="front-projects">
+		<div class="front-borders">
+			<div id="front-projects-content">
+				<h2>Xfce projects</h2>
+				<ul>
+					<li><a href="#">Thunar</a><br />
+						<span class="grey">Thunar is a new modern file manager for the Xfce Desktop Environment.</span>
+					</li>
+					<li><a href="#">Mousepad</a><br />
+						<span class="grey">Mousepad is a simple, fast and easy-to-use text editor for the Xfce desktop environment, based on the Leafpad text editor.</span>
+					</li>
+					<li><a href="#">Orage</a><br />
+						<span class="grey">Calendar for the Xfce project.</span>
+					</li>
+					<li><a href="#">Xfburn</a><br />
+						<span class="grey">CD and DVD burning application.</span>
+					</li>
+					<li><a href="#">Xfmedia</a><br />
+						<span class="grey">Xfmedia is a simple, easy-to-use media player based on the xine engine.</span>
+					</li>
+				</ul>
+				<div class="readmore">
+					<a href="#" title="View all Xfce projects...">more projects</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="front-blog">
+		<div id="front-blog-content">
+			<h2><a href="#"><img src="/layout/images/feed.gif" alt="feed" title="Subscribe to 'Xfce Blog'..." width="12" height="12" border="0" align="right" /></a>Developers Blog</h2>
+			<ul>
+				<li><span class="grey">Eduard @ 2006-01-10</span><br />
+					<a href="#">Appfinder integration in panel</a>
+				</li>
+				<li><span class="grey">Jasper @ 2006-01-10</span><br />
+					<a href="#">&quot;Use KDE&quot; -- Linus Torvalds</a>
+				</li>
+				<li><span class="grey">Olivier @ 2006-01-10</span><br />
+					<a href="#">NVidia drivers updated</a>
+				</li>
+			</ul>
+			<div class="readmore">
+				<a href="#" title="View the blog archive...">archive</a>
+			</div>
+			<h2>Shortcuts</h2>
+			<ul>
+				<li><a href="#" title="Mailinglists">Mailinglists</a></li>
+				<li><a href="#" title="Xfce Forum">Xfce Forum</a></li>
+				<li><a href="#" title="Report a bug">Report a bug</a></li>
+				<li><a href="#" title="Handbook">Handbook</a></li>
+				<li><a href="#" title="Xfce Wiki">Xfce Wiki</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
