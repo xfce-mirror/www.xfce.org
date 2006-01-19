a1c265eea16774c5903ffbd625efa82c600928dd
Nick Schermer <nick@xfce.org>
2006-01-19 15:13:42 +0000

    files
    (Old svn revision: 77)

---
+++ i18n/projects/projects.en.php
@@ -0,0 +1,31 @@
+	<div id="content-right">
+		<div id="content-menu">
+			<h2 class="hidden">Content Navigation</h2>
+			<ul>
+				<li><strong>Applications</strong>
+					<ul>
+						<li><a href="/projects/thunar">Thunar</a></li>
+						<li>Terminal</li>
+						<li>Xfmedia</li>
+						<li>Xffm</li>
+						<li>Xfburn</li>
+						<li>Mousepad</li>
+					</ul>
+				</li>
+				<li><strong>Other</strong>
+					<ul>
+						<li>Xfce website</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div id="content-sponsor">
+			<h2 class="hidden">Sponsor</h2>
+			<a href="http://www.2x.com/" target="_blank"><img src="/layout/sponsors/2x_large.jpg" width="160" height="50" alt="2X" border="0" /></a>
+			<p><a href="http://www.2x.com/" title="2X  Software Ltd" target="_blank">Developers</a> of <a href="http://www.2x.com/terminalserver" target="_blank" title="2X Terminal Server for Linux">2X Terminal Server for Linux</a> & <a href="http://www.2x.com/thinclientserver" target="_blank" title="2X Thin Client Server">2X Thin Client Server</a><br>Linux <a href="http://www.2x.com/" target="_blank" title="2X  Software Ltd">thin client OS</a>, deployment & management</p>
+		</div>
+	</div>
+	<div id="content-text">
+		<h1>Projects</h1>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Ut faucibus  ipsum sit amet libero. Donec tincidunt. Cras elementum. In venenatis,  risus sagittis bibendum viverra, orci purus ullamcorper lacus, vitae  viverra erat libero eget dolor. Maecenas ac leo a libero mollis rutrum.  Morbi sit amet lectus. Pellentesque porttitor. Proin risus justo,  interdum vitae, convallis eget, gravida et, nisi. Ut aliquet felis sed  nisi. Cras accumsan mauris vulputate pede. Nullam sit amet quam ac  turpis viverra blandit. Sed at pede vel lorem varius aliquam. Fusce non  est. Etiam lacus leo, convallis id, ultrices sit amet, dictum et,  ipsum. Cras iaculis ligula vitae nibh. Sed dictum lacus sit amet velit. </p>
+	</div>
