1197f071138c27bd4ffdc2d3000f87b021381141
Auke Kok <sofar@foo-projects.org>
2006-01-19 22:21:12 +0000

    Contr*I*butors.
    (Old svn revision: 103)

--- i18n/about/contributors.en.php
+++ i18n/about/contributors.en.php
@@ -1,16 +1,16 @@
 <h1>Xfce contributors</h1>
 <p>Thanks to all who helped making this software available.</p>
 
-<?php $contrubutors = new Contrubutors(); ?>
+<?php $contributors = new Contributors(); ?>
 
 <h2 id="developers">Developers</h2>
-<?php echo $contrubutors->Developers (); ?>
+<?php echo $contributors->Developers (); ?>
 
 <h2 id="translators">Translators</h2>
-<?php echo $contrubutors->Translators (); ?>
+<?php echo $contributors->Translators (); ?>
 
 <h2 id="webtranslators">Web Translators</h2>
-<?php echo $contrubutors->Webtranslators (); ?>
+<?php echo $contributors->Webtranslators (); ?>
 
 <h2 id="contributors">Contributors</h2>
-<?php echo $contrubutors->Contributors (); ?>
+<?php echo $contributors->Contributors (); ?>
