c5b7051a1911bcf88f932f0a93fc51d983193b42
Nick Schermer <nick@xfce.org>
2006-01-22 10:59:32 +0000

    * test
    (Old svn revision: 144)

--- include/news.php
+++ include/news.php
@@ -1,8 +1,59 @@
 <?php
 
+function getElementAttributes($DomNode,$elementName,$attriName)
+{
+   if ($ChildDomNode = $DomNode->first_child())
+   {
+       while($ChildDomNode)
+       {
+           if ($ChildDomNode->node_type() == XML_ELEMENT_NODE)
+             {
+               if($ChildDomNode->node_name() == $elementName)
+               {
+              
+                 if ($ChildDomNode->has_attributes())
+                 {
+                   $Array = $ChildDomNode->attributes();
+                   foreach ($Array AS $DomAttribute)
+                   {
+                       if($DomAttribute->name() == $attriName)
+                       {
+                         $nodeArray[] = $DomAttribute->value();
+                       }
+                   }// foreach ($Array AS $DomAttribute)
+
+                 }//if ($ChildDomNode->has_attributes())
+              
+               }
+          
+             if ($ChildDomNode->has_child_nodes())
+               {
+                   $tmpArray = (getElementAttributes($ChildDomNode,$elementName,$attriName));
+                   $nodeArray = array_merge($nodeArray, $tmpArray);
+                   unset($tmpArray);
+               }// if ($ChildDomNode->has_child_nodes())
+              
+           }//if ($ChildDomNode->node_type() == XML_ELEMENT_NODE)
+          
+           $ChildDomNode = $ChildDomNode->next_sibling();
+
+       }//while($ChildDomNode)
+      
+   return $nodeArray;
+  }//if ($ChildDomNode = $DomNode->first_child())
+ 
+}
+
 function ParseFile ($file)
 {
-	$domxml = domxml_open_file($file);
+	$element = "pb";
+$att = "id";
+$DomDocument = domxml_open_file($file);
+$RootDomNode = $DomDocument->document_element();
+$array = getElementAttributes($RootDomNode,$element,$att);
+echo "<pre>";
+print_r($array);
+echo "</pre>";
 }
 
 ?>
