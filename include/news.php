2037cfe91649fbe9b24bde612e865bdda39f88df
Nick Schermer <nick@xfce.org>
2006-01-22 10:45:59 +0000

    * Removed some (useless) news.php code * Added phpinfo, to get some server information
    (Old svn revision: 142)

--- include/news.php
+++ include/news.php
@@ -1,21 +1,11 @@
 <?php
-function trustedFile($file)
-{
-   // only trust local files owned by ourselves
-   if (!eregi("^([a-z]+)://", $file)
-       && fileowner($file) == getmyuid()) {
-           return true;
-   }
-   return false;
-}
-
 function startElement($parser, $name, $attribs)
 {
-   echo "&lt;<font color=\"#0000cc\">$name</font>";
+   echo "&lt;<font color=\"blue\">$name</font>";
    if (count($attribs)) {
        foreach ($attribs as $k => $v) {
-           echo " <font color=\"#009900\">$k</font>=\"<font
-                   color=\"#990000\">$v</font>\"";
+           echo " <font color=\"green\">$k</font>=\"<font
+                   color=\"red\">$v</font>\"";
        }
    }
    echo "&gt;";
@@ -23,64 +13,12 @@ function startElement($parser, $name, $attribs)
 
 function endElement($parser, $name)
 {
-   echo "&lt;/<font color=\"#0000cc\">$name</font>&gt;";
+   echo "&lt;/<font color=\"blue\">$name</font>&gt;";
 }
 
 function characterData($parser, $data)
 {
-   echo "<b>$data</b>";
-}
-
-function PIHandler($parser, $target, $data)
-{
-   switch (strtolower($target)) {
-       case "php":
-           global $parser_file;
-           // If the parsed document is "trusted", we say it is safe
-           // to execute PHP code inside it.  If not, display the code
-           // instead.
-           if (trustedFile($parser_file[$parser])) {
-               eval($data);
-           } else {
-               printf("Untrusted PHP code: <i>%s</i>",
-                       htmlspecialchars($data));
-           }
-           break;
-   }
-}
-
-function defaultHandler($parser, $data)
-{
-   if (substr($data, 0, 1) == "&" && substr($data, -1, 1) == ";") {
-       printf('<font color="#aa00aa">%s</font>',
-               htmlspecialchars($data));
-   } else {
-       printf('<font size="-1">%s</font>',
-               htmlspecialchars($data));
-   }
-}
-
-function externalEntityRefHandler($parser, $openEntityNames, $base, $systemId,
-                                 $publicId) {
-   if ($systemId) {
-       if (!list($parser, $fp) = new_xml_parser($systemId)) {
-           printf("Could not open entity %s at %s\n", $openEntityNames,
-                   $systemId);
-           return false;
-       }
-       while ($data = fread($fp, 4096)) {
-           if (!xml_parse($parser, $data, feof($fp))) {
-               printf("XML error: %s at line %d while parsing entity %s\n",
-                       xml_error_string(xml_get_error_code($parser)),
-                       xml_get_current_line_number($parser), $openEntityNames);
-               xml_parser_free($parser);
-               return false;
-           }
-       }
-       xml_parser_free($parser);
-       return true;
-   }
-   return false;
+   echo "$data";
 }
 
 function new_xml_parser($file)
@@ -91,9 +29,6 @@ function new_xml_parser($file)
    xml_parser_set_option($xml_parser, XML_OPTION_CASE_FOLDING, 1);
    xml_set_element_handler($xml_parser, "startElement", "endElement");
    xml_set_character_data_handler($xml_parser, "characterData");
-   //xml_set_processing_instruction_handler($xml_parser, "PIHandler");
-   //xml_set_default_handler($xml_parser, "defaultHandler");
-  // xml_set_external_entity_ref_handler($xml_parser, "externalEntityRefHandler");
   
    if (!($fp = @fopen($file, "r"))) {
        return false;
