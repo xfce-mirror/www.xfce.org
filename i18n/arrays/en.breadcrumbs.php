8689f5ce0c755488b80801e3d922779be9b9c93e
Nick Schermer <nick@xfce.org>
2006-01-31 22:07:03 +0000

    * Breadcrumb fix
    (Old svn revision: 162)

--- i18n/arrays/en.breadcrumbs.php
+++ i18n/arrays/en.breadcrumbs.php
@@ -4,6 +4,7 @@
 $breadcrumbs = array (
 	"home" => "Home",
 	"/download/plugins/" => "Panel Plugins",
+	"/documentation/manuals/developers/" => "Developers (API)",
 	"/documentation/manuals/installation/" => "Installation Guide",
 	"/documentation/manuals/program/" => "Program Documentation",
 	"/documentation/whatsnew/" => "What's New",
