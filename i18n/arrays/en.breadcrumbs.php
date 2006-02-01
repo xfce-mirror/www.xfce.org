188aa97b751002bb466221ca477487ff4860843d
Nick Schermer <nick@xfce.org>
2006-02-01 21:17:37 +0000

    * Some breadcrumb style fixes
    (Old svn revision: 169)

--- i18n/arrays/en.breadcrumbs.php
+++ i18n/arrays/en.breadcrumbs.php
@@ -2,12 +2,17 @@
 
 # Translations or Style updates for the breadcrumbs
 $breadcrumbs = array (
-	"home" => "Home",
-	"/download/plugins/" => "Panel Plugins",
-	"/documentation/manuals/developers/" => "Developers (API)",
-	"/documentation/manuals/installation/" => "Installation Guide",
-	"/documentation/manuals/program/" => "Program Documentation",
-	"/documentation/whatsnew/" => "What's New",
-	"/community/lists/" => "Mailing Lists",
-	"/community/irc/" => "IRC"
+	"home" 									=> "Home",
+	"/projects/core/"						=> "Core Components",
+	"/projects/related/"					=> "Xfce Related",
+	"/download/development/"				=> "Development version",
+	"/download/plugins/"					=> "Panel Plugins",
+	"/documentation/manuals/developers/"	=> "Developers (API)",
+	"/documentation/manuals/installation/"	=> "Installation Guide",
+	"/documentation/manuals/program/"		=> "Program Documentation",
+	"/documentation/whatsnew/"				=> "What's New",
+	"/community/lists/"						=> "Mailing Lists",
+	"/community/irc/"						=> "IRC"
 );
+
+?>
