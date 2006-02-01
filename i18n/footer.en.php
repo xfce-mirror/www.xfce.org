efb57532ffe7cad9e626bf153d6bfd540a73654a
Nick Schermer <nick@xfce.org>
2006-02-01 21:49:25 +0000

    * Logo update, they all have the same size now
    (Old svn revision: 180)

--- i18n/footer.en.php
+++ i18n/footer.en.php
@@ -6,11 +6,11 @@
 			<div id="footer-sponsors">
 				<h2 class="hidden">Logos</h2>
 				<ul>
-					<li><a href="http://www.2x.com/" title="2X Thin client computing software" target="_blank"><img src="/layout/friends/2x_small.jpg" alt="2X" width="96" height="30" border="0" /></a></li>
-					<li><a href="http://www.os-cillation.com/" title="os-cillation" target="_blank"><img src="/layout/friends/os-cillation.jpg" alt="os-cillation" width="115" height="30" border="0" /></a></li>
-					<li><a href="http://sourceforge.net/" title="SourceForge.net" target="_blank"><img src="/layout/friends/sf.jpg" alt="SourceForge.net" width="87" height="30" border="0" /></a></li>
-					<li><a href="http://www.lunar-linux.org/" title="Lunar-Linux" target="_blank"><img src="/layout/friends/lunarlinux.jpg" alt="Lunar-Linux" width="112" height="30" border="0" /></a></li>
-					<li><a href="http://www.web-adept.nl/" title="Web-Adept" target="_blank"><img src="/layout/friends/web_adept.jpg" alt="Web-Adept" width="94" height="30" border="0" /></a></li>
+					<li><a href="http://www.2x.com/" title="2X Thin client computing software" target="_blank"><img src="/layout/friends/2x_small.jpg" alt="2X" border="1" /></a></li>
+					<li><a href="http://www.os-cillation.com/" title="os-cillation" target="_blank"><img src="/layout/friends/os-cillation.jpg" alt="os-cillation" border="1" /></a></li>
+					<li><a href="http://sourceforge.net/" title="SourceForge.net" target="_blank"><img src="/layout/friends/sf.jpg" alt="SourceForge.net" border="1" /></a></li>
+					<li><a href="http://www.lunar-linux.org/" title="Lunar-Linux" target="_blank"><img src="/layout/friends/lunarlinux.jpg" alt="Lunar-Linux" border="1" /></a></li>
+					<li><a href="http://www.web-adept.nl/" title="Web-Adept" target="_blank"><img src="/layout/friends/web_adept.jpg" alt="Web-Adept" border="1" /></a></li>
 				</ul>
 			</div>
 			<div id="footer-copyright">
