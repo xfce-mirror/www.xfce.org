d6ac0b604865e2fa670dcbc4bef42020cea0dcc2
Nick Schermer <nick@xfce.org>
2006-02-07 13:56:43 +0000

    * More sample changes
    (Old svn revision: 194)

--- i18n/documentation/faq.en.php
+++ i18n/documentation/faq.en.php
@@ -3,48 +3,57 @@
 <h2>Questions</h2>
 <h3>Getting started</h3>
 <ul>
-  <li>What is &quot;Xfce&quot; and why should I use it?</li>
-  <li>How is Xfce pronounced and what does it mean? </li>
-  <li>What does the logo symbolize?</li>
-  <li>On what platforms does Xfce currently run? </li>
+  <li><a href="#1">What is &quot;Xfce&quot; and why should I use it?</a></li>
+  <li><a href="#2">How is Xfce pronounced and what does it mean? </a></li>
+  <li><a href="#3">What does the logo symbolize?</a></li>
+  <li><a href="#4">On what platforms does Xfce currently run? </a></li>
 </ul>
 <h3>Installation</h3>
 <ul>
-  <li>How do I installate Xfce? </li>
-  <li>What is the "use startup notification" option?</li>
-  <li>The graphical installer fails with &quot;C++ preprocessor fails sanity check&quot;</li>
-  <li>The graphical installer fails with &quot;Unable to connect to Xserver&quot;</li>
-  <li>Is it possible to use Xfce with DM's? </li>
-  <li>Is it possible to start Xfce when I manual login? </li>
+  <li><a href="#">How do I installate Xfce? </a></li>
+  <li><a href="#">What is the "use startup notification" option?</a></li>
+  <li><a href="#">The graphical installer fails with &quot;C++ preprocessor fails sanity check&quot;</a></li>
+  <li><a href="#">The graphical installer fails with &quot;Unable to connect to Xserver&quot;</a></li>
+  <li><a href="#">Is it possible to use Xfce with DM's? </a></li>
+  <li><a href="#">Is it possible to start Xfce when I manual login? </a></li>
 </ul>
 <h3>Usage</h3>
 <ul>
-  <li>How do I start Xfce. </li>
-  <li>I want to use Xfce in a corporate environment and don't let users to modify their menu.</li>
-  <li>How do I display a list of all windows?</li>
-  <li>How do I disable the taskbar in Xfce 4.2?</li>
-  <li>When I start Xfce a dialog pops up saying &quot;Could not look up internet address for&quot; ... </li>
-  <li>How do I get numlock to start on login?</li>
-  <li>Is it possible to use Media keys in the Shortcut Editor? </li>
-  <li>I'm unable to shutdown or restart my computer when running Xfce. </li>
+  <li><a href="#">How do I start Xfce. </a></li>
+  <li><a href="#">I want to use Xfce in a corporate environment and don't let users to modify their menu.</a></li>
+  <li><a href="#">How do I display a list of all windows?</a></li>
+  <li><a href="#">How do I disable the taskbar in Xfce 4.2?</a></li>
+  <li><a href="#">When I start Xfce a dialog pops up saying &quot;Could not look up internet address for&quot; ... </a></li>
+  <li><a href="#">How do I get numlock to start on login?</a></li>
+  <li><a href="#">Is it possible to use Media keys in the Shortcut Editor? </a></li>
+  <li><a href="#">I'm unable to shutdown or restart my computer when running Xfce. </a></li>
 </ul>
 <h3>Visual Look</h3>
 <ul>
-  <li>How do I set the panel layer in Xfce 4.2 and 4.4?</li>
-  <li>How do I enable panel transparency and windows shadows? </li>
-  <li>The Xfce Desktop doesn't has any shortcut icons, why? </li>
-  <li>How do </li>
-  <li>The gtk-xfce-engine themes do not appear in the &quot;user interface&quot; settings dialog. </li>
-  <li>Is it possible to change the icon used by the iconbox or taskbar for a given application?</li>
+  <li><a href="#">How do I set the panel layer in Xfce 4.2 and 4.4?</a></li>
+  <li><a href="#">How do I enable panel transparency and windows shadows? </a></li>
+  <li><a href="#">The Xfce Desktop doesn't has any shortcut icons, why? </a></li>
+  <li><a href="#">How do </a></li>
+  <li><a href="#">The gtk-xfce-engine themes do not appear in the &quot;user interface&quot; settings dialog. </a></li>
+  <li><a href="#">Is it possible to change the icon used by the iconbox or taskbar for a given application?</a></li>
 </ul>
 <h3>Development</h3>
 <ul>
-  <li>How can I report bugs?</li>
-  <li>How often are new releases made? </li>
+  <li><a href="#">How can I report bugs?</a></li>
+  <li><a href="#">How often are new releases made? </a></li>
 </ul>
 <h2>Answers</h2>
 <h3>Getting started</h3>
-<p>...</p>
+<dl>
+	<dt><strong><a name="1">What is &quot;Xfce&quot; and why should I use it?</a></strong></dt>
+	<dd>bladiebladiebla</dd>
+	<dt><strong><a name="2">How is Xfce pronounced and what does it mean?</a></strong></dt>
+	<dd>bladiebladiebla</dd>
+	<dt><strong><a name="3">What does the logo symbolize?</a></strong></dt>
+	<dd>bladiebladiebla</dd>
+	<dt><strong><a name="4">On what platforms does Xfce currently run?</a></strong></dt>
+	<dd>bladiebladiebla</dd>
+</dl>
 <h3>Installation</h3>
 <p>...</p>
 <h3>Usage</h3>
