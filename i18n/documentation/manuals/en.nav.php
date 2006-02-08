f577f81461b8fd6a57430d62b8730dee196dd43a
Nick Schermer <nick@xfce.org>
2006-02-08 10:16:57 +0000

    * Synchronize the content and frontpage fonts * Update manuals navigation
    (Old svn revision: 202)

--- i18n/documentation/manuals/en.nav.php
+++ i18n/documentation/manuals/en.nav.php
@@ -1,10 +1,15 @@
 <ul>
 	<li><a href="/documentation">Documentation</a></li>
-		<li><a href="/documentation/faq">FAQs</a>
+	<li><a href="/documentation/faq">FAQs</a>
 		<ul>
-			<li><a href="http://xfce.wikicities.com/wiki/Frequently_Asked_Questions" target="_blank" class="external">Wiki FAQ</a></li>
+			<li><a href="/documentation/faq">Getting started</a>
+			<li><a href="/documentation/faq">Installation</a>
+			<li><a href="/documentation/faq">Usage</a>
+			<li><a href="/documentation/faq">Visual Look</a>
+			<li><a href="/documentation/faq">Development</a>
 		</ul>
 	</li>
+	<li><a href="http://xfce.wikicities.com/wiki/Frequently_Asked_Questions" target="_blank" class="external">Wiki FAQ</a></li>
 	<li><a href="/documentation/requirements">Requirements</a></li>
 	<li><a href="/documentation/manuals/">Manuals</a>
 		<ul>
