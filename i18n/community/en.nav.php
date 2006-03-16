f55be4f1921df2a5756288b9952f0f81e5e64adb
Nick Schermer <nick@xfce.org>
2006-03-16 19:56:47 +0000

    * Some changes in the mailing list page * Files were on Windows XP a while, so they are modified, but they aren't (get it?)
    (Old svn revision: 206)

--- i18n/community/en.nav.php
+++ i18n/community/en.nav.php
@@ -2,12 +2,6 @@
 	<li><a href="/community">Community</a></li>
 	<li><a href="http://forum.xfce.org" target="_blank" class="external">Forum</a></li>
 	<li><a href="http://blog.xfce.org" target="_blank" class="external">Xfce Blog</a></li>
-	<li><a href="/community/lists">Mailing Lists</a>
-		<ul>
-			<li><a href="/community/lists#users">Users</a></li>
-			<li><a href="/community/lists#developers">Developers</a></li>
-			<li><a href="/community/lists#projects">Projects</a></li>
-		</ul>
-	</li>
+  <li><a href="/community/lists">Mailing Lists</a></li>
 	<li><a href="/community/irc">IRC</a></li>
 </ul>
--- i18n/community/lists.en.php
+++ i18n/community/lists.en.php
@@ -1,69 +1,47 @@
 <h1>Xfce Mailing Lists</h1>
-<p></p>
+<p><strong>NOTE:</strong> In order to post to these lists, you must first subscribe. </p>
 
-<h2 id="users">Users</h2>
 <p>
-<table>
+<table width="100%" cellpadding="2">
     <tr>
-	<th>List</th>
-	<th>Description</th>
+		<th align="left"><h3><span class="style1">List</span></h3></th>
+		<th align="left"><h3><span class="style1">Description</span></h3></th>
     </tr>
     <tr>
-	<td><a href="http://foo-projects.org/mailman/listinfo/xfce/">xfce</a></td>
-	<td>For xfce users</td>
+	  <td bgcolor="#eeeeee"><a href="http://foo-projects.org/mailman/listinfo/xfce/">xfce</a></td>
+		<td bgcolor="#eeeeee">Discussion list for users of Xfce</td>
     </tr>
     <tr>
-	<td><a href="http://foo-projects.org/mailman/listinfo/xfce-announce/">xfce-announce</a></td>
-	<td>To be notified of new releases.</td>
+		<td><a href="http://foo-projects.org/mailman/listinfo/xfce-announce/">xfce-announce</a></td>
+		<td>Announcements of new versions of Xfce and related projects.</td>
     </tr>
     <tr>
-	<td><a href="http://foo-projects.org/mailman/listinfo/xfce-advocacy/">xfce-advocacy</a></td>
-	<td>To coordinate Xfce advocacy.</td>
-    </tr>
-</table>
-</p>
-
-
-<h2 id="developers">Developers</h2>
-<p>
-<table>
-    <tr>
-	<th>List</th>
-	<th>Description</th>
+	  <td bgcolor="#EEEEEE"><a href="http://foo-projects.org/mailman/listinfo/xfce-advocacy/">xfce-advocacy</a></td>
+		<td bgcolor="#EEEEEE">This mailing list is used as a point of contact for people interested in promoting Xfce.</td>
     </tr>
     <tr>
-	<td><a href="http://foo-projects.org/mailman/listinfo/xfce-i18n/">xfce-i18n</a></td>
-	<td>To coordinate the translation of Xfce into new languages</td>
+		<td><a href="http://foo-projects.org/mailman/listinfo/xfce-i18n/">xfce-i18n</a></td>
+		<td>To coordinate the translation of Xfce into new languages.</td>
     </tr>
     <tr>
-	<td><a href="http://foo-projects.org/mailman/listinfo/xfce4-dev/">xfce4-dev</a></td>
-	<td>For Xfce4 specific development.</td>
+	  <td bgcolor="#EEEEEE"><a href="http://foo-projects.org/mailman/listinfo/xfce4-dev/">xfce4-dev</a></td>
+		<td bgcolor="#EEEEEE">A mailing list for developers of Xfce.</td>
     </tr>
     <tr>
-	<td><a href="http://foo-projects.org/mailman/listinfo/xfce4-commits/">xfce4-commits</a></td>
-	<td>To be notified of Xfce4 SVN commits.</td>
+		<td><a href="http://foo-projects.org/mailman/listinfo/xfce4-commits/">xfce4-commits</a></td>
+		<td>To be notified of Xfce4 SVN commits.</td>
     </tr>
     <tr>
-	<td><a href="http://developer.berlios.de/mail/?group_id=910">goodies</a></td>
-	<td>For xfce-goodies development</td>
+	  <td bgcolor="#EEEEEE"><a href="http://developer.berlios.de/mail/?group_id=910">goodies</a></td>
+		<td bgcolor="#EEEEEE">A mailing list for developers of Xfce Goodies.</td>
     </tr>
     <tr>
-	<td><a href="http://foo-projects.org/mailman/listinfo/xfce-bugs/">xfce-bugs</a></td>
-	<td>To be notified of new bugs in the bug tracking system</td>
-    </tr>
-</table>
-</p>
-
-<h2 id="projects">Projects</h2>
-<p>
-<table>
-    <tr>
-	<th>List</th>
-	<th>Description</th>
+		<td><a href="http://foo-projects.org/mailman/listinfo/xfce-bugs/">xfce-bugs</a></td>
+		<td>Discussion of bugs in Xfce products. You can report a bug <a href="http://bugzilla.xfce.org" target="_blank" class="external">here</a>.</td>
     </tr>
     <tr>
-	<td><a href="http://foo-projects.org/mailman/listinfo/xfce-i18n/">thunar-dev</a></td>
-	<td>For the new Xfce file manager development</td>
+	  <td bgcolor="#EEEEEE"><a href="http://foo-projects.org/mailman/listinfo/xfce-i18n/">thunar-dev</a></td>
+		<td bgcolor="#EEEEEE">A mailing list for development of the Thunar file manager</td>
     </tr>
 </table>
 </p>
