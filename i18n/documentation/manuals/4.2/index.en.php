06e933b3b170c7b5aa74ddc08d08d8d2eb983fef
Auke Kok <sofar@foo-projects.org>
2006-03-27 02:18:29 +0000

    horribly broken but this is all the 4.2 docs - Ill need to clean it up
    (Old svn revision: 209)

---
+++ i18n/documentation/manuals/4.2/index.en.php
@@ -0,0 +1,27 @@
+<h1>Xfce 4.2.0 Documentation</h1>
+
+<p>The documentation for Xfce 4.2.0 is installed with the different 
+components that make up the desktop environment. As a consequence, 
+some of the documents listed here may not be available on your system.
+</p>
+
+<hr />
+
+<h2>Guides</h2>
+
+<p><a href="/documentation/manuals/4.2/xfce4-user-guide">Xfce 4.2 User Guide</a></p>
+
+<h2>Manuals</h2>
+
+<ul>
+    <li><a href="/documentation/manuals/4.2/xfce4-panel">Panel</a></li>
+    <li><a href="/documentation/manuals/4.2/xfwm4">Window Manager</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce4-session">Session Manager</a></li>
+    <li><a href="/documentation/manuals/4.2/xfdesktop">Desktop Manager</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce-utils">Utilities and Scripts</a></li>
+    <li><a href="/documentation/manuals/4.2/xfprint">Printing Management</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce-mcs-manager">Settings Manager</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce-mcs-plugins">Settings Manager Plugins</a></li>
+    <li><a href="/documentation/manuals/4.2/xffm">Filemanager</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce4-appfinder">Application Finder</a></li>
+</ul>
---
+++ i18n/documentation/manuals/4.2/xfce4-components.en.php
@@ -0,0 +1,40 @@
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"><title>Components of Xfce 4</title><link rel="stylesheet" href="../xfce.css" type="text/css"><meta name="generator" content="DocBook XSL Stylesheets V1.66.1"><link rel="start" href="xfce4-user-guide.html" title="
+  The Xfce 4 Desktop Environment
+"><link rel="up" href="xfce4-user-guide.html" title="
+  The Xfce 4 Desktop Environment
+"><link rel="prev" href="xfce4-use.html" title="Using Xfce4"><link rel="next" href="xfce4-install.html" title="Installing Xfce4"></head><body bgcolor="white" text="black" link="#0000FF" vlink="#840084" alink="#0000FF"><div class="navheader"><table width="100%" summary="Navigation header"><tr><th colspan="3" align="center">Components of Xfce 4</th></tr><tr><td width="20%" align="left"><a accesskey="p" href="xfce4-use.html">Prev</a> </td><th width="60%" align="center"> </th><td width="20%" align="right"> <a accesskey="n" href="xfce4-install.html">Next</a></td></tr></table><hr></div><div class="sect1" lang="en"><div class="titlepage"><div><div><h2 class="title" style="clear: both"><a name="xfce4-components"></a>Components of Xfce 4</h2></div></div></div><p>The Xfce 4 components are packaged separately. This section contains a
+    list of the available modules with a short description.
+  </p><div class="variablelist"><dl><dt><span class="term">libxfce4util</span></dt><dd><p>Library with non-graphical helper functions.</p></dd><dt><span class="term">libxfcegui4</span></dt><dd><p>Widget library and X Window System interaction.</p></dd><dt><span class="term">libxfce4mcs</span></dt><dd><p>Settings management library used by most Xfce 4 modules.
+	</p></dd><dt><span class="term">xfce-mcs-manager</span></dt><dd><p>Settings manager. Runs in the background and provides global
+	  settings information to other Xfce components. There is a main
+	  control panel to access the configuration dialogs of Xfce
+	  components.
+	</p></dd><dt><span class="term">xfce-mcs-plugins</span></dt><dd><p>Settings manager plugins. These plugins provide settings 
+	  dialogs for general items that are not part of a package, e.g. 
+	  user interface settings (Gtk+ theme, icon theme, font settings),
+          mouse settings, keyboard settings and display settings.
+	</p></dd><dt><span class="term">xfce4-session</span></dt><dd><p>Session manager. The session manager controls the startup and
+	  shutdown of the Xfce Desktop Environment. On logout it can save the
+	  state of running applications (if the application supports it) and
+          restore them properly again the next time you log in.
+	</p></dd><dt><span class="term">xfwm4</span></dt><dd><p>The Xfce 4 window mananger. Manages the placement of 
+	  application windows on the screen, provides window decorations 
+	  and manages workspaces.
+	</p></dd><dt><span class="term">xfce4-panel</span></dt><dd><p>The Xfce 4 panel. Provides program lauchers, a workspace
+	  switcher, a clock, menus and more.
+	</p></dd><dt><span class="term">xfdesktop</span></dt><dd><p>Desktop background manager. This program sets the background 
+	  image and/or color, and provides a root window menu, a menu panel
+	  plugin and a menu editor.
+	</p></dd><dt><span class="term">xfce-utils</span></dt><dd><p>Essential utilities and scripts. Provides a taskbar, the
+	  Xfce 4 about dialog, a run dialog, the startxfce4 script and 
+	  several other important scripts. Also contains this user guide.
+	</p></dd><dt><span class="term">xffm</span></dt><dd><p>A fast file manager, with double pane and integrated samba 
+	  network browser.
+	</p></dd><dt><span class="term">xfprint</span></dt><dd><p>Printing support. Provides a graphical frontend for printing.
+	  Includes <b class="application"><tt>xfprint4</tt></b> and 
+	  <b class="application"><tt>xfprint-manager</tt></b>.
+	</p></dd><dt><span class="term">xfce4-appfinder</span></dt><dd><p>An application finder, which allows you to search, launch and 
+	  find information about applications installed on your system.
+	</p></dd><dt><span class="term">xfcalendar</span></dt><dd><p>Simple calendar application with reminders.</p></dd><dt><span class="term">gtk-xfce-engine-2</span></dt><dd><p>Theme engine for GTK2. Not required for the desktop, but
+	  it's a nice theme engine so you might just as well give it a try.
+	</p></dd><dt><span class="term">xfce4-icon-theme</span></dt><dd><p>Default icon theme for Xfce 4, called <span class="emphasis"><em>Rodent</em></span>.</p></dd><dt><span class="term">xfwm4-themes</span></dt><dd><p>Window decoration themes for xfwm4.</p></dd></dl></div><p> </p></div><div class="navfooter"><hr><table width="100%" summary="Navigation footer"><tr><td width="40%" align="left"><a accesskey="p" href="xfce4-use.html">Prev</a> </td><td width="20%" align="center"><a accesskey="u" href="xfce4-user-guide.html">Up</a></td><td width="40%" align="right"> <a accesskey="n" href="xfce4-install.html">Next</a></td></tr><tr><td width="40%" align="left" valign="top">Using Xfce4 </td><td width="20%" align="center"><a accesskey="h" href="xfce4-user-guide.html">Home</a></td><td width="40%" align="right" valign="top"> Installing Xfce4</td></tr></table></div></body></html>
