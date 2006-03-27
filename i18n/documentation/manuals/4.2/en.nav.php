3b1a35a614727bc92fe27536c02770026fea1cde
Auke Kok <sofar@foo-projects.org>
2006-03-27 02:24:20 +0000

    Adding nav file.
    (Old svn revision: 210)

---
+++ i18n/documentation/manuals/4.2/en.nav.php
@@ -0,0 +1,14 @@
+<h2>Manuals</h2>
+
+<ul>
+    <li><a href="/documentation/manuals/4.2/xfce4-panel">Panel</a></li>
+    <li><a href="/documentation/manuals/4.2/xfwm4">Window Manager</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce4-session">Session Manager</a></li>
+    <li><a href="/documentation/manuals/4.2/xfdesktop">Desktop Manager</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce-utils">Utilities and Scripts</a></li>
+    <li><a href="/documentation/manuals/4.2/xfprint">Printing Management</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce-mcs-manager">Settings Manager</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce-mcs-plugins">Settings Manager Plugins</a></li>
+    <li><a href="/documentation/manuals/4.2/xffm">Filemanager</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce4-appfinder">Application Finder</a></li>
+</ul>
