2981890f383ad658a62f517cbee1d03d0e19c380
Nick Schermer <nick@xfce.org>
2006-03-28 20:46:29 +0000

    * Finally added. There are to many people working on Xfce ^_~.
    (Old svn revision: 212)

--- i18n/about/credits.en.php
+++ i18n/about/credits.en.php
@@ -2,16 +2,366 @@
 <p></p>
 
 <h2 id="lead">Project Lead</h2>
-<p></p>
-
+<p>
+Olivier Fourdan [fourdan at xfce.org]
+</p>
+<br />
 <h2 id="core">Core Developers</h2>
-<p></p>
-
+<p>
+<table width="100%">
+	<tr>
+		<th width="40%" align="left"><h3>Contributor</h3></th>
+		<th width="60%" align="left"><h3>Main Project(s)</h3></th>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Benedikt Meurer [benny at xfce.org]</td>
+		<td width="60%" bgcolor="#eeeeee">Thunar, Terminal, Session Manager, Libexo </td>
+	</tr>
+	<tr>
+		<td width="40%">Biju Chacko [botsie at xfce.org]</td>
+		<td width="60%">Beta tester (and Xfce user for a long time) </td>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Brian J. Tarricone [kelnos at xfce.org]</td>
+		<td width="60%" bgcolor="#eeeeee">Desktop Manager, Media Player </td>
+	</tr>
+	<tr>
+		<td width="40%">Danny Milosavljevic [dannym at xfce.org]</td>
+		<td width="60%">Volume Control </td>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Edscott Wilson Garcia [edscott at xfce.org]</td>
+		<td width="60%" bgcolor="#eeeeee">Filemanager</td>
+	</tr>
+	<tr>
+		<td width="40%">Eduard Roccatello [eduard at xfce.org]</td>
+		<td width="60%">Text Editor, Application Finder </td>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Fran&ccedil;ois Le Clainche [fleclainche at wanadoo.fr]</td>
+		<td width="60%" bgcolor="#eeeeee">Webmaster and artwork</td>
+	</tr>
+	<tr>
+		<td width="40%">Giuseppe Torelli [colossus at xfce.org]</td>
+		<td width="60%">Archive manager </td>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Jasper Huijsmans [jasper at xfce.org]</td>
+		<td width="60%" bgcolor="#eeeeee">Panel</td>
+	</tr>
+	<tr>
+		<td width="40%">Jean-Fran&ccedil;ois Wauthy [pollux at xfce.org]</td>
+		<td width="60%">Printer Manager, CD burning application</td>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Micka&euml;l Graf [korbinus at xfce.org]</td>
+		<td width="60%" bgcolor="#eeeeee">Calendar</td>
+	</tr>
+	<tr>
+		<td width="40%">Olivier Fourdan [fourdan at xfce.org]</td>
+		<td width="60%">Window Manager </td>
+	</tr>
+</table>
+</p>
+<br />
 <h2 id="contributors">Contributors</h2>
-<p></p>
-
-<h2 id="server">Server</h2>
-<p></p>
-
+<p>
+<table width="100%">
+	<tr>
+	  <th width="40%" align="left"><h3>Contributor</h3></th>
+		<th width="60%" align="left"><h3>Contribution</h3></th>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Bernhard Walle [Bernhard.Walle at gmx.de]</td>
+		<td width="60%" bgcolor="#eeeeee"></td>
+	</tr>
+	<tr>
+		<td width="40%">Craig Betts [craig.betts at dfrc.nasa.gov]</td>
+		<td width="60%"></td>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Ejvend Nielsen [prophet at sphere-x.net]</td>
+		<td width="60%" bgcolor="#eeeeee"></td>
+	</tr>
+	<tr>
+		<td width="40%">Erik Touve [etouve at earthlink.net]</td>
+		<td width="60%"></td>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Jens Guballa [j.guballa at t-online.de]</td>
+		<td width="60%" bgcolor="#eeeeee"></td>
+	</tr>
+	<tr>
+		<td width="40%">Jens Luedicke [jens at irs-net.com]</td>
+		<td width="60%"></td>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Joakim Andreasson [joakim.andreasson at gmx.net]</td>
+		<td width="60%" bgcolor="#eeeeee"></td>
+	</tr>
+	<tr>
+		<td width="40%">Karsten Luetkewitz [phrep at plskthx.org]</td>
+		<td width="60%"></td>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Martin Loschwitz [madkiss at debian.org]</td>
+		<td width="60%" bgcolor="#eeeeee"></td>
+	</tr>
+	<tr>
+		<td width="40%">Michael Mosier [michael at spyonit.com]</td>
+		<td width="60%"></td>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Thomas Leonard [tal00r at ecs.soton.ac.uk]</td>
+		<td width="60%" bgcolor="#eeeeee"></td>
+	</tr>
+	<tr>
+		<td width="40%">Tobias Henle [tobias at page23.de]</td>
+		<td width="60%"></td>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Xavier Maillard [zedek at fxgsproject.org]</td>
+		<td width="60%" bgcolor="#eeeeee"></td>
+	</tr>
+</table>
+</p>
+<br />
+<h2 id="server">Server and website </h2>
+<p>
+<table width="100%">
+	<tr>
+	  <th align="left"><h3>Contributor</h3></th>
+		<th align="left"><h3>Contribution</h3></th>
+	</tr>
+	<tr>
+		<td width="40%" bgcolor="#eeeeee">Auke Kok [sofar at foo-projects.org]</td>
+		<td width="60%" bgcolor="#eeeeee">Server maintainence</td>
+	</tr>
+	<tr>
+		<td width="40%">Brian J. Tarricone [kelnos at xfce.org]</td>
+		<td width="60%">Bugzilla administrator</td>
+	</tr>
+	<tr>
+      <td bgcolor="#eeeeee">Nick Schermer [nick at xfce.org]</td>
+	  <td bgcolor="#eeeeee">Webdesigner</td>
+  </tr>
+	<tr>
+      <td>Remco Lubbers [remco@web-adept.nl]</td>
+	  <td><a href="http://www.web-adept.nl" target="_blank">Hosting</a></td>
+  </tr>
+</table>
+</p>
+<br />
 <h2 id="translators">Translators</h2>
-<p></p>
+<p>
+<table width="100%">
+  <tr>
+    <th align="left"><h3>Translations supervision</h3></th>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Daichi Kawahata [daichi at xfce.org]</td>
+  </tr>
+  <tr>
+    <td>Jean-Fran&ccedil;ois Wauthy [pollux at xfce.org]</td>
+  </tr>
+</table>
+</p>
+<p>
+<table width="100%">
+  <tr>
+    <th width="20%" align="left"><h3>Language</h3></th>
+    <th width="80%" align="left"><h3>Contributor(s)</h3></th>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Afrikaans [af]</td>
+    <td bgcolor="#eeeeee">Petri Jooste [petri.jooste at gmail.com]</td>
+  </tr>
+  <tr>
+    <td>Arabic [ar]</td>
+    <td>Saleh Alhathal [hathalsal at hotmail.com]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Azerbaijani [az]</td>
+    <td bgcolor="#eeeeee">Metin Amiroff [metin at karegen.com]</td>
+  </tr>
+  <tr>
+    <td>Breton [be]</td>
+    <td>Ales Nyakhaychyk [nab at mail.by]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Bulgarian [bg]</td>
+    <td bgcolor="#eeeeee">Miroslav Yordanov [miordanov at i-space.org]</td>
+  </tr>
+  <tr>
+    <td>Bengali [bn_IN]</td>
+    <td>Runa Bhattacharjee [runabh at gmail.com]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Catalan [ca]</td>
+    <td bgcolor="#eeeeee">Pau Rullan Ferragut [paurullan at bulma.net]</td>
+  </tr>
+  <tr>
+    <td>Czech [cs]</td>
+    <td>Alois Nespor [Alois.Nespor at seznam.cz]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Danish [da]</td>
+    <td bgcolor="#eeeeee">Jesper Berth [big at santaz.dk]</td>
+  </tr>
+  <tr>
+    <td>German [de]</td>
+    <td>Fabian Nowak [timystery at arcor.de], Moritz Heiber [moe at xfce.org]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Greek [el]</td>
+    <td bgcolor="#eeeeee">Stavros Giannouris [stavrosg2002 at freemail.gr]</td>
+  </tr>
+  <tr>
+    <td>English [en_GB]</td>
+    <td>Dwayne Bailey [dwayne at translate.org.za]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Esperanto [eo]</td>
+    <td bgcolor="#eeeeee">Sylvain Vedrenne [gnu_sylvain at xfce.org]</td>
+  </tr>
+  <tr>
+    <td>Spanish [es]</td>
+    <td>Jaime Buffery [nestu at lunar-linux.org], Pablo Hernandez-M. Saiz [homeless3d at gmail.com], Rudy Godoy [rudy at stone-head.org]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Spanish [es_MX]</td>
+    <td bgcolor="#eeeeee">Edscott Wilson Garcia [edscott at xfce.org]</td>
+  </tr>
+  <tr>
+    <td>Estonian [et]</td>
+    <td>Mart Tonso [Mart.Tonso at ttu.ee], Peeter Vois [Peeter.Vois at mail.ee]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Basque [eu]</td>
+    <td bgcolor="#eeeeee">Piarres Beobide Egaa [pi at beobide.net]</td>
+  </tr>
+  <tr>
+    <td>Persian [fa]</td>
+    <td>Abbas Izad [abbasizad at hotmail.com]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Finnish [fi]</td>
+    <td bgcolor="#eeeeee">Jari Rahkonen [jari.rahkonen at pp1.inet.fi]</td>
+  </tr>
+  <tr>
+    <td>French [fr]</td>
+    <td>Collet Etienne [xanaxlnx at gmail.com], Olivier Fourdan [fourdan at xfce.org], Stephane Roy [sroy at j2n.net]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Gujarati [gu]</td>
+    <td bgcolor="#eeeeee">Ankitkumar Rameshchandra Patel [ankit at redhat.com]</td>
+  </tr>
+  <tr>
+    <td>Hebrew [he]</td>
+    <td>Dotan Kamber [kamberd at yahoo.com], Yuval Tanny [tanai at int.gov.il]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Hindi [hi]</td>
+    <td bgcolor="#eeeeee">Ravishankar Shrivastava [raviratlami at yahoo.com]</td>
+  </tr>
+  <tr>
+    <td>Hungarian [hu]</td>
+    <td>Szerv&Atilde;&iexcl;c Attila [sas at 321.hu]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Armenian [hy]</td>
+    <td bgcolor="#eeeeee">Norayr Chilingaryan [norik at oric.org]</td>
+  </tr>
+  <tr>
+    <td>Italian [it]</td>
+    <td>Alex Dupre [ale at freebsd.org], ;Gabriele Modena [rnd at email.it], Roberto Pariset [r.pariset at gmail.com]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Japanese [ja]</td>
+    <td bgcolor="#eeeeee">Daichi Kawahata [daichi at xfce.org]</td>
+  </tr>
+  <tr>
+    <td>Korean [ko]</td>
+    <td>ByungHyun Choi [byunghyun.choi at gmail.com], Kibum Han [yui at yui.pe.kr]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Kurdish [ku]</td>
+    <td bgcolor="#eeeeee">Erdal Ronahi [erdal.ronahi at gmail.com]</td>
+  </tr>
+  <tr>
+    <td>Lithuanian [lt]</td>
+    <td>Mantas Zapolskas [mantaz at users.sf.net], Rimas Kudelis [rq at akl.lt]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Marathi [mr]</td>
+    <td bgcolor="#eeeeee">Pallavi Nikam [pallavi_edu at rediffmail.com]</td>
+  </tr>
+  <tr>
+    <td>Malay [ms]</td>
+    <td>Harun Musa [mharun at gmx.net]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Norwegian [nb_NO]</td>
+    <td bgcolor="#eeeeee">Eskild Hustvedt [zerodogg at skolelinux.no]</td>
+  </tr>
+  <tr>
+    <td>Dutch [nl]</td>
+    <td>Jasper Huijsmans [jasper at xfce.org], Vincent Tunru [imnotb at gmail.com]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Punjabi [pa]</td>
+    <td bgcolor="#eeeeee">Jaswinder Singh Phulewala [jaswinderphulewala at yahoo.com]</td>
+  </tr>
+  <tr>
+    <td>Polish [pl]</td>
+    <td>Marek Piechut [mco at o2.pl], Piotr Mali&Aring;&bdquo;ski [admin at rk.edu.pl], Wit Wili&Aring;&bdquo;ski [wit.wilinski at gmail.com]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Portuguese [pt_BR]</td>
+    <td bgcolor="#eeeeee">Adriano Winter Bess [awbess at gmail.com], Felix da Silva Costa [felix at online.eti.br], Joao Pedrosa [joaopedrosa at gmail.com], Rodrigo Coacci [rcoacci at gmail.com], Tomas Schertel [tschertel at gmail.com]</td>
+  </tr>
+  <tr>
+    <td>Portuguese [pt_PT]</td>
+    <td>Nuno Donato [nunodonato at zmail.pt]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Romanian [ro]</td>
+    <td bgcolor="#eeeeee">Mi&Aring;&Yuml;u Moldovan [dumol at gnome.ro]</td>
+  </tr>
+  <tr>
+    <td>Russian [ru]</td>
+    <td>Andrey Fedoseev [andrey.fedoseev at gmail.com], Anthony Ivanoff [a-i at bk.ru], Anthony Ivanoff [a-i at bk.ru]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Slovak [sk]</td>
+    <td bgcolor="#eeeeee">Juraj Brosz [juro at jurajbrosz.info], Roman Moravcik [morgan at pobox.sk]</td>
+  </tr>
+  <tr>
+    <td>Swedish [sv]</td>
+    <td>Daniel Nylander [po at danielnylander.se], Magnus Berg [berg.magnus at bredband.net]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Tamil [ta]</td>
+    <td bgcolor="#eeeeee">Dinesh Nadarajah [dinesh_list at sbcglobal.net]</td>
+  </tr>
+  <tr>
+    <td>Turkish [tr]</td>
+    <td>Er&Atilde;&sect;in Eker [erc.caldera at gmx.net]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Ukrainian [uk]</td>
+    <td bgcolor="#eeeeee">Maxim V. Dziumanenko [mvd at mylinux.com.ua]</td>
+  </tr>
+  <tr>
+    <td>Vietnamese [vi]</td>
+    <td>L&Atilde;&cent;m V&Auml;&copy;nh Ni&Atilde;&ordf;n [nienvl at yahoo.ca]</td>
+  </tr>
+  <tr>
+    <td bgcolor="#eeeeee">Chinese [zh_CN]</td>
+    <td bgcolor="#eeeeee">Huang Huan [unicon221 at gmail.com], Yongtao Yang [yongtao.yang at telia.com]</td>
+  </tr>
+  <tr>
+    <td>Chinese [zh_TW]</td>
+    <td>Hydonsingore Cia [hydonsingore at mail.educities.edu.tw]</td>
+  </tr>
+</table>
