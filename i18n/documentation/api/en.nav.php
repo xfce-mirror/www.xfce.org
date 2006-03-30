abdeb4e140c8b4ad506a6b940dbc1faafd1ad2d3
Nick Schermer <nick@xfce.org>
2006-03-30 18:28:49 +0000

    * Import of all API documentation
    (Old svn revision: 219)

---
+++ i18n/documentation/api/en.nav.php
@@ -0,0 +1,3 @@
+<?php
+	include "i18n/documentation/en.nav.php";
+?>
---
+++ i18n/documentation/api/index.en.php
@@ -0,0 +1,21 @@
+<h1>Developers Documentation</h1>
+<p></p>
+
+<h3><a href="/i18n/documentation/api/libxfce4util/index.html">libxfce4util</a></h3>
+<p>This component is the basic utilitiy library for the Xfce Desktop Environment. Everything else is build on top of this library and the functionality provided by it.</p>
+
+<h3><a href="/i18n/documentation/api/libxfce4mcs/index.html">libxfce4mcs</a></h3>
+<p>This package contains the modules used in the Xfce Multi-Channel Settings (MCS) system. The manual describes both the manager-side API - used when creating plugins for the Xfce MCS Manager - and the client-side API - used in creation of clients that access their settings using the MCS system.</p>
+
+<h3><a href="/i18n/documentation/api/libxfcegui4/index.html">libxfcegui4</a></h3>
+<p>The basic widget library for the Xfce Desktop Environment, it contains additional widgets, that extend the basic widget set provided by Gtk+, functions to communicate with other NET compliant Xclients and various utility functions and modules.</p>
+
+<h3><a href="/i18n/documentation/api/libexo/index.html">libexo</a></h3>
+<p>Libexo is an extension library for Xfce, developed by <a href="http://www.os-cillation.com/" target="_blank">os-cillation</a>. While the core Xfce libraries are targeted at desktop development, libexo is targeted at application developement - in particular, developing applications for the Xfce Desktop Environment.</p>
+
+<h3><a href="/i18n/documentation/api/libxfce4panel/index.html">libxfce4panel</a></h3>
+<p>Documentation for the new panel framework.</p>
+<h3><a href="/i18n/documentation/api/thunar-vfs/index.html">thunar-vfs</a></h3>
+<p> Thunar-vfs is a filesystem abstraction library allowing applications        plugable transparent access to a variety of &quot;real&quot; filesystems.</p>
+<h3><a href="/i18n/documentation/api/thunarx/index.html">thunarx</a></h3>
+<p> The Thunar Extension Framework (thunarx) provides        developers with an easy way to extend the basic functionality provided by        the <a href="http://thunar.xfce.org/" target="_top">Thunar File Manager</a>.        The thunarx library includes interfaces that can        be implemented by extensions for various purposes.</p>
