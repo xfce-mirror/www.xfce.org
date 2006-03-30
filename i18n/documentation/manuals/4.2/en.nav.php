c0ebe3fd2c94f5dc3bcd7b8a5f94e0b61ce26a0f
Auke Kok <sofar@foo-projects.org>
2006-03-30 22:58:34 +0000

    relative urls
    (Old svn revision: 224)

--- i18n/documentation/manuals/4.2/en.nav.php
+++ i18n/documentation/manuals/4.2/en.nav.php
@@ -1,27 +1,27 @@
 <p>User Guide</p>
 
 <ul>
-    <li><a href="/documentation/manuals/4.2/xfce4-user-guide#preface">Preface</a></li>
-    <li><a href="/documentation/manuals/4.2/xfce4-intro">Introduction</a></li>
-    <li><a href="/documentation/manuals/4.2/xfce4-use">Using Xfce 4</a></li>
-    <li><a href="/documentation/manuals/4.2/xfce4-components">Components of Xfce 4</a></li>
-    <li><a href="/documentation/manuals/4.2/xfce4-install">Installing Xfce 4</a></li>
-    <li><a href="/documentation/manuals/4.2/xfce4-copyright">Copyright and Licenses</a></li>
+    <li><a href="xfce4-user-guide#preface">Preface</a></li>
+    <li><a href="xfce4-intro">Introduction</a></li>
+    <li><a href="xfce4-use">Using Xfce 4</a></li>
+    <li><a href="xfce4-components">Components of Xfce 4</a></li>
+    <li><a href="xfce4-install">Installing Xfce 4</a></li>
+    <li><a href="xfce4-copyright">Copyright and Licenses</a></li>
 </ul>
 
 <p>User Manuals</p>
 
 <ul>
-    <li><a href="/documentation/manuals/4.2/xfce4-panel">Panel</a></li>
-    <li><a href="/documentation/manuals/4.2/xfwm4">Window Manager</a></li>
-    <li><a href="/documentation/manuals/4.2/xfce4-session">Session Manager</a></li>
-    <li><a href="/documentation/manuals/4.2/xfdesktop">Desktop Manager</a></li>
-    <li><a href="/documentation/manuals/4.2/xfce-utils">Utilities and Scripts</a></li>
-    <li><a href="/documentation/manuals/4.2/xfprint">Printing Management</a></li>
-    <li><a href="/documentation/manuals/4.2/xfce-mcs-manager">Settings Manager</a></li>
-    <li><a href="/documentation/manuals/4.2/xfce-mcs-plugins">Settings Manager Plugins</a></li>
-    <li><a href="/documentation/manuals/4.2/xffm">Filemanager</a></li>
-    <li><a href="/documentation/manuals/4.2/xfce4-appfinder">Application Finder</a></li>
+    <li><a href="xfce4-panel">Panel</a></li>
+    <li><a href="xfwm4">Window Manager</a></li>
+    <li><a href="xfce4-session">Session Manager</a></li>
+    <li><a href="xfdesktop">Desktop Manager</a></li>
+    <li><a href="xfce-utils">Utilities and Scripts</a></li>
+    <li><a href="xfprint">Printing Management</a></li>
+    <li><a href="xfce-mcs-manager">Settings Manager</a></li>
+    <li><a href="xfce-mcs-plugins">Settings Manager Plugins</a></li>
+    <li><a href="xffm">Filemanager</a></li>
+    <li><a href="xfce4-appfinder">Application Finder</a></li>
 </ul>
 
 <p><a href="../../api/">API Documentation</a></p>
