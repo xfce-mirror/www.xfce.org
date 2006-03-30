dc5193cdd3659e5acf4c3ce94042e44a1f649978
Auke Kok <sofar@foo-projects.org>
2006-03-30 23:23:50 +0000

    Fixes.
    (Old svn revision: 227)

--- i18n/documentation/en.nav.php
+++ i18n/documentation/en.nav.php
@@ -1,13 +1,13 @@
 <p><a href="/documentation">Documentation</a></p>
 <ul>
-	<li><a href="manuals/4.2/xfce4-user-guide">Xfce User Guide</a>
-	<li><a href="manuals/4.2/">Manuals</a>
+	<li><a href="/documentation/manuals/4.2/xfce4-user-guide">Xfce User Guide</a>
+	<li><a href="/documentation/manuals/4.2/">Manuals</a>
 </ul>
 
 
 <p>FAQ's</p>
 <ul>
-	<li><a href="faq/">Xfce FAQ</a></li>
+	<li><a href="/documentation/faq/">Xfce FAQ</a></li>
 	<li><a href="http://xfce.wikicities.com/wiki/Frequently_Asked_Questions" target="_blank" class="external">Wiki FAQ</a></li>
 </ul>
 
