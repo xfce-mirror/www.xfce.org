e8664f89a0f13e54d11d11575a49f43ebbbddfb5
Auke Kok <sofar@foo-projects.org>
2006-03-30 22:43:02 +0000

    New layout/nav
    (Old svn revision: 222)

--- i18n/documentation/manuals/4.2/en.nav.php
+++ i18n/documentation/manuals/4.2/en.nav.php
@@ -1,4 +1,15 @@
-<h2>Manuals</h2>
+<h2>User Guide</h2>
+
+<ul>
+    <li><a href="/documentation/manuals/4.2/xfce4-user-guide#preface">Preface</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce4-intro">Introduction</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce4-use">Using Xfce 4</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce4-components">Components of Xfce 4</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce4-install">Installing Xfce 4</a></li>
+    <li><a href="/documentation/manuals/4.2/xfce4-copyright">Copyright and Licenses</a></li>
+</ul>
+
+<h2>User Manuals</h2>
 
 <ul>
     <li><a href="/documentation/manuals/4.2/xfce4-panel">Panel</a></li>
@@ -12,3 +23,5 @@
     <li><a href="/documentation/manuals/4.2/xffm">Filemanager</a></li>
     <li><a href="/documentation/manuals/4.2/xfce4-appfinder">Application Finder</a></li>
 </ul>
+
+<h2><a href="../../api/">API Documentation</a></h2>
