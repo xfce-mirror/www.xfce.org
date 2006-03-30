895e4000b356387d84d34cc2c460d24155546bef
Auke Kok <sofar@foo-projects.org>
2006-03-30 22:36:44 +0000

    Remove stuff that's unneeded.
    (Old svn revision: 221)

--- i18n/documentation/manuals/4.2/xfce4-user-guide.en.php
+++ i18n/documentation/manuals/4.2/xfce4-user-guide.en.php
@@ -1,21 +1,4 @@
 
-  <table width="100%" summary="Navigation header">
-    <tr>
-      <th colspan="3" align="center">The Xfce 4 Desktop
-      Environment</th>
-    </tr>
-
-    <tr>
-      <td width="20%" align="left">�</td>
-
-      <th width="60%" align="center">�</th>
-
-      <td width="20%" align="right">�<a accesskey="n" href=
-      "xfce4-intro.html">Next</a></td>
-    </tr>
-  </table>
-  <hr>
-
   <h1><a name="xfce4-user-guide" id="xfce4-user-guide"></a> The
   Xfce 4 Desktop Environment</h1>
 
@@ -32,73 +15,73 @@
   <p><tt>&lt;<a href=
   "mailto:benny@xfce.org">benny@xfce.org</a>&gt;</tt></p>
 
-  <p>Copyright � 2004 Jasper Huijsmans</p>
+  <p>Copyright &copy; 2004 Jasper Huijsmans</p>
   <hr>
 
   <p><b>Table of Contents</b></p>
 
   <dl>
-    <dt><a href="xfce4-user-guide.html#preface">Preface</a></dt>
+    <dt><a href="xfce4-user-guide#preface">Preface</a></dt>
 
-    <dt><a href="xfce4-intro.html">Introduction</a></dt>
+    <dt><a href="xfce4-intro">Introduction</a></dt>
 
     <dd>
       <dl>
         <dt><a href=
-        "xfce4-intro.html#xfce4-resources">Resources</a></dt>
+        "xfce4-intro#xfce4-resources">Resources</a></dt>
       </dl>
     </dd>
 
-    <dt><a href="xfce4-use.html">Using Xfce4</a></dt>
+    <dt><a href="xfce4-use">Using Xfce4</a></dt>
 
     <dd>
       <dl>
-        <dt><a href="xfce4-use.html#xfce4-getting-started">Getting
+        <dt><a href="xfce4-use#xfce4-getting-started">Getting
         started</a></dt>
 
-        <dt><a href="xfce4-use.html#id2533342">The Default
+        <dt><a href="xfce4-use#id2533342">The Default
         Desktop</a></dt>
 
-        <dt><a href="xfce4-use.html#id2533998">Common
+        <dt><a href="xfce4-use#id2533998">Common
         Tasks</a></dt>
 
-        <dt><a href="xfce4-use.html#id2534182">Further
+        <dt><a href="xfce4-use#id2534182">Further
         reading</a></dt>
       </dl>
     </dd>
 
-    <dt><a href="xfce4-components.html">Components of Xfce
+    <dt><a href="xfce4-components">Components of Xfce
     4</a></dt>
 
-    <dt><a href="xfce4-install.html">Installing Xfce4</a></dt>
+    <dt><a href="xfce4-install">Installing Xfce4</a></dt>
 
     <dd>
       <dl>
-        <dt><a href="xfce4-install.html#id2534533">Binary
+        <dt><a href="xfce4-install#id2534533">Binary
         packages</a></dt>
 
-        <dt><a href="xfce4-install.html#id2534968">Graphical
+        <dt><a href="xfce4-install#id2534968">Graphical
         installers</a></dt>
 
-        <dt><a href="xfce4-install.html#id2534990">Building from
+        <dt><a href="xfce4-install#id2534990">Building from
         source</a></dt>
 
-        <dt><a href="xfce4-install.html#id2535072">Using
+        <dt><a href="xfce4-install#id2535072">Using
         CVS</a></dt>
 
-        <dt><a href="xfce4-install.html#id2596877">Additional
+        <dt><a href="xfce4-install#id2596877">Additional
         software</a></dt>
       </dl>
     </dd>
 
-    <dt><a href="xfce4-copyright.html">Copyright and
+    <dt><a href="xfce4-copyright">Copyright and
     Licences</a></dt>
 
-    <dt><a href="BSD.html">BSD License</a></dt>
+    <dt><a href="BSD">BSD License</a></dt>
 
-    <dt><a href="GPL.html">GNU General Public License</a></dt>
+    <dt><a href="GPL">GNU General Public License</a></dt>
 
-    <dt><a href="LGPL.html">GNU Library General Public
+    <dt><a href="LGPL">GNU Library General Public
     License</a></dt>
   </dl>
 
@@ -115,21 +98,3 @@
   "_top">this location</a>.</p>
   <hr>
 
-  <table width="100%" summary="Navigation footer">
-    <tr>
-      <td width="40%" align="left">�</td>
-
-      <td width="20%" align="center">�</td>
-
-      <td width="40%" align="right">�<a accesskey="n" href=
-      "xfce4-intro.html">Next</a></td>
-    </tr>
-
-    <tr>
-      <td width="40%" align="left" valign="top">�</td>
-
-      <td width="20%" align="center">�</td>
-
-      <td width="40%" align="right" valign="top">�Introduction</td>
-    </tr>
-  </table>
