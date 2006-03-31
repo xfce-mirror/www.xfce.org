672d9207c85e7572a397b9a7be1c77f9fb6e8fd9
Nick Schermer <nick@xfce.org>
2006-03-31 16:58:13 +0000

    * Couple of changes suggested by Sofar
    (Old svn revision: 232)

--- i18n/documentation/4.2/en.nav.php
+++ i18n/documentation/4.2/en.nav.php
@@ -1,10 +1,14 @@
 <ul>
-    <li><a href="/documentation/4.2/#preface">Preface</a></li>
-    <li><a href="xfce4-intro">Introduction</a></li>
-    <li><a href="xfce4-use">Using Xfce 4</a></li>
-    <li><a href="xfce4-components">Components of Xfce 4</a></li>
-    <li><a href="xfce4-install">Installing Xfce 4</a></li>
-    <li><a href="xfce4-copyright">Copyright and Licenses</a></li>
+    <li><a href="/documentation/4.2/">Xfce 4.2 Users Guide</a>
+		<ul>
+			<li><a href="/documentation/4.2/#preface">Preface</a></li>
+			<li><a href="xfce4-intro">Introduction</a></li>
+			<li><a href="xfce4-use">Using Xfce 4</a></li>
+			<li><a href="xfce4-components">Components of Xfce 4</a></li>
+			<li><a href="xfce4-install">Installing Xfce 4</a></li>
+			<li><a href="xfce4-copyright">Copyright and Licenses</a></li>
+		</ul>
+	</li>
 </ul>
 <hr />
 <?php
--- i18n/documentation/index.en.php
+++ i18n/documentation/index.en.php
@@ -1,50 +1,53 @@
 <h1>Documentation</h1>
 <p>This page provides most of the Xfce documenation for both users and developers.</p>
-<h2><a href="/documentation/4.2/">4.2 Users Guide</a></h2>
-<ul>
-    <li><a href="/documentation/4.2/#preface">Preface</a></li>
-    <li><a href="/documentation/4.2/xfce4-intro">Introduction</a></li>
-    <li><a href="/documentation/4.2/xfce4-use">Using Xfce 4</a></li>
-    <li><a href="/documentation/4.2/xfce4-components">Components of Xfce 4</a></li>
-    <li><a href="/documentation/4.2/xfce4-install">Installing Xfce 4</a></li>
-    <li><a href="/documentation/4.2/xfce4-copyright">Copyright and Licenses</a></li>
-</ul>
-
-<h2><a href="/documentation/manuals/">Application Manuals</a></h2>
-<ul>
-    <li><a href="/documentation/manuals/xfce4-panel">Panel</a></li>
-    <li><a href="/documentation/manuals/xfwm4">Window Manager</a></li>
-    <li><a href="/documentation/manuals/xfce4-session">Session Manager</a></li>
-    <li><a href="/documentation/manuals/xfdesktop">Desktop Manager</a></li>
-    <li><a href="/documentation/manuals/xfce-utils">Utilities and Scripts</a></li>
-    <li><a href="/documentation/manuals/xfprint">Printing Management</a></li>
-    <li><a href="/documentation/manuals/xfce-mcs-manager">Settings Manager</a></li>
-    <li><a href="/documentation/manuals/xfce-mcs-plugins">Settings Manager Plugins</a></li>
-    <li><a href="/documentation/manuals/xffm">Filemanager</a></li>
-    <li><a href="/documentation/manuals/xfce4-appfinder">Application Finder</a></li>
-</ul>
-
-<h2><a href="/documentation/api/">Developers (API)</a></h2>
-<ul>
-	<li><a href="/documentation/api/libxfce4util/index.html">libxfce4util</a>.</li>
-	<li><a href="/documentation/api/libxfce4mcs/index.html">libxfce4mcs</a></li>
-	<li><a href="/documentation/api/libxfcegui4/index.html">libxfcegui4</a></li>
-	<li><a href="/documentation/api/libexo/index.html">libexo</a></li>
-	<li><a href="/documentation/api/libxfce4panel/index.html">libxfce4panel</a>.</li>
-	<li><a href="/documentation/api/thunar-vfs/index.html">thunar-vfs</a></li>
-	<li><a href="/documentation/api/thunarx/index.html">thunarx</a></li>
-</ul>
-
-<h2><a href="documentation/faq/">FAQs</a></h2>
-<ul>
-	<li><a href="/documentation/faq/#starting">Getting started</a></li>
-	<li><a href="/documentation/faq/#installation">Installation</a></li>
-	<li><a href="/documentation/faq/#usage">Usage</a></li>
-	<li><a href="/documentation/faq/#looks">Visual Look</a></li>
-	<li><a href="/documentation/faq/#development">Development</a></li>
-</ul>
-
-<h2><a href="/documentation/requirements">Requirements</a></h2>
-<p>A detailed list of all Xfce dependencies. </p>
-<h2><a href="/documentation/notes">Release Notes     </a></h2>
-<p>All Xfce 4.2 and 4.4 release notes, ordered by version. </p>
+<table width="100%">
+  <tr>
+    <td width="50%" valign="top">
+	<h2><a href="/documentation/4.2/">4.2 Users Guide</a></h2>
+      <ul>
+        <li><a href="/documentation/4.2/#preface">Preface</a></li>
+        <li><a href="/documentation/4.2/xfce4-intro">Introduction</a></li>
+        <li><a href="/documentation/4.2/xfce4-use">Using Xfce 4</a></li>
+        <li><a href="/documentation/4.2/xfce4-components">Components of Xfce 4</a></li>
+        <li><a href="/documentation/4.2/xfce4-install">Installing Xfce 4</a></li>
+        <li><a href="/documentation/4.2/xfce4-copyright">Copyright and Licenses</a></li>
+    </ul>
+      <h2><a href="/documentation/api/">Developers (API)</a></h2>
+      <ul>
+        <li><a href="/documentation/api/libxfce4util/index.html">libxfce4util</a>.</li>
+        <li><a href="/documentation/api/libxfce4mcs/index.html">libxfce4mcs</a></li>
+        <li><a href="/documentation/api/libxfcegui4/index.html">libxfcegui4</a></li>
+        <li><a href="/documentation/api/libexo/index.html">libexo</a></li>
+        <li><a href="/documentation/api/libxfce4panel/index.html">libxfce4panel</a>.</li>
+        <li><a href="/documentation/api/thunar-vfs/index.html">thunar-vfs</a></li>
+        <li><a href="/documentation/api/thunarx/index.html">thunarx</a></li>
+      </ul>
+      <h2><a href="/documentation/requirements">Requirements</a></h2>
+    <p>A detailed list of all Xfce dependencies.</p>      
+    </td>
+    <td width="50%" valign="top"><h2><a href="/documentation/manuals/">Application Manuals</a></h2>
+      <ul>
+        <li><a href="/documentation/manuals/xfce4-panel">Panel</a></li>
+        <li><a href="/documentation/manuals/xfwm4">Window Manager</a></li>
+        <li><a href="/documentation/manuals/xfce4-session">Session Manager</a></li>
+        <li><a href="/documentation/manuals/xfdesktop">Desktop Manager</a></li>
+        <li><a href="/documentation/manuals/xfce-utils">Utilities and Scripts</a></li>
+        <li><a href="/documentation/manuals/xfprint">Printing Management</a></li>
+        <li><a href="/documentation/manuals/xfce-mcs-manager">Settings Manager</a></li>
+        <li><a href="/documentation/manuals/xfce-mcs-plugins">Settings Manager Plugins</a></li>
+        <li><a href="/documentation/manuals/xffm">Filemanager</a></li>
+        <li><a href="/documentation/manuals/xfce4-appfinder">Application Finder</a></li>
+    </ul>
+      <h2><a href="documentation/faq/">FAQs</a></h2>
+      <ul>
+        <li><a href="/documentation/faq/#starting">Getting started</a></li>
+        <li><a href="/documentation/faq/#installation">Installation</a></li>
+        <li><a href="/documentation/faq/#usage">Usage</a></li>
+        <li><a href="/documentation/faq/#looks">Visual Look</a></li>
+        <li><a href="/documentation/faq/#development">Development</a></li>
+      </ul>
+      <h2><a href="/documentation/notes">Release Notes </a></h2>
+    <p>All Xfce 4.2 and 4.4 release notes, ordered by version.</p>      
+    </td>
+  </tr>
+</table>
