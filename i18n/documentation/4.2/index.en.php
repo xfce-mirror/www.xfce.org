34d03544103e9c669e8edfe76589bda7160da382
Auke Kok <sofar@foo-projects.org>
2006-04-01 06:56:44 +0000

    quick index
    (Old svn revision: 270)

---
+++ i18n/documentation/4.2/index.en.php
@@ -0,0 +1,35 @@
+
+  <h1>Xfce 4.2 User Guide</h1>
+  <ul>
+    <li><a href="userguide/#preface">Preface</a></li>
+    <li><a href="userguide/xfce4-intro">Introduction</a></li>
+    <li><a href="userguide/xfce4-use">Using Xfce4</a></li>
+    <li><a href="userguide/xfce4-components">Components of Xfce</a></li>
+    <li><a href="userguide/xfce4-install">Installing Xfce4</a></li>
+    <li><a href="userguide/xfce4-copyright">Copyright and License</a></li>
+  </ul>
+
+  <h1>Xfce 4.2 Application Manuals</h1>
+  <ul>
+    <li><a href="manuals/xfce4-panel">Panel</a></li>
+    <li><a href="manuals/xfwm4">Window Manager</a></li>
+    <li><a href="manuals/xfce4-session">Session Manager</a></li>
+    <li><a href="manuals/xfdesktop">Desktop Manager</a></li>
+    <li><a href="manuals/xfce-utils">Utilities and Scripts</a></li>
+    <li><a href="manuals/xfprint">Printing Management</a></li>
+    <li><a href="manuals/xfce-mcs-manager">Settings Manager</a></li>
+    <li><a href="manuals/xfce-mcs-plugins">Settings Manager Plugins</a></li>
+    <li><a href="manuals/xffm">Filemanager</a></li>
+    <li><a href="manuals/xfce4-appfinder">Application Finder</a></li>
+  </ul>
+
+  <h1>Developer Documentation</h1>
+  <ul>
+    <li><a href="api/libxfce4util/">libxfce4util</a></li>
+    <li><a href="api/libxfce4mcs/">libxfce4mcs</a></li>
+    <li><a href="api/libxfcegui4/">libxfcegui4</a></li>
+    <li><a href="api/libexo/">libexo</a></li>
+    <li><a href="api/libxfce4panel/">libxfce4panel</a></li>
+    <li><a href="api/thunar-vfs/">thunar-vfs</a></li>
+    <li><a href="api/thunarx/">thunarx</a></li>
+  </ul>
