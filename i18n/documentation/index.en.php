69364483339ed28e47c0c0c63c1813c64aa2cdd2
Auke Kok <sofar@foo-projects.org>
2006-04-01 04:31:07 +0000

    Fixing all links.
    (Old svn revision: 243)

--- i18n/documentation/index.en.php
+++ i18n/documentation/index.en.php
@@ -3,51 +3,51 @@
 <table width="100%">
   <tr>
     <td width="50%" valign="top">
-	<h2><a href="/documentation/4.2/">4.2 Users Guide</a></h2>
+      <h2><a href="/documentation/4.2/userguide/">4.2 Users Guide</a></h2>
       <ul>
-        <li><a href="/documentation/4.2/#preface">Preface</a></li>
-        <li><a href="/documentation/4.2/xfce4-intro">Introduction</a></li>
-        <li><a href="/documentation/4.2/xfce4-use">Using Xfce 4</a></li>
-        <li><a href="/documentation/4.2/xfce4-components">Components of Xfce 4</a></li>
-        <li><a href="/documentation/4.2/xfce4-install">Installing Xfce 4</a></li>
-        <li><a href="/documentation/4.2/xfce4-copyright">Copyright and Licenses</a></li>
-    </ul>
-      <h2><a href="/documentation/api/">Developers (API)</a></h2>
+        <li><a href="/documentation/4.2/userguide/#preface">Preface</a></li>
+        <li><a href="/documentation/4.2/userguide/xfce4-intro">Introduction</a></li>
+        <li><a href="/documentation/4.2/userguide/xfce4-use">Using Xfce 4</a></li>
+        <li><a href="/documentation/4.2/userguide/xfce4-components">Components of Xfce 4</a></li>
+        <li><a href="/documentation/4.2/userguide/xfce4-install">Installing Xfce 4</a></li>
+        <li><a href="/documentation/4.2/userguide/xfce4-copyright">Copyright and Licenses</a></li>
+      </ul>
+      <h2><a href="/documentation/4.2/api/">Developers (API)</a></h2>
       <ul>
-        <li><a href="/documentation/api/libxfce4util/index.html">libxfce4util</a>.</li>
-        <li><a href="/documentation/api/libxfce4mcs/index.html">libxfce4mcs</a></li>
-        <li><a href="/documentation/api/libxfcegui4/index.html">libxfcegui4</a></li>
-        <li><a href="/documentation/api/libexo/index.html">libexo</a></li>
-        <li><a href="/documentation/api/libxfce4panel/index.html">libxfce4panel</a>.</li>
-        <li><a href="/documentation/api/thunar-vfs/index.html">thunar-vfs</a></li>
-        <li><a href="/documentation/api/thunarx/index.html">thunarx</a></li>
+        <li><a href="/documentation/4.2/api/libxfce4util/index.html">libxfce4util</a>.</li>
+        <li><a href="/documentation/4.2/api/libxfce4mcs/index.html">libxfce4mcs</a></li>
+        <li><a href="/documentation/4.2/api/libxfcegui4/index.html">libxfcegui4</a></li>
+        <li><a href="/documentation/4.2/api/libexo/index.html">libexo</a></li>
+        <li><a href="/documentation/4.2/api/libxfce4panel/index.html">libxfce4panel</a>.</li>
+        <li><a href="/documentation/4.2/api/thunar-vfs/index.html">thunar-vfs</a></li>
+        <li><a href="/documentation/4.2/api/thunarx/index.html">thunarx</a></li>
       </ul>
       <h2><a href="/documentation/requirements">Requirements</a></h2>
-    <p>A detailed list of all Xfce dependencies.</p>      
+      <p>A detailed list of all Xfce dependencies.</p>
     </td>
-    <td width="50%" valign="top"><h2><a href="/documentation/manuals/">Application Manuals</a></h2>
+    <td width="50%" valign="top"><h2><a href="/documentation/4.2/manuals/">Application Manuals</a></h2>
       <ul>
-        <li><a href="/documentation/manuals/xfce4-panel">Panel</a></li>
-        <li><a href="/documentation/manuals/xfwm4">Window Manager</a></li>
-        <li><a href="/documentation/manuals/xfce4-session">Session Manager</a></li>
-        <li><a href="/documentation/manuals/xfdesktop">Desktop Manager</a></li>
-        <li><a href="/documentation/manuals/xfce-utils">Utilities and Scripts</a></li>
-        <li><a href="/documentation/manuals/xfprint">Printing Management</a></li>
-        <li><a href="/documentation/manuals/xfce-mcs-manager">Settings Manager</a></li>
-        <li><a href="/documentation/manuals/xfce-mcs-plugins">Settings Manager Plugins</a></li>
-        <li><a href="/documentation/manuals/xffm">Filemanager</a></li>
-        <li><a href="/documentation/manuals/xfce4-appfinder">Application Finder</a></li>
+        <li><a href="/documentation/4.2/manuals/xfce4-panel">Panel</a></li>
+        <li><a href="/documentation/4.2/manuals/xfwm4">Window Manager</a></li>
+        <li><a href="/documentation/4.2/manuals/xfce4-session">Session Manager</a></li>
+        <li><a href="/documentation/4.2/manuals/xfdesktop">Desktop Manager</a></li>
+        <li><a href="/documentation/4.2/manuals/xfce-utils">Utilities and Scripts</a></li>
+        <li><a href="/documentation/4.2/manuals/xfprint">Printing Management</a></li>
+        <li><a href="/documentation/4.2/manuals/xfce-mcs-manager">Settings Manager</a></li>
+        <li><a href="/documentation/4.2/manuals/xfce-mcs-plugins">Settings Manager Plugins</a></li>
+        <li><a href="/documentation/4.2/manuals/xffm">Filemanager</a></li>
+        <li><a href="/documentation/4.2/manuals/xfce4-appfinder">Application Finder</a></li>
     </ul>
-      <h2><a href="documentation/faq/">FAQs</a></h2>
+      <h2><a href="documentation/faq">FAQs</a></h2>
       <ul>
-        <li><a href="/documentation/faq/#starting">Getting started</a></li>
-        <li><a href="/documentation/faq/#installation">Installation</a></li>
-        <li><a href="/documentation/faq/#usage">Usage</a></li>
-        <li><a href="/documentation/faq/#looks">Visual Look</a></li>
-        <li><a href="/documentation/faq/#development">Development</a></li>
+        <li><a href="/documentation/faq#starting">Getting started</a></li>
+        <li><a href="/documentation/faq#installation">Installation</a></li>
+        <li><a href="/documentation/faq#usage">Usage</a></li>
+        <li><a href="/documentation/faq#looks">Visual Look</a></li>
+        <li><a href="/documentation/faq#development">Development</a></li>
       </ul>
-      <h2><a href="/documentation/notes">Release Notes </a></h2>
-    <p>All Xfce 4.2 and 4.4 release notes, ordered by version.</p>      
+      <h2><a href="/documentation/notes/">Release Notes </a></h2>
+    <p>All Xfce 4.2 and 4.4 release notes, ordered by version.</p>
     </td>
   </tr>
 </table>
