3cdde1bd09c9a2f988cf85dad12d4ed2277accbe
Auke Kok <sofar@foo-projects.org>
2006-04-01 06:37:08 +0000

    fixes
    (Old svn revision: 266)

--- i18n/documentation/4.2/userguide/en.nav.php
+++ i18n/documentation/4.2/userguide/en.nav.php
@@ -1,14 +1,11 @@
+<h2>User Guide</h2>
 <ul>
-    <li><a href="/documentation/4.2/">Xfce 4.2 Users Guide</a>
-		<ul>
-			<li><a href="/documentation/4.2/#preface">Preface</a></li>
-			<li><a href="xfce4-intro">Introduction</a></li>
-			<li><a href="xfce4-use">Using Xfce 4</a></li>
-			<li><a href="xfce4-components">Components of Xfce 4</a></li>
-			<li><a href="xfce4-install">Installing Xfce 4</a></li>
-			<li><a href="xfce4-copyright">Copyright and Licenses</a></li>
-		</ul>
-	</li>
+	<li><a href="../userguide/#preface">Preface</a></li>
+	<li><a href="xfce4-intro">Introduction</a></li>
+	<li><a href="xfce4-use">Using Xfce 4</a></li>
+	<li><a href="xfce4-components">Components of Xfce 4</a></li>
+	<li><a href="xfce4-install">Installing Xfce 4</a></li>
+	<li><a href="xfce4-copyright">Copyright and Licenses</a></li>
 </ul>
 <hr />
 <?php
