2125adf34b73e4b04673741f8d37a8369390b3da
Auke Kok <sofar@foo-projects.org>
2006-04-01 07:19:11 +0000

    more nav.
    (Old svn revision: 278)

--- i18n/documentation/4.2/api/en.nav.php
+++ i18n/documentation/4.2/api/en.nav.php
@@ -1,3 +1,14 @@
+
+  <h2><a href="../api/">Developer Documentation</a></h2>
+  <ul>
+    <li><a href="../api/libxfce4util/">libxfce4util</a></li>
+    <li><a href="../api/libxfce4mcs/">libxfce4mcs</a></li>
+    <li><a href="../api/libxfcegui4/">libxfcegui4</a></li>
+    <li><a href="../api/libexo/">libexo</a></li>
+    <li><a href="../api/libxfce4panel/">libxfce4panel</a></li>
+    <li><a href="../api/thunar-vfs/">thunar-vfs</a></li>
+    <li><a href="../api/thunarx/">thunarx</a></li>
+  </ul>
 <?php
 	include "i18n/documentation/en.nav.php";
 ?>
--- i18n/documentation/4.2/userguide/en.nav.php
+++ i18n/documentation/4.2/userguide/en.nav.php
@@ -1,13 +1,14 @@
-<h2>User Guide</h2>
-<ul>
-	<li><a href="../userguide/#preface">Preface</a></li>
-	<li><a href="xfce4-intro">Introduction</a></li>
-	<li><a href="xfce4-use">Using Xfce 4</a></li>
-	<li><a href="xfce4-components">Components of Xfce 4</a></li>
-	<li><a href="xfce4-install">Installing Xfce 4</a></li>
-	<li><a href="xfce4-copyright">Copyright and Licenses</a></li>
-</ul>
-<hr />
+
+  <h2><a href="../userguide/">User Guide</a></h2>
+  <ul>
+    <li><a href="../userguide/#preface">Preface</a></li>
+    <li><a href="../userguide/xfce4-intro">Introduction</a></li>
+    <li><a href="../userguide/xfce4-use">Using Xfce4</a></li>
+    <li><a href="../userguide/xfce4-components">Components of Xfce</a></li>
+    <li><a href="../userguide/xfce4-install">Installing Xfce4</a></li>
+    <li><a href="../userguide/xfce4-copyright">Copyright and License</a></li>
+  </ul>
+
 <?php
 	include "i18n/documentation/en.nav.php";
 ?>
