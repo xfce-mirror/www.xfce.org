85e115aa3fa95b8492d71eabb9b0094e9325b328
Auke Kok <sofar@foo-projects.org>
2006-04-01 05:18:14 +0000

    nav
    (Old svn revision: 256)

--- i18n/documentation/4.0/en.nav.php
+++ i18n/documentation/4.0/en.nav.php
@@ -1,18 +1,20 @@
-<h2><a href="/documentation/4.0/userguide/">User Guide</a></h2>
 <ul>
-  <li><a href="/documentation/4.0/userguide/#preface">Preface</a></li>
-  <li><a href="/documentation/4.0/userguide/xfce4-intro">Introduction</a></li>
-  <li><a href="/documentation/4.0/userguide/xfce4-use">Using Xfce 4</a></li>
-  <li><a href="/documentation/4.0/userguide/xfce4-install">Installing Xfce 4</a></li>
-  <li><a href="/documentation/4.0/userguide/xfce4-copyright">Copyright and Licenses</a></li>
-</ul>
-<h2><a href="/documentation/4.0/manuals/">Application Manuals</a></h2>
-<ul>
-  <li><a href="/documentation/4.0/manuals/xfce4-panel">Panel</a></li>
-  <li><a href="/documentation/4.0/manuals/xfwm4">Window Manager</a></li>
-  <li><a href="/documentation/4.0/manuals/xfdesktop">Desktop Manager</a></li>
-  <li><a href="/documentation/4.0/manuals/xfce-utils">Utilities and Scripts</a></li>
-  <li><a href="/documentation/4.0/manuals/xfce-mcs-manager">Settings Manager</a></li>
-  <li><a href="/documentation/4.0/manuals/xfce-mcs-plugins">Settings Manager Plugins</a></li>
-  <li><a href="/documentation/4.0/manuals/xffm">Filemanager</a></li>
+  <li><a href="/documentation/4.0/userguide/">User Guide</a>
+  <ul>
+    <li><a href="/documentation/4.0/userguide/#preface">Preface</a></li>
+    <li><a href="/documentation/4.0/userguide/xfce4-intro">Introduction</a></li>
+    <li><a href="/documentation/4.0/userguide/xfce4-use">Using Xfce 4</a></li>
+    <li><a href="/documentation/4.0/userguide/xfce4-install">Installing Xfce 4</a></li>
+    <li><a href="/documentation/4.0/userguide/xfce4-copyright">Copyright and Licenses</a></li>
+  </ul>
+  <li><a href="/documentation/4.0/manuals/">Manuals</a></li>
+  <ul>
+    <li><a href="/documentation/4.0/manuals/xfce4-panel">Panel</a></li>
+    <li><a href="/documentation/4.0/manuals/xfwm4">Window Manager</a></li>
+    <li><a href="/documentation/4.0/manuals/xfdesktop">Desktop Manager</a></li>
+    <li><a href="/documentation/4.0/manuals/xfce-utils">Utilities and Scripts</a></li>
+    <li><a href="/documentation/4.0/manuals/xfce-mcs-manager">Settings Manager</a></li>
+    <li><a href="/documentation/4.0/manuals/xfce-mcs-plugins">Settings Manager Plugins</a></li>
+    <li><a href="/documentation/4.0/manuals/xffm">Filemanager</a></li>
+  </ul>
 </ul>
