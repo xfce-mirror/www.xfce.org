25b8bf146eb94549a63e6368ddceeae68722f899
Auke Kok <sofar@foo-projects.org>
2006-04-01 07:04:38 +0000

    only put major titles in here for now...
    (Old svn revision: 273)

--- i18n/arrays/en.breadcrumbs.php
+++ i18n/arrays/en.breadcrumbs.php
@@ -12,28 +12,13 @@ $breadcrumbs = array (
 	"/documentation/notes/"				=> "Release Notes",
 	
 	"/documentation/4.2/"				=> "4.2 Documentation",
-	"/documentation/4.2/userguide/"			=> "User Guides",
+	"/documentation/4.2/userguide/"			=> "User Guide",
 	"/documentation/4.2/manuals/"			=> "Manuals",
 	"/documentation/4.2/api/"			=> "API",
-	"/documentation/4.2/xfce4-intro/"		=> "Introduction",
-	"/documentation/4.2/xfce4-use/"			=> "Using Xfce",
-	"/documentation/4.2/xfce4-components/"		=> "Components of Xfce 4",
-	"/documentation/4.2/xfce4-install/"		=> "Installing Xfce4",
-	"/documentation/4.2/xfce4-copyright/"		=> "Copyright and Licences",
-	"/documentation/4.2/bsd/"			=> "BSD License",
-	"/documentation/4.2/gpl/"			=> "GNU General Public License",
-	"/documentation/4.2/lgpl/"			=> "GNU Library General Public License",
 	
-	"/documentation/manuals/xfce4-panel/"		=> "Panel",
-	"/documentation/manuals/xfwm4/"			=> "Window Manager",
-	"/documentation/manuals/xfce4-session/"		=> "Session Manager",
-	"/documentation/manuals/xfdesktop/"		=> "Desktop Manager",
-	"/documentation/manuals/xfce-utils/"		=> "Utilities and Scripts",
-	"/documentation/manuals/xfprint/"		=> "Printing Management",
-	"/documentation/manuals/xfce-mcs-manager/"	=> "Settings Manager",
-	"/documentation/manuals/xfce-mcs-plugins/"	=> "Settings Manager Plugins",
-	"/documentation/manuals/xffm/"			=> "File Manager",
-	"/documentation/manuals/xfce4-appfinder/"	=> "Application Finder",
+	"/documentation/4.0/"				=> "4.0 Documentation",
+	"/documentation/4.0/userguide/"			=> "User Guide",
+	"/documentation/4.0/manuals/"			=> "Manuals",
 );
 
 ?>
