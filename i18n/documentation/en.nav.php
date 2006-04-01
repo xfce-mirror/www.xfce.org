657a2dd4d2b83947a6bbec4ed33fe660ecc63615
Auke Kok <sofar@foo-projects.org>
2006-04-01 06:28:02 +0000

    typo
    (Old svn revision: 263)

--- i18n/documentation/en.nav.php
+++ i18n/documentation/en.nav.php
@@ -1,9 +1,9 @@
 <ul>
 	<li><a href="/documentation/">Documentation</a>
 		<ul>
-			<li><a href="/documentation/4.2/">4.2 Users Guide</a></li>
-			<li><a href="/documentation/manuals/">Manuals</a></li>
-			<li><a href="/documentation/api/">Developers (API)</a></li>
+			<li><a href="/documentation/4.2/userguide">4.2 User Guide</a></li>
+			<li><a href="/documentation/4.2/manuals/">Manuals</a></li>
+			<li><a href="/documentation/4.2/api/">Developers (API)</a></li>
 		</ul>
 	</li>
 	<li><a href="/documentation/faq/">FAQs</a>
