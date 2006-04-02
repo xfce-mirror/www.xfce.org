dacb3c0cba20806c0138219ddad55e3f4f737a94
Auke Kok <sofar@foo-projects.org>
2006-04-02 21:33:30 +0000

    Sync api links.
    (Old svn revision: 300)

--- i18n/documentation/4.4/api/index.en.php
+++ i18n/documentation/4.4/api/index.en.php
@@ -1,19 +1,21 @@
 <h1>Developers Documentation (4.4) </h1>
-<h3><a href="/documentation/api/libxfce4util/">libxfce4util</a></h3>
+<h3><a href="/documentation/4.4/api/libxfce4util/">libxfce4util</a></h3>
 <p>This component is the basic utilitiy library for the Xfce Desktop Environment. Everything else is build on top of this library and the functionality provided by it.</p>
 
-<h3><a href="/documentation/api/libxfce4mcs/">libxfce4mcs</a></h3>
+<h3><a href="/documentation/4.4/api/libxfce4mcs/">libxfce4mcs</a></h3>
 <p>This package contains the modules used in the Xfce Multi-Channel Settings (MCS) system. The manual describes both the manager-side API - used when creating plugins for the Xfce MCS Manager - and the client-side API - used in creation of clients that access their settings using the MCS system.</p>
 
-<h3><a href="/documentation/api/libxfcegui4/">libxfcegui4</a></h3>
+<h3><a href="/documentation/4.4/api/libxfcegui4/">libxfcegui4</a></h3>
 <p>The basic widget library for the Xfce Desktop Environment, it contains additional widgets, that extend the basic widget set provided by Gtk+, functions to communicate with other NET compliant Xclients and various utility functions and modules.</p>
 
-<h3><a href="/documentation/api/libexo/">libexo</a></h3>
+<h3><a href="/documentation/4.4/api/libxfce4panel/">libxfce4panel</a></h3>
+<p>Documentation for the new panel framework.</p>
+
+<h3><a href="/documentation/4.4/api/libexo/">libexo</a></h3>
 <p>Libexo is an extension library for Xfce, developed by <a href="http://www.os-cillation.com/" target="_blank">os-cillation</a>. While the core Xfce libraries are targeted at desktop development, libexo is targeted at application developement - in particular, developing applications for the Xfce Desktop Environment.</p>
 
-<h3><a href="/documentation/api/libxfce4panel/">libxfce4panel</a></h3>
-<p>Documentation for the new panel framework.</p>
-<h3><a href="/documentation/api/thunar-vfs/">thunar-vfs</a></h3>
+<h3><a href="/documentation/4.4/api/thunar-vfs/">thunar-vfs</a></h3>
 <p> Thunar-vfs is a filesystem abstraction library allowing applications plugable transparent access to a variety of &quot;real&quot; filesystems.</p>
-<h3><a href="/documentation/api/thunarx/">thunarx</a></h3>
+
+<h3><a href="/documentation/4.4/api/thunarx/">thunarx</a></h3>
 <p> The Thunar Extension Framework (thunarx) provides developers with an easy way to extend the basic functionality provided by        the <a href="http://thunar.xfce.org/" target="_top">Thunar File Manager</a>.        The thunarx library includes interfaces that can        be implemented by extensions for various purposes.</p>
