d23113af4d87c91d0498e1ba998f1933ff36bab6
Nick Schermer <nick@xfce.org>
2006-04-03 19:44:23 +0000

    * Added download page
    (Old svn revision: 305)

--- i18n/download/index.en.php
+++ i18n/download/index.en.php
@@ -1,11 +1,58 @@
-<h1>Download Stable version</h1>
-<p></p>
+<?php
+	$version = "4.2.3.2";
+	$sf_net  = "http://sourceforge.net/project/showfiles.php?group_id=19869&amp;package_id=131509&amp;release_id=371171";
+?>
+
+<h1>Download Stable version (<?php echo $version; ?>) </h1>
+<p>Most distributions ship with Xfce. But if you want a newer version or you want to build Xfce from scratch, you can find the packages below. </p>
 
 <h2 id="source">Source code</h2>
-<p></p>
-
-<h2 id="installer">Graphical installer</h2>
-<p></p>
-
+<p>All Xfce modules are available in one single tarball from our SourceForge  repository, and from several mirrors (in one single tarball <em>or</em> separate  tarballs):</p>
+<ul>
+  <li><a href="<?php echo $sf_net; ?>" target="_blank">SourceForge.net</a></li>
+  <li><a href="http://www.xfce.org/archive/xfce-<?php echo $version; ?>" target="_blank">xfce.org</a> | <a href="http://www.us.xfce.org/archive/xfce-<?php echo $version; ?>" target="_blank">us.xfce.org</a> | <a href="http://www.ca-us.xfce.org/archive/xfce-<?php echo $version; ?>" target="_blank">ca-us.xfce.org</a></li>
+  <li><a href="http://www.p0llux.be/xfce/xfce-<?php echo $version; ?>/" target="_blank">p0llux.be</a></li>
+  <!-- <li><a href="http://public.fh-wolfenbuettel.de/~luedickj/xfce4/xfce-<?php echo $version; ?>/">http://public.fh-wolfenbuettel.de/~luedickj/xfce4/xfce-<?php echo $version; ?>/</a></li> -->
+  <li><a href="http://hannelore.f1.fhtw-berlin.de/mirrors/xfce4/xfce-<?php echo $version; ?>/" target="_blank">Hannelore</a></li>
+</ul>
+<h2 id="installer">Graphical installers</h2>
+<p> Xfce-Installers is a collection of packages which noticably simplify the installation process of Xfce and some extensions. Read more about the installer <a href="http://www.os-cillation.com/index.php?id=31&amp;L=5" target="_blank">here</a>. </p>
+<ul>
+  <li>Xfce installer
+    <ul>
+      <li><a href="http://prdownloads.sourceforge.net/xfce/xfce4-<?php echo $version; ?>-installer.bin?download" target="_blank">SourceForge.net</a></li>
+      <li><a href="http://www.xfce.org/archive/xfce-<?php echo $version; ?>/installers/" target="_blank">xfce.org</a> | <a href="http://www.us.xfce.org/archive/xfce-<?php echo $version; ?>/installers/" target="_blank">us.xfce.org</a> | <a href="http://www.ca-us.xfce.org/archive/xfce-<?php echo $version; ?>/installers/" target="_blank">ca-us.xfce.org</a></li>
+      <li><a href="http://www.p0llux.be/xfce/xfce-<?php echo $version; ?>/installers/" target="_blank">p0llux.be</a></li>
+      <li><a href="http://hannelore.f1.fhtw-berlin.de/mirrors/xfce4/xfce-<?php echo $version; ?>/installers/" target="_blank">Hannelore</a></li>
+    </ul>
+  </li>
+  <li><a href="http://www.os-cillation.com/index.php?id=32&amp;L=5" target="_blank">Xfce GTK engine installer</a></li>
+  <li><a href="http://www.os-cillation.com/index.php?id=33&amp;L=5" target="_blank">Xfce Goodies installer</a></li>
+  <li><a href="http://www.os-cillation.com/index.php?id=34&amp;L=5" target="_blank">Terminal installer    </a></li>
+</ul>
 <h2 id="binaries">Binaries</h2>
-<p></p>
+<table cellspacing="5">
+  <tr>
+    <td><img src="../../images/download/debian.png" alt="Debian" width="40" height="40" /></td>
+    <td><strong>Debian Testing / Unstable</strong><br />
+      <a href="http://os-cillation.de/index.php?id=23&amp;L=5" target="_blank">os-cillation.de</a></td>
+  </tr>
+  <tr>
+    <td><img src="../../images/download/fedora.png" alt="Fedora" width="40" height="40" /></td>
+    <td><strong>Redhat & Fedora</strong><br />
+    <a href="<?php echo $sf_net; ?>" target="_blank">SourceForge.net<br />
+    </a><a href="http://www.xfce.org/archive/xfce-<?php echo $version; ?>" target="_blank">xfce.org</a> | <a href="http://www.us.xfce.org/archive/xfce-<?php echo $version; ?>" target="_blank">us.xfce.org</a> | <a href="http://www.ca-us.xfce.org/archive/xfce-<?php echo $version; ?>" target="_blank">ca-us.xfce.org</a><br />
+    <a href="http://www.p0llux.be/xfce/xfce-<?php echo $version; ?>/installers/" target="_blank">p0llux.be</a><br />
+    <a href="http://hannelore.f1.fhtw-berlin.de/mirrors/xfce4/xfce-<?php echo $version; ?>/installers/" target="_blank">Hannelore</a><a href="http://hannelore.f1.fhtw-berlin.de/mirrors/xfce4/xfce-4.2.2/" target="_blank"></a></td>
+  </tr>
+  <tr>
+    <td><img src="../../images/download/suse.png" alt="Suse" width="40" height="40" /></td>
+    <td><strong>Suse 9.3</strong><br />
+      <a href="http://www.students.oamk.fi/~olilju00/xfce4/" target="_blank">http://www.students.oamk.fi/~olilju00/xfce4/</a></td>
+  </tr>
+  <tr>
+    <td><img src="../../images/download/solaris.png" alt="Solaris" width="40" height="40" /></td>
+    <td><strong>Solaris SPARC/x86/AMD</strong><br />
+      <a href="http://www.blastwave.org" target="_blank">http://www.blastwave.org</a></td>
+  </tr>
+</table>
