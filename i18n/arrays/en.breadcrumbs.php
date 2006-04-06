268b0599cb4e0e8fee0572191e7750ca5a6ffe2a
Nick Schermer <nick@xfce.org>
2006-04-06 15:34:33 +0000

    * Various Fixes in the breadcrumbs and projects overview
    (Old svn revision: 321)

--- i18n/arrays/en.breadcrumbs.php
+++ i18n/arrays/en.breadcrumbs.php
@@ -17,11 +17,18 @@ $breadcrumbs = array (
 	"xfdesktop"		=> "Desktop Manager",
 	"xfce4-session" => "Session Manager",
 	"xfce-utils"	=> "Utilities and Scripts",
-	"xfprint"		=> "Printing Management",
+	"xfprint"		=> "Printing Helper",
 	"xfce-mcs-manager" => "Settings Manager",
 	"xfce-mcs-plugins" => "Settings Manager Plugins",
 	"xffm" 			=> "Filemanager",
 	"xfce4-appfinder"  => "Application Finder",
+	"xfce4-mixer"	=> "Volume Control",
+	"xfce4-terminal"   => "Xfce4 Terminal",
+	"xfce4-dev-tools"  => "Xfce Development Tools",
+	"xarchive"		   => "Archive Manager",
+	"xfc"			=> "Xfce Foundation Classes",
+	"xfce-gtk-engine"  => "GTK Theme Engine",
+	"dbh"			=> "Disk Based Hashtables"
 );
 
 ?>
