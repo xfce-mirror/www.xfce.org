e2271436e602ee71ff157d4ac7a59f53dc95cd0b
Nick Schermer <nick@xfce.org>
2006-04-06 12:04:39 +0000

    * Reorder projects * Added dbh
    (Old svn revision: 319)

--- i18n/projects/en.nav.php
+++ i18n/projects/en.nav.php
@@ -2,8 +2,9 @@
 	<li><a href="/projects">Projects</a>
 		<ul>
 			<li><a href="/projects#core">Core Components</a></li>
-			<li><a href="/projects#projects">Projects</a></li>
-			<li><a href="/projects#related">Related</a></li>
+			<li><a href="/projects#applications">Applications</a></li>
+		    <li><a href="/projects#framework">Framework</a></li>
+		    <li><a href="/projects#art">Art and Graphics</a></li>
 		</ul>
 	</li>
 </ul>
