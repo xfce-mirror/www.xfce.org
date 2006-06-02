5492307a04c8a95f80fb9363c5068c23c836eadd
Stephan Arts <stephan@xfce.org>
2006-06-02 15:39:42 +0000

    Fixed links in development page
    (Old svn revision: 334)

--- i18n/development/index.en.php
+++ i18n/development/index.en.php
@@ -1,17 +1,19 @@
 <h1>Development</h1>
 <p>Xfce is a lightweight desktop environment, designed for productivity.</p>
 <h2 id="involved">Getting involved</h2>
+<p>If you are looking for a way to contribute to Xfce, 
+please concider joining the <a href="../community/lists">mailing lists</a> and <a href="http://forum.xfce.org">forum</a> regarding one of these items.</p>
 <table width="99%" border="0" cellspacing="10" cellpadding="0">
 <tr>
 <td width="48" valign="top"><img src="../../images/development/i18n.png" alt="i18n" width="48" height="48" /></td>
-<td width="33%" valign="top"><h3><a href="">Translations</a></h3>
+<td width="33%" valign="top"><h3><a href="http://i18n.xfce.org">Translations</a></h3>
 <p>Xfce is translated in many different languages, if your language of choice is missing, please help us translate Xfce.</p></td>
 <td width="48" valign="top"><img src="../../images/development/docs.png" alt="docs" width="48" height="48" /></td>
-<td width="33%" valign="top"><h3><a href="">Documentation</a></h3>
+<td width="33%" valign="top"><h3><a href="../documentation/">Documentation</a></h3>
 <p>We always welcome additional documentation about Xfce</p></td>
 <td width="48" valign="top"><img src="../../images/development/artwork.png" alt="art" width="48" height="48" /></td>
 <td width="33%" valign="top">
-<h3><a href="">Artwork</a></h3>
+<h3><a href="../documentation/howto#artwork">Artwork</a></h3>
 <p>Want to create your own Xfce look? You can by following our step-by-step example howto's. Or you can just take a look at <a href="http://www.xfce-look.org/">xfce-look</a> to see how others have customised their Xfce.</p></td>
 </tr>
 <tr>
@@ -19,7 +21,7 @@
 <td width="33%" valign="top"><h3><a href="http://goodies.xfce.org">Goodies</a></h3>
 <p>The Xfce Goodies Project includes additional software and artwork that are related to the Xfce desktop, but not part of the official release.</p></td>
 <td width="48" valign="top"><img src="../../images/development/devel.png" alt="dev" width="48" height="48" /></td>
-<td width="33%" valign="top"><h3><a href="">Development</a></h3>
+<td width="33%" valign="top"><h3><a href="../development#svn">Development</a></h3>
 <p>Feel free to join us developing the next major branch of Xfce, or fix bugs in existing releases</p></td>
 <td width="48" valign="top">&nbsp;</td>
 <td width="33%" valign="top">&nbsp;</td>
