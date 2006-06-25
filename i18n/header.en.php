03132bf9a41c6f03b790bcd134f71b41584bf1bf
Nick Schermer <nick@xfce.org>
2006-06-25 11:45:38 +0000

    * Small improvement
    (Old svn revision: 346)

--- i18n/header.en.php
+++ i18n/header.en.php
@@ -43,7 +43,7 @@
 						<h2 class="hidden">Search</h2>
 						<form method="get" action="http://ww.google.com/search" name="g">
 							<input type="text" name="q" id="q" onfocus="if(this.value==this.defaultValue ) this.value='';" onblur="if(this.value=='') this.value=this.defaultValue;" value="Search..." />
-							<input type="hidden" name="as_sitesearch" value="xfce.org" />
+							<input type="hidden" name="sitesearch" value="xfce.org" />
 							<input type="hidden" name="num" value="30" />
 							<input type="hidden" name="lr" value="lang_<?php echo $lang ?>" />
 							<input type="submit" name="btnG" class="hidden" id="submit" value="Search" />
