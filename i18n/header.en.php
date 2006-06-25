acb59916aabf523d158336f3b0a5eacc3a0a5992
Nick Schermer <nick@xfce.org>
2006-06-25 10:58:54 +0000

    Test for google sitemap
    (Old svn revision: 343)

--- i18n/header.en.php
+++ i18n/header.en.php
@@ -3,6 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>" />
+	<META name="verify-v1" content="cSLdw4tBCdH+3VZ59li/DF8AjMeKqiUEEr+RKUr1i/I=" />
 	<title>Xfce - Desktop Environment</title>
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 
