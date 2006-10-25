cfac9f91b75a9576112902ea3569be6c863adabd
Nick Schermer <nick@xfce.org>
2006-10-25 09:32:45 +0000

    Test google co-op search
    (Old svn revision: 433)

---
+++ i18n/search.en.php
@@ -0,0 +1,9 @@
+<!-- Google CSE Search Box Begins -->
+<form id="searchbox_008920933163277390313:rifucd7pcta" action="http://">
+  <input type="hidden" name="cx" value="008920933163277390313:rifucd7pcta" />
+  <input name="q" type="text" size="40" />
+  <input type="submit" name="sa" value="Search" />
+  <input type="hidden" name="cof" value="FORID:9" />
+</form>
+<script type="text/javascript" src="http://google.com/coop/cse/brand?form=searchbox_008920933163277390313%3Arifucd7pcta"></script>
+<!-- Google CSE Search Box Ends -->
