d487e6d4142794a250f5edb440309372e2190bb3
Nick Schermer <nick@xfce.org>
2006-10-25 15:08:09 +0000

    search page
    (Old svn revision: 455)

--- i18n/search.en.php
+++ i18n/search.en.php
@@ -1,8 +1,8 @@
 <!-- Google Search Result Snippet Begins -->
-<div id="Searchresults" style="overflow: hidden"></div>
+<div id="results_008920933163277390313:rifucd7pcta" style="overflow: hidden"></div>
 <script type="text/javascript">
-  var googleSearchIframeName = "Searchresults";
-  var googleSearchFormName = "";
+  var googleSearchIframeName = "results_008920933163277390313:rifucd7pcta";
+  var googleSearchFormName = "searchbox_008920933163277390313:rifucd7pcta";
   var googleSearchFrameWidth = 650;
   var googleSearchFrameborder = 0;
   var googleSearchDomain = "google.com";
