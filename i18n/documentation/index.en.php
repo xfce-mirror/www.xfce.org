435629f42f653e09d8d96832339fc557c7a2abe0
Nick Schermer <nick@xfce.org>
2006-10-25 20:08:00 +0000

    Remove the 4.4 documentation links
    (Old svn revision: 510)

--- i18n/documentation/index.en.php
+++ i18n/documentation/index.en.php
@@ -4,14 +4,10 @@
   <tr>
     <td width="49%" valign="top">
       <h2><a href="/documentation/4.4/userguide/">User Guide</a></h2>
-      <ul>
-        <li><a href="/documentation/4.4/userguide/#preface">Preface</a></li>
-        <li><a href="/documentation/4.4/userguide/xfce4-intro">Introduction</a></li>
-        <li><a href="/documentation/4.4/userguide/xfce4-use">Using Xfce 4</a></li>
-        <li><a href="/documentation/4.4/userguide/xfce4-components">Components of Xfce 4</a></li>
-        <li><a href="/documentation/4.4/userguide/xfce4-install">Installing Xfce 4</a></li>
-        <li><a href="/documentation/4.4/userguide/xfce4-copyright">Copyright and License</a></li>
-      </ul>
+      <p>We're sorry, but there is no documentation for 4.4 (<a href="/development/">yet</a>). You can 
+         search the <a href="/documentation/4.2/">4.2 documentation</a>, visit the 
+         <a href="http://forum.xfce.org" target="_blank">Xfce forum</a> or use the 
+         <a href="/community/lists">mailing lists</a> to solve your problem.</p>
       <h2><a href="/documentation/4.4/api">Developers (API)</a></h2>
       <ul>
         <li><a href="/documentation/4.4/api/libxfce4mcs/">libxfce4mcs</a></li>
@@ -23,27 +19,13 @@
         <li><a href="/documentation/4.4/api/thunarx/">thunarx</a></li>
         <li><a href="/documentation/4.4/api/libxfprint/">libxfprint</a></li>
       </ul>
-      <h2><a href="/documentation/requirements">Requirements</a></h2>
-          <p>A detailed list of all Xfce <a href="/documentation/requirements">dependencies</a>.</p>
           <h2><a href="/documentation/archive">Archive</a></h2>
           <ul>
             <li><a href="/documentation/4.2/">4.2 documentation</a></li>
             <li><a href="/documentation/4.0/">4.0 documentation  </a></li>
     </ul></td>
 	<td width="2%">&nbsp;</td>
-    <td width="49%" valign="top"><h2><a href="/documentation/4.4/manuals/">Application Manuals</a></h2>
-      <ul>
-        <li><a href="/documentation/4.4/manuals/xfce4-panel">Panel</a></li>
-        <li><a href="/documentation/4.4/manuals/xfwm4">Window Manager</a></li>
-        <li><a href="/documentation/4.4/manuals/xfce4-session">Session Manager</a></li>
-        <li><a href="/documentation/4.4/manuals/xfdesktop">Desktop Manager</a></li>
-        <li><a href="/documentation/4.4/manuals/xfce-utils">Utilities and Scripts</a></li>
-        <li><a href="/documentation/4.4/manuals/xfprint">Printing Management</a></li>
-        <li><a href="/documentation/4.4/manuals/xfce-mcs-manager">Settings Manager</a></li>
-        <li><a href="/documentation/4.4/manuals/xfce-mcs-plugins">Settings Manager Plugins</a></li>
-        <li><a href="/documentation/4.4/manuals/xffm">Filemanager</a></li>
-        <li><a href="/documentation/4.4/manuals/xfce4-appfinder">Application Finder</a></li>
-    </ul>
+    <td width="49%" valign="top">
       <h2><a href="/documentation/faq">FAQs</a></h2>
       <ul>
         <li><a href="/documentation/faq#starting">Getting started</a></li>
@@ -52,6 +34,8 @@
         <li><a href="/documentation/faq#appearance">Visual Appearance</a></li>
         <li><a href="/documentation/faq#development">Development</a></li>
       </ul>
+      <h2><a href="/documentation/requirements">Requirements</a></h2>
+          <p>A detailed list of all Xfce <a href="/documentation/requirements">dependencies</a>.</p>
       <h2><a href="/documentation/changelogs/">Changelogs</a></h2>
     <p>All Xfce 4 <a href="/documentation/changelogs/">changelogs</a>, ordered by version.</p>
     </td>
