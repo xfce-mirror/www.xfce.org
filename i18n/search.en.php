2f352019d0024b3c0772b9f499d075e533d6a013
Nick Schermer <nick@xfce.org>
2006-10-25 16:39:31 +0000

    search page
    (Old svn revision: 476)

--- i18n/search.en.php
+++ i18n/search.en.php
@@ -1,5 +1,5 @@
 <!-- Google CSE Search Box Begins -->
-<form id="searchbox_008920933163277390313:rifucd7pcta" action="/search">
+<form id="SearchForm" action="/search">
   <input type="hidden" name="cx" value="008920933163277390313:rifucd7pcta" />
   <input name="q" size="47" class="field" id="q" type="text" />
   <input type="submit" name="sa" value="Search" />
@@ -8,7 +8,7 @@
 <!-- Google CSE Search Box Ends -->
 
 <!-- Google Search Result Snippet Begins -->
-<div id="Searchresults" style="overflow: hidden"></div>
+<div id="SearchResults" style="overflow: hidden"></div>
 <style type="text/css">
   #Searchresults iframe {
     width: 100%;
@@ -16,8 +16,8 @@
   }
 </style>
 <script type="text/javascript">
-  var googleSearchIframeName = "Searchresults";
-  var googleSearchFormName = "searchbox_008920933163277390313:rifucd7pcta";
+  var googleSearchIframeName = "SearchResults";
+  var googleSearchFormName = "SearchForm";
   var googleSearchFrameWidth = 550;
   var googleSearchFrameborder = 0;
   var googleSearchDomain = "google.com";
