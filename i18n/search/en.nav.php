dc981f4731bb581404dffd6a27d66b2cc2824764
Nick Schermer <nick@xfce.org>
2006-10-28 12:53:54 +0000

    Google Co-op logo
    (Old svn revision: 527)

--- i18n/search/en.nav.php
+++ i18n/search/en.nav.php
@@ -1,8 +1,10 @@
 <div class="tenpadding"
-  <p>
-    <a href="http://www.google.com/" target="_blank"><img src="/layout/friends/google.png" alt="Google" title="Powered By Google" border="0" /></a>
-  </p>
-  <p class="grey">
-    If you're using AdBlock (Plus) and you can not view the search results, you might need to unblock Google AdSence in the AdBlock Plugin.
-  </p>
+	<p>
+		<div align="center">
+			<a href="http://www.google.com/" target="_blank"><img src="/layout/friends/google.gif" alt="Google" title="Powered By Google" border="0" /></a>
+		</div>
+	</p>
+	<p class="grey">
+		If you're using AdBlock (Plus) and you can not view the search results, you might need to unblock Google AdSence in the AdBlock Plugin.
+	</p>
 </div>
