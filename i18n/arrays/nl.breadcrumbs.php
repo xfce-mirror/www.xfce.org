f72dd0fba694588bba67eb7f0f50ccc863b80c5c
Nick Schermer <nick@xfce.org>
2006-12-03 18:01:32 +0000

    Some dutch translations
    (Old svn revision: 552)

---
+++ i18n/arrays/nl.breadcrumbs.php
@@ -0,0 +1,37 @@
+<?php
+
+# Translations or Style updates for the breadcrumbs
+$breadcrumbs = array (
+    # Catogory names
+    "home"             => "Home",
+    "lists"            => "Discussiegroepen",
+    "irc"              => "IRC",
+    "faq"              => "FAQs",
+    "userguide"        => "Gebruikershandleiding",
+    "api"              => "Ontwikkelaars (API)",
+    "tour"             => "Xfce 4.4 Rondleiding",
+    "search"           => "Google Zoeken",
+    "distros"          => "Xfce Georiënteerde Distributies",
+    
+    # Common Xfce components
+    "xfce4-panel"      => "Panel",
+    "xfwm4"            => "Window Manager",
+    "xfdesktop"        => "Desktop Manager",
+    "xfce4-session"    => "Session Manager",
+    "xfce-utils"       => "Utilities and Scripts",
+    "xfprint"          => "Printing Helper",
+    "xfce-mcs-manager" => "Settings Manager",
+    "xfce-mcs-plugins" => "Settings Manager Plugins",
+    "xffm"             => "Filemanager",
+    "xfce4-appfinder"  => "Application Finder",
+    "xfce4-mixer"      => "Volume Control",
+    "xfce4-terminal"   => "Xfce4 Terminal",
+    "xfce4-dev-tools"  => "Xfce Development Tools",
+    "xarchive"         => "Archive Manager",
+    "xfc"              => "Xfce Foundation Classes",
+    "xfce-gtk-engine"  => "GTK Theme Engine",
+    "dbh"              => "Disk Based Hashtables",
+    "installit"        => "InstallIt"
+);
+
+?>
---
+++ i18n/header.nl.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $charset; ?>" />
+	<title>Xfce - <?php echo $headtitle; ?></title>
+	<link rel="shortcut icon" href="/favicon.png" type="image/png" />
+
+<?php if (!$headup) { /* show only on frontpage */ ?>
+	<meta name="description" content="Xfce Desktop Environment" />
+	<meta name="keywords" content="desktop environment, window manager, desktop, speed, lightweight, gtk+, open source, xforms common environment" />
+<?php } ?>
+
+	<link rel="home" href="/" />
+	<link rel="help" href="/documentation/" />
+<?php if ($headup) { ?>
+	<link rel="up" href="<?php echo $headup ?>" />
+<?php } ?>
+
+	<link rel="stylesheet" media="screen" href="/layout/css/layout.css" type="text/css" />
+	<?php echo $linked_css; ?>
+
+
+	<style type="text/css">
+		<?php echo $layout_css; ?>
+	</style>
+
+	<link rel="alternate" type="application/rss+xml" title="Xfce News" href="/?feed=rss2&amp;lang=<?php echo $lang ?>" />
+	<link rel="alternate" type="application/rss+xml" title="Xfce Blog" href="http://blog.xfce.org/?feed=rss2" />
+</head>
+<body>
+	<div class="hidden">
+		<a href="#global-contentwrap" title="Skip site navigation" accesskey="1">Skip site navigation</a> (1)
+<?php if ($content_bool) { ?>
+		<a href="#content-text" title="Skip section navigation" accesskey="2">Skip section navigation</a> (2)
+<?php } ?>
+
+		<h1>Xfce Desktop Environment</h1>
+		<h4>...and everything goes faster!</h4>
+		<hr />
+	</div>
+	<div id="global-pagewrap">
+		<div id="global-page">
+			<div id="header">
+				<div id="header-logo"></div>
+				<div id="header-right">
+					<div id="header-style">
+						<h2 class="hidden">Website Opmaak</h2>
+						<p>Layout: <a href="?layout=normal" title="Normal layout (Min: 740px, Max: 1000px)">Normaal</a> / 
+						<a href="?layout=liquid" title="Fluid Layout (100% Width)">Uitvullen</a></p>
+					</div>
+					<div id="header-search">
+						<h2 class="hidden">Zoeken</h2>
+						<form id="GoogleSearch" action="/search/">
+							<input type="hidden" name="cx" value="008920933163277390313:rifucd7pcta" />
+							<input name="q" type="text" onfocus="if(this.value==this.defaultValue) this.value='';" onblur="if(this.value=='') this.value=this.defaultValue;" value="Google Zoeken..." />
+							<input type="submit" name="sa" class="hidden" value="Zoeken" />
+							<input type="hidden" name="cof" value="FORID:11" />
+						</form>
+					</div>
+				</div>
+				<div id="header-menu">
+					<h2 class="hidden">Site navigatie</h2>
+					<ul>
+						<li><a href="/" title="Home">Start</a></li>
+						<li><a href="/about/" title="About">Over Xfce</a></li>
+						<li><a href="/projects/" title="Projects">Projecten</a></li>
+						<li><a href="/download/" title="Download">Download</a></li>
+						<li><a href="/documentation/" title="Documentation">Documentatie</a></li>
+						<li><a href="/community/" title="Community">Gemeenschap</a></li>
+						<li><a href="/development/" title="Development">Ontwikkeling</a></li>
+					</ul>
+				</div>
+				<div id="header-white">
+					<div id="header-language">
+						<h2 class="hidden">Website Taal</h2>
+						<?php echo $lang_links; ?> 
+					</div>
+<?php if ($breadcrumbs) { ?>
+					<div id="header-breadcrumbs">
+						<h2 class="hidden">Navigatie</h2>
+						<?php echo $breadcrumbs; ?>
+					</div>
+<?php } ?>
+				</div>
+			</div><!-- header -->
+			<hr class="hidden" />
+			<div id="global-contentwrap">
+<!-- start: page content -->
