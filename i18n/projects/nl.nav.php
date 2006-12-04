a4aced5d3bde3cec746428efbebc8c3d136bd013
Stephan Arts <stephan@xfce.org>
2006-12-04 11:38:54 +0000

    Added some Dutch translations.
    (Old svn revision: 554)

---
+++ i18n/projects/nl.nav.php
@@ -0,0 +1,10 @@
+<ul>
+	<li><a href="/projects">Projecten</a>
+		<ul>
+			<li><a href="/projects/#core">Kern Onderdelen</a></li>
+			<li><a href="/projects/#applications">Programma's</a></li>
+		    <li><a href="/projects/#framework">Raamwerk</a></li>
+		    <li><a href="/projects/#art">Kunst en Afbeeldingen</a></li>
+		</ul>
+	</li>
+</ul>
---
+++ i18n/projects/xfdesktop/nl.nav.php
@@ -0,0 +1,17 @@
+<ul>
+	<li><a href="/projects#core">Kern Onderdelen</a>
+		<ul>
+		    <li><a href="/projects/xfdesktop">Bureaubladbeheer</a></li>
+		    <li><a href="/projects/xfce4-panel">Paneel</a></li>
+		    <li><a href="/projects/xfprint">Print Ondersteuning</a></li>
+		    <li><a href="/projects/xfce4-session">Sessiebeheer</a></li>
+		    <li><a href="/projects/xfce-mcs-manager">Configuratiebeheer</a></li>
+		    <li><a href="/projects/xfce-utils">Voorzieningen en scripts</a></li>
+		    <li><a href="/projects/xfwm4">Window Manager</a></li>
+		    <li><a href="/projects/libraries">Xfce Libraries</a></li>
+		</ul>
+	</li>
+	<li><a href="/projects#applications">Programma's</a></li>
+	<li><a href="/projects#framework">Raamwerk</a></li>
+	<li><a href="/projects#art">Kunst en Afbeeldingen</a></li>
+</ul>
