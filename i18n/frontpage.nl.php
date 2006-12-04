756cb4c36df2e9a2a235c72cce72acf3b2683d6a
Stephan Arts <stephan@xfce.org>
2006-12-04 13:52:02 +0000

    translated frontpage
    (Old svn revision: 557)

---
+++ i18n/frontpage.nl.php
@@ -0,0 +1,89 @@
+<div id="front-intro">
+	<div id="front-right">
+		<div id="front-download">
+			<div class="link">
+				<h2><a href="/download/" title="Download Xfce 4.4">Download Xfce 4.4</a></h2>
+			</div>
+			<div class="notes">
+				<p><a href="/documentation/changelogs/4.4.0" title="Lees het ChangeLog">ChangeLogs</a><span class="grey"> | </span><a href="/about/tour" title="Wat is er nieuw aan Xfce 4.4">Tour</a></p>
+			</div>
+		</div>
+	</div>
+	<div id="front-intro-about">
+		<h1>Over Xfce</h1>
+		<p>Xfce is a lightweight desktop environment for various *NIX systems. Designed for productivity, it loads and executes applications fast, while conserving system resources.</p>
+		<p>Xfce 4.4 embodies the traditional UNIX philosophy of modularity and re-usability. It consists of a number of components that together provide the full functionality of the desktop environment. They are packaged separately and you can pick and choose from the available packages to create the best personal working environment.</p>
+		<p class="readmore"><a href="/about/" title="Learn more about Xfce...">learn more</a></p>
+	</div>
+</div>
+<div class="clearboth"></div>
+<div id="front-container">
+	<div id="front-news">
+		<div id="front-news-content">
+			<h2><a href="/?feed=rss2&amp;lang=<?php echo $lang ?>"><img src="/layout/images/feed.gif" alt="feed" title="Subscribe to 'Xfce News'..." width="12" height="12" border="0" align="right" /></a>Laatste Nieuws</h2>
+			<?php
+				echo $frontpage["news"];
+			?>
+			<div class="readmore">
+				<a href="/about/news" title="Read all Xfce news...">read more</a>
+			</div>
+		</div>
+	</div>
+	<div id="front-projects">
+		<div id="front-borders">
+			<div id="front-projects-content">
+				<h2>Xfce projecten</h2>
+				<ul>
+					<li>
+						<a href="/projects/thunar/">Thunar</a>
+						<br />
+						<span class="grey">Thunar is een nieuwe moderne bestandsbeheerder voor de Xfce Desktop Environment.</span>
+					</li>
+					<li>
+						<a href="/projects/mousepad/">Mousepad</a>
+						<br />
+						<span class="grey">Mousepad is een simpele, snelle en gemakkelijke tekstverwerker voor de Xfce Desktop Environment, gebaseerd op de Leafpad tekstverwerker.</span>
+					</li>
+					<li>
+						<a href="/projects/orage/">Orage</a>
+						<br />
+						<span class="grey">Kalender voor het Xfce project</span>
+					</li>
+					<li>
+						<a href="/projects/xfburn/">Xfburn</a>
+						<br />
+						<span class="grey">CD and DVD brand programma.</span>
+					</li>
+					<li>
+						<a href="/projects/xfmedia/">Xfmedia</a>
+						<br />
+						<span class="grey">Xfmedia is een simpele, gemakkelijke media speler gebaseerd op de xine engine.</span>
+					</li>
+				</ul>
+				<div class="readmore">
+					<a href="/projects/" title="Toon alle Xfce projecten...">meer projecten</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="front-blog">
+		<div id="front-blog-content">
+			<h2><a href="http://blog.xfce.org/?feed=rss2"><img src="/layout/images/feed.gif" alt="feed" title="Aanmelden voor 'Xfce Blog'..." width="12" height="12" border="0" align="right" /></a>Ontwikkelaars Blog</h2>
+			<?php
+				echo $frontpage["blog"];
+			?>
+			<div class="readmore">
+				<a href="http://blog.xfce.org/" title="Toon het gehele Xfce blog...">bezoek blog</a>
+			</div>
+			<h2>Snelkoppelingen</h2>
+			<ul>
+				<li><a href="/community/lists">Mailinglists</a></li>
+				<li><a href="/about/screenshots">Screenshots</a></li>
+				<li><a href="http://forum.xfce.org" target="_blank" title="Xfce Forum (New window)" class="external">Xfce Forum</a></li>
+				<li><a href="http://bugzilla.xfce.org" title="Report a bug  (New window)" target="_blank" class="external">Rapporteer een bug</a></li>
+				<li><a href="http://wiki.xfce.org" title="Xfce Wiki  (New window)" target="_blank" class="external">Xfce Wiki</a></li>
+				<li><a href="/download/distros">Xfce Distributies</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
