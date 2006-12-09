cac9a8706f65d997c067f96da10fe63fafaac828
Nick Schermer <nick@xfce.org>
2006-12-09 10:45:04 +0000

    * Redirect table from old -> new website * Only include files when needed
    (Old svn revision: 568)

---
+++ include/redirect.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Redirct array from the old website pages to the new ones
+ *
+ * Note: The first / will be prepended by the header
+ **/
+
+$redirect = array (
+	"news"				=> "about/news",
+	"screenshots"		=> "about/screenshots",
+	"devel_screenshots"	=> "about/screenshots",
+	"users_screenshots"	=> "about/screenshots",
+	"overview"			=> "about/",
+	"developers"		=> "development/",
+	"mailinglists"		=> "community/lists",
+	"about"				=> "about/credits",
+	"links"				=> "about/press",
+	"documentation"		=> "documentation/",
+	"download"			=> "download/"
+);
+
+?>
--- index.php
+++ index.php
@@ -2,69 +2,84 @@
 
 function microtime_float ()
 {
-	list ($usec, $sec) = explode(" ", microtime());
-	return ((float)$usec + (float)$sec);
+    list ($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
 }
 $time_start = microtime_float ();
 
 include ("include/functions.php");
 include ("include/arrays.php");
-include ("include/header.php");
-include ("include/footer.php");
-include ("include/frontpage.php");
-include ("include/content.php");
-include ("include/feed.php");
-include ("include/news.php");
 
-# Load session valiables
+/* Load session valiables */
 session_start ();
 
-# User language
+/* User language */
 $userlang = substr(trim($_SERVER["HTTP_ACCEPT_LANGUAGE"]), 0, 2);
 if (!in_array ($userlang, array_keys($languages)))
-	$userlang = "en";
+    $userlang = "en";
 
-# Get saved variables
+/* Get saved variables */
 $layout = UserVariable ("layout", $layouts, "normal");
 $lang = UserVariable ("lang", array_keys($languages), $userlang);
 $lastvisit = UserLastVisit ();
 
-# Set the locale for the date() function
+/* Set the locale for the date() function */
 setlocale (LC_ALL, $languages[$lang][2]);
 
-# Check for feed request
+/* Redirects from old to new website */
+if ($page = $_GET["page"])
+{
+    include ("include/redirect.php");
+
+    /* Send user to the new url */
+	header ("Location: http://". $_SERVER['SERVER_NAME'] ."/". $redirect[$page]);
+	exit;
+}
+
+/* Check for feed request */
 if ($_GET["feed"] == "rss2")
 {
-    # Show feed xml and exit
+    include ("include/feed.php");
+
+    /* Show feed xml and exit */
     ParseRssFeed ($lang);
     exit;
 }
 
-# Get relative url
+/* Get relative url */
 $uri = $_SERVER["REDIRECT_URL"];
 $uri = trim ($uri, '/');
 $uri = strtolower ($uri);
 
-# Block some pages users are not allowed to see
+/* Block some pages users are not allowed to see */
 $forbidden = array ("footer", "frontpage", "header");
 if (in_array ($uri, $forbidden))
-	$uri = "";
+    $uri = "";
 
-# Create webpage
+/* Include needed files */
+include ("include/header.php");
+include ("include/footer.php");
+include ("include/news.php");
+
+/* Create webpage */
 if ($uri == "")
 {
-	PrintHeader ($uri, $lang, $layout, $languages);
-	PrintFrontpage ($lang, $lastvisit);
-	PrintFooter ($lang);
+    include ("include/frontpage.php");
+
+    PrintHeader ($uri, $lang, $layout, $languages);
+    PrintFrontpage ($lang, $lastvisit);
+    PrintFooter ($lang);
 }
 else
 {
-	$content = PrepareContent ($uri, $lang);
-	PrintHeader ($uri, $lang, $layout, $languages);
-	PrintContent ($content, $lang);
-	PrintFooter ($lang);
+    include ("include/content.php");
+
+    $content = PrepareContent ($uri, $lang);
+    PrintHeader ($uri, $lang, $layout, $languages);
+    PrintContent ($content, $lang);
+    PrintFooter ($lang);
 }
 
 $time_end = microtime_float ();
-echo "<!-- Execution time: ". round ($time_end - $time_start, 4) ." second -->"; 
+echo "<!-- Execution time: ". round ($time_end - $time_start, 4) ." second -->";
 ?>
