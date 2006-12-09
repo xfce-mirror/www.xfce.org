edf14229162319854a596663a990429bae807d04
Nick Schermer <nick@xfce.org>
2006-12-09 11:25:01 +0000

    Misc dutch translations
    (Old svn revision: 570)

--- i18n/frontpage.nl.php
+++ i18n/frontpage.nl.php
@@ -5,82 +5,82 @@
 				<h2><a href="/download/" title="Download Xfce 4.4">Download Xfce 4.4</a></h2>
 			</div>
 			<div class="notes">
-				<p><a href="/documentation/changelogs/4.4.0" title="Lees het ChangeLog">ChangeLogs</a><span class="grey"> | </span><a href="/about/tour" title="Wat is er nieuw aan Xfce 4.4">Tour</a></p>
+				<p><a href="/documentation/changelogs/4.4.0" title="Lees de ChangeLog">ChangeLogs</a><span class="grey"> | </span><a href="/about/tour" title="Wat is er nieuw in Xfce 4.4?">Rondleiding</a></p>
 			</div>
 		</div>
 	</div>
 	<div id="front-intro-about">
-		<h1>Over Xfce</h1>
-		<p>Xfce is a lightweight desktop environment for various *NIX systems. Designed for productivity, it loads and executes applications fast, while conserving system resources.</p>
-		<p>Xfce 4.4 embodies the traditional UNIX philosophy of modularity and re-usability. It consists of a number of components that together provide the full functionality of the desktop environment. They are packaged separately and you can pick and choose from the available packages to create the best personal working environment.</p>
-		<p class="readmore"><a href="/about/" title="Learn more about Xfce...">learn more</a></p>
+		<h1>Wat is Xfce?</h1>
+		<p>Xfce is een lichtgewicht bureaublad omgeving voor unix-achtige besturings-systemen. Het is gericht op snelheid en geheugengebruik, terwijl het nog steeds visueel aantrekkelijk en gemakkelijk te gebruiken is.</p>
+		<p>De doelstelling van Xfce is herbruikbaarheid en modulariteit. Het bestaat uit een aantal onderdelen die tezamen de volledige functionaliteit van een bureaublad omgeving bieden. De onderdelen zijn apart te installeren zodat je kunt kiezen uit de beschikbare pakketten om de beste persoonlijke werk omgeving te maken.</p>
+		<p class="readmore"><a href="/about/" title="Lees meer over Xfce...">lees verder</a></p>
 	</div>
 </div>
 <div class="clearboth"></div>
 <div id="front-container">
 	<div id="front-news">
 		<div id="front-news-content">
-			<h2><a href="/?feed=rss2&amp;lang=<?php echo $lang ?>"><img src="/layout/images/feed.gif" alt="feed" title="Subscribe to 'Xfce News'..." width="12" height="12" border="0" align="right" /></a>Laatste Nieuws</h2>
+			<h2><a href="/?feed=rss2&amp;lang=<?php echo $lang ?>"><img src="/layout/images/feed.gif" alt="feed" title="Subscribe to 'Xfce News'..." width="12" height="12" border="0" align="right" /></a>Latest News</h2>
 			<?php
 				echo $frontpage["news"];
 			?>
 			<div class="readmore">
-				<a href="/about/news" title="Read all Xfce news...">read more</a>
+				<a href="/about/news" title="Lees al het Xfce nieuws...">lees verder</a>
 			</div>
 		</div>
 	</div>
 	<div id="front-projects">
 		<div id="front-borders">
 			<div id="front-projects-content">
-				<h2>Xfce projecten</h2>
+				<h2>Xfce projects</h2>
 				<ul>
 					<li>
 						<a href="/projects/thunar/">Thunar</a>
 						<br />
-						<span class="grey">Thunar is een nieuwe moderne bestandsbeheerder voor de Xfce Desktop Environment.</span>
+						<span class="grey">Thunar is een moderne bestandsbeheerder voor de Xfce bureaublad omgeving.</span>
 					</li>
 					<li>
 						<a href="/projects/mousepad/">Mousepad</a>
 						<br />
-						<span class="grey">Mousepad is een simpele, snelle en gemakkelijke tekstverwerker voor de Xfce Desktop Environment, gebaseerd op de Leafpad tekstverwerker.</span>
+						<span class="grey">Mousepad is een simpele, handige en vooral snelle tekstverwerker voor de Xfce bureaublad omgeving.</span>
 					</li>
 					<li>
 						<a href="/projects/orage/">Orage</a>
 						<br />
-						<span class="grey">Kalender voor het Xfce project</span>
+						<span class="grey">Kalender voor het Xfce project.</span>
 					</li>
 					<li>
 						<a href="/projects/xfburn/">Xfburn</a>
 						<br />
-						<span class="grey">CD and DVD brand programma.</span>
+						<span class="grey">Programma voor het schrijven van CD's en DVD's.</span>
 					</li>
 					<li>
-						<a href="/projects/xfmedia/">Xfmedia</a>
+						<a href="http://spuriousinterrupt.org/projects/xfmedia/">Xfmedia</a>
 						<br />
-						<span class="grey">Xfmedia is een simpele, gemakkelijke media speler gebaseerd op de xine engine.</span>
+						<span class="grey">Xfmedia is een simpele multimediaspeler, gebaseerd op Xine.</span>
 					</li>
 				</ul>
 				<div class="readmore">
-					<a href="/projects/" title="Toon alle Xfce projecten...">meer projecten</a>
+					<a href="/projects/" title="Bekijk alle Xfce projecten...">meer projecten</a>
 				</div>
 			</div>
 		</div>
 	</div>
 	<div id="front-blog">
 		<div id="front-blog-content">
-			<h2><a href="http://blog.xfce.org/?feed=rss2"><img src="/layout/images/feed.gif" alt="feed" title="Aanmelden voor 'Xfce Blog'..." width="12" height="12" border="0" align="right" /></a>Ontwikkelaars Blog</h2>
+			<h2><a href="http://blog.xfce.org/?feed=rss2"><img src="/layout/images/feed.gif" alt="feed" title="Schrijf je in voor de 'Xfce Blog' feed..." width="12" height="12" border="0" align="right" /></a>Developers Blog</h2>
 			<?php
 				echo $frontpage["blog"];
 			?>
 			<div class="readmore">
-				<a href="http://blog.xfce.org/" title="Toon het gehele Xfce blog...">bezoek blog</a>
+				<a href="http://blog.xfce.org/" title="Bezoek het Xfce blog...">bezoek blog</a>
 			</div>
-			<h2>Snelkoppelingen</h2>
+			<h2>Shortcuts</h2>
 			<ul>
 				<li><a href="/community/lists">Mailinglists</a></li>
 				<li><a href="/about/screenshots">Screenshots</a></li>
 				<li><a href="http://forum.xfce.org" target="_blank" title="Xfce Forum (New window)" class="external">Xfce Forum</a></li>
-				<li><a href="http://bugzilla.xfce.org" title="Report a bug  (New window)" target="_blank" class="external">Rapporteer een bug</a></li>
+				<li><a href="http://bugzilla.xfce.org" title="Report a bug  (New window)" target="_blank" class="external">Meld een bug</a></li>
 				<li><a href="http://wiki.xfce.org" title="Xfce Wiki  (New window)" target="_blank" class="external">Xfce Wiki</a></li>
 				<li><a href="/download/distros">Xfce Distributies</a></li>
 			</ul>
