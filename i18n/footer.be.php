28ff1bfaca540bced979b8e4ae60960ca1ca2bf0
Maximilian Schleiss <maxschleiss@solnet.ch>
2007-01-13 20:57:21 +0000

    Added the partial Belarusian translations from Alexander Nyakhaychyk for the new website
    (Old svn revision: 583)

---
+++ i18n/footer.be.php
@@ -0,0 +1,22 @@
+<!-- end: page content -->
+					<div class="clearboth"></div>
+				</div><!-- global-contentwrap --> 
+			<div id="footer">
+				<div id="footer-line"><hr class="hidden" /></div>
+				<div id="footer-sponsors">
+					<h2 class="hidden">Лягатыпы</h2>
+					<ul>
+						<li><a href="http://www.2x.com/" title="2X Thin client computing software" target="_blank"><img src="/layout/friends/2x_small.jpg" alt="2X" border="1" /></a></li>
+						<li><a href="http://www.os-cillation.com/" title="os-cillation" target="_blank"><img src="/layout/friends/os-cillation.jpg" alt="os-cillation" border="1" /></a></li>
+						<li><a href="http://sourceforge.net/" title="SourceForge.net" target="_blank"><img src="/layout/friends/sf.jpg" alt="SourceForge.net" border="1" /></a></li>
+					</ul>
+				</div>
+				<div id="footer-copyright">
+					<a href="/about/credits#lead" title="Кантак з Olivier Fourdan">&copy; Olivier Fourdan 1996 - <?php echo date ("Y"); ?></a>
+				</div>
+				<div class="clearboth"></div>
+			</div>
+		</div><!-- global-page -->
+	</div><!-- global-pagewrap -->
+</body>
+</html>
