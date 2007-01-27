64a62ba7cea9e6237d060e6106e4c225307bf18b
Maximilian Schleiss <maxschleiss@solnet.ch>
2007-01-27 11:40:17 +0000

    Fixed it. The \' caracter needs a \ before it.
    (Old svn revision: 654)

--- i18n/news/fr.news.php
+++ i18n/news/fr.news.php
@@ -29,9 +29,9 @@ $news = array (
         'title'   => 'Sortie de Xfce 4.4.0',
         'content' => 'Après plus de deux années de développement, Xfce 4.4.0 vient juste de sortir.'.
                      '[br]'.
-                     'Xfce 4.4 comprend de nouveaux utilitaires dont le très attendu gestionnaire de fichiers Thunar. Xfce 4.4 contient aussi d'énormes améliorations de ses composants de base.'.
+                     'Xfce 4.4 comprend de nouveaux utilitaires dont le très attendu gestionnaire de fichiers Thunar. Xfce 4.4 contient aussi d\'énormes améliorations de ses composants de base.'.
                      '[br][br]'.
-                     'Un tour visuel de Xfce 4.4 est disponible à l'adresse suivante :[br]'.
+                     'Un tour visuel de Xfce 4.4 est disponible à l\'adresse suivante :[br]'.
                      '[link=/about/tour]http://www.xfce.org/about/tour[/link]'.
                      '[br][br]'.
                      'Téléchargez Xfce 4.4 à partir le la section téléchargement :[br]'.
