0080a3eeb42344cd4762931bccb799ac0513e52b
Maximilian Schleiss <maxschleiss@solnet.ch>
2007-01-27 11:36:18 +0000

    broke something
    (Old svn revision: 653)

--- i18n/arrays/fr.content.php
+++ i18n/arrays/fr.content.php
@@ -3,6 +3,6 @@
  * Common string used in website
  **/
  
-$notinlang = 'Cette page n'est pas disponible dans votre<a href="/translate" title="Joignez le projet '.
-             'de traduction i18n de Xfce pour aider à la traduction du site Web.">langue</a>.';
+$notinlang = 'This page is not available in your <a href="/translate" title="Join the Xfce '.
+             'i18n project to help translating this website.">language</a>.';
 ?>
