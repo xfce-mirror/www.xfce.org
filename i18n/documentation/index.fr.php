b5cb581f29b03d518a004752f020112fb84d6203
Maximilian Schleiss <maxschleiss@solnet.ch>
2007-02-07 19:48:33 +0000

    More French translated sections.
    (Old svn revision: 679)

---
+++ i18n/documentation/index.fr.php
@@ -0,0 +1,35 @@
+<h1>Documentations</h1>
+<p>Cette page contient la plupart des documentations Xfce existantes pour l'utilisateur ou le développeur.</p>
+<table width="99%">
+  <tr>
+    <td width="49%" valign="top">
+      <h2><a href="/documentation/4.4/userguide/">Guide de l'utilisateur</a></h2>
+      <p>Nous sommes vraiment désolés mais il n'existe <a href="/development/">pas encore</a> de documentation pour la version 4.4 de Xfce. Vous pouvez chercher dans la <a href="/documentation/4.2/">documentation 4.2</a>, visiter le <a href="http://forum.xfce.org" target="_blank">forum Xfce</a> ou utiliser la <a href="/community/lists">liste de distribution</a> pour résoudre votre problème.</p>
+
+      <h2><a href="/documentation/4.4/api">Développeurs (API)</a></h2>
+      <ul>
+        <li><a href="/documentation/4.4/api/libxfce4mcs/">libxfce4mcs</a></li>
+        <li><a href="/documentation/4.4/api/libxfce4util/">libxfce4util</a></li>
+        <li><a href="/documentation/4.4/api/libxfcegui4/">libxfcegui4</a></li>
+        <li><a href="/documentation/4.4/api/libxfce4panel/">libxfce4panel</a></li>
+        <li><a href="/documentation/4.4/api/libexo/">libexo</a></li>
+        <li><a href="/documentation/4.4/api/thunar-vfs/">thunar-vfs</a></li>
+        <li><a href="/documentation/4.4/api/thunarx/">thunarx</a></li>
+        <li><a href="/documentation/4.4/api/libxfprint/">libxfprint</a></li>
+      </ul>
+          <h2><a href="/documentation/archive">Archives</a></h2>
+          <ul>
+            <li><a href="/documentation/4.2/">Documentations 4.2</a></li>
+            <li><a href="/documentation/4.0/">Documentations 4.0</a></li>
+    </ul></td>
+	<td width="2%">&nbsp;</td>
+    <td width="49%" valign="top">
+      <h2><a href="http://wiki.xfce.org/faq" target="_blank">FAQs</a></h2>
+      <p>Vous pouvez trouver les questions les plus fréquement posées sur Xfce dans le <a href="http://wiki.xfce.org/faq" target="_blank">wiki Xfce</a>.</p>
+      <h2><a href="/documentation/requirements">Dépendances</a></h2>
+          <p>Une liste détaillée des <a href="/documentation/requirements">dépendances</a> de Xfce.</p>
+      <h2><a href="/documentation/changelogs/">Changelogs</a></h2>
+    <p>Tous les <a href="/documentation/changelogs/">changelogs</a> de Xfce 4, par ordre de version.</p>
+    </td>
+  </tr>
+</table>
