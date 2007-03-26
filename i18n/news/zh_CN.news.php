1a63d1665a1bb1ddc0f9d24cbe7dd142a8f644d9
Nick Schermer <nick@xfce.org>
2007-03-26 21:34:37 +0000

    Fix zh_CN new file
    (Old svn revision: 710)

--- i18n/news/zh_CN.news.php
+++ i18n/news/zh_CN.news.php
@@ -22,10 +22,7 @@ $news = array (
         'content' => '<stuff you want to say>',
     ),
 */
-    array (
-        'id'      => 11,
-        'date'    => '21 jan 2007',
-        'author'  => 'Olivier Fourdan',
+    11 = > array (
         'title'   => 'Xfce 4.4.0发布',
         'content' => '经过超过两年的开发，Xfce 4.4.0刚刚发布了。'.
                      '[br]'.
@@ -37,11 +34,8 @@ $news = array (
                      '[br][br]'.
                      '从新的Xfce网站上下载Xfce 4.4：[br]'.
                      '[link=/download]http://www.xfce.org/download[/link]',
-    ),    
-    array (
-        'id'      => 10,
-        'date'    => '15 jan 2007',
-        'author'  => 'Olivier Fourdan',
+    ),
+    10 => array (
         'title'   => 'Xfce 4.2.4发布',
         'content' => 'Xfce 4.2的一个新的错误修正版本可用了。这个版本应该是'.
                      ' 4.2 系列的最后一个版本了。它包含了一些来自于'.
@@ -50,10 +44,7 @@ $news = array (
                      '它只是先前稳定版本的一个错误修正版本。'.
                      '从[link=/download/]本页[/link]得到。',
     ),
-    array (
-        'id'      => 9,
-        'date'    => '5 nov 2006',
-        'author'  => 'Olivier Fourdan',
+    9 => array (
         'title'   => 'Xfce 4.4 Release Candidate 2 (4.3.99.2) 发布',
         'content' => '第二个，但愿也是最后一个Xfce 4.4桌面的发布候选版本现在'.
                      '可供下载了。[br]此版本着重于错误修正和优化。'.
@@ -61,10 +52,7 @@ $news = array (
                      '和更新的清单。请帮助我们以使Xfce 4.4成为史上最好的Xfce 版本，下载它，'.
                      '尝试它，帮助我们完善它！从[link=/download/]本页[/link]得到。',
     ),
-    array (
-        'id'      => 8,
-        'date'    => '3 sep 2006',
-        'author'  => 'Olivier Fourdan',
+    8 => array (
         'title'   => 'Xfce 4.4 Release Candidate 1 (4.3.99.1) 发布',
         'content' => '即将来临的Xfce 4.4桌面的第一个发布候选版本现在可供下载了。'.
                      '此版本修正了许多在第二个测试版中存在的错误，但是也引进了'.
@@ -74,10 +62,7 @@ $news = array (
                      '成为史上最好的Xfce版本，下载它，尝试它，帮助我们完善它！'.
                      '从[link=/download/]本页[/link]得到。',
     ),
-    array (
-        'id'      => 7,
-        'date'    => '10 aug 2006',
-        'author'  => 'Olivier Fourdan',
+    7 => array (
         'title'   => 'Xfce 4.4 beta 2 (4.3.90.2) 发布',
         'content' => 'Xfce 4.4 beta2 (4.3.90.2)现在可供下载了。除了Mousepad和Thunar，'.
                      '这个版本也包括了全新的Xfce归档管理器Xarchiver。除了一大堆'.
@@ -87,54 +72,36 @@ $news = array (
                      '请帮助我们以使Xfce 4.4成为史上最好的Xfce版本，下载它，尝试它，帮助我们完善它！'.
                      '从[link=/download/]本页[/link]得到。',
     ),
-    array (
-        'id'      => 6,
-        'date'    => '17 apr 2006',
-        'author'  => 'Olivier Fourdan',
+    6 => array (
         'title'   => 'Xfce 4.4 beta 1 (4.3.90.1) 发布',
         'content' => 'Xfce 4.4 beta1 (4.3.90.1)现在可供下载了。Xfce 4.4以新的工具为特色，'.
                      '诸如众人所期望的“Thunar”文件管理器，同时还有一些其它核心组件的巨大改进。'.
                      '请帮助我们以使Xfce 4.4成为史上最好的Xfce版本，下载它，'.
                      '尝试它，帮助我们完善它！从本页得到。',
     ),
-    array (
-        'id'      => 5,
-        'date'    => '15 nov 2005',
-        'author'  => 'Olivier Fourdan',
+    5 => array (
         'title'   => 'Xfce 4.2.3.2 发布',
         'content' => '一次“小版本号”的发布，修正了一处在窗口管理器设置的回归问题，'.
                      '从本页的下载位置处之一得到，4.2.3的更新日志在此保持原样。',
     ),
-    array (
-        'id'      => 4,
-        'date'    => '08 nov 2005',
-        'author'  => 'Olivier Fourdan',
+    4 => array (
         'title'   => 'Xfce 4.2.3.1 发布',
         'content' => 'Xfce的一个新的错误修正版本，在几乎6个月之后，终于可供使用了！Xfce 4.2.3.1'.
                      '可以从列在本页上的通常的位置下载得到，并且更新日志'.
                      '也能在此得到。',
     ),
-    array (
-        'id'      => 3,
-        'date'    => '17 may 2005',
-        'author'  => 'Olivier Fourdan',
+    3 => array (
         'title'   => 'Xfce 4.2.2发布',
         'content' => 'Xfce 4.2.2可供使用了。下载地址可以在本页找到，并且更新日志'.
                      '也能在此得到。',
     ),
-    array (
-        'id'      => 2,
-        'date'    => '17 mar 2005',
-        'author'  => 'Olivier Fourdan',
+    2 => array (
         'title'   => 'Xfce 4.2.1.1发布',
         'content' => 'Xfce 4.2.1.1在4.2.1之后很快地发布了。它包含了一个严重错误的修复，错误在于'.
                      '当在 4.2.1中保存会话时，面板会丢失其配置。Sourceforge 服务器'.
                      '已更新了，其他镜像站点也会随之同步。',
     ),
-    array (
-        'id'      => 1,
-        'date'    => '16 mar 2005',
-        'author'  => 'Olivier Fourdan',
+    1 => array (
         'title'   => 'Xfce 4.2.1发布',
         'content' => 'Xfce 4.2.1可供使用了。这是一次维护性发布。下载地址可以在本页找到，'.
                      '并且更新日志也能在本页得到。',
