d49025796a20379518f2379231007feef2b3584f
Mike Massonnet <mmassonnet@xfce.org>
2007-04-20 00:31:58 +0000

    reduce the size of the credits by scripting a little bit with php
    (Old svn revision: 737)

--- i18n/about/credits.en.php
+++ i18n/about/credits.en.php
@@ -1,4 +1,5 @@
 <?php
+include 'include/credits.php';
 function at(){
   echo '<img src="/layout/images/mail.png" alt="" width="10" height="10" hspace="2" border="0" />';
 }
@@ -20,50 +21,20 @@ function at(){
         <th width="40%" align="left"><h3>Contributor</h3></th>
         <th width="60%" align="left"><h3>Main Project(s)</h3></th>
     </tr>
+<?php
+foreach ($credits['core-developers'] as $v)
+{
+  $color = ($color + 1) % 2;
+?>
     <tr>
-        <td width="40%" bgcolor="#eeeeee">Benedikt Meurer [benny<?php at(); ?>xfce.org]</td>
-        <td width="60%" bgcolor="#eeeeee">Thunar, Terminal, Session Manager, Libexo</td>
+        <td width="40%"<?php echo ($color) ? ' bgcolor="#eeeeee"' : ''; ?>>
+                <?php echo $v[0]; ?> [<?php echo $v[1], at(), $v[2]; ?>]</td>
+        <td width="60%"<?php echo ($color) ? ' bgcolor="#eeeeee"' : ''; ?>>
+                <?php echo $v[3]; ?></td>
     </tr>
-    <tr>
-        <td width="40%">Brian J. Tarricone [kelnos<?php at(); ?>xfce.org]</td>
-        <td width="60%">Desktop Manager, Media Player</td>
-    </tr>
-    <tr>
-        <td width="40%" bgcolor="#eeeeee">Danny Milosavljevic [dannym<?php at(); ?>xfce.org]</td>
-        <td width="60%" bgcolor="#eeeeee">Volume Control</td>
-    </tr>
-    <tr>
-        <td width="40%">Eduard Roccatello [eduard<?php at(); ?>xfce.org]</td>
-        <td width="60%">Application Finder</td>
-    </tr>
-    <tr>
-        <td width="40%" bgcolor="#eeeeee">Erik Harrison [erikharrison xfce.org]</td>
-        <td width="60%" bgcolor="#eeeeee">Text Editor</td>
-    </tr>
-    <tr>
-        <td width="40%">Fran&ccedil;ois Le Clainche [fleclainche<?php at(); ?>wanadoo.fr]</td>
-        <td width="60%">Webmaster and artwork</td>
-    </tr>
-    <tr>
-        <td width="40%" bgcolor="#eeeeee">Giuseppe Torelli [colossus<?php at(); ?>xfce.org]</td>
-        <td width="60%" bgcolor="#eeeeee">Archive manager</td>
-    </tr>
-    <tr>
-        <td width="40%">Jasper Huijsmans [jasper<?php at(); ?>xfce.org]</td>
-        <td width="60%">Panel</td>
-    </tr>
-    <tr>
-        <td width="40%" bgcolor="#eeeeee">Jean-Fran&ccedil;ois Wauthy [pollux<?php at(); ?>xfce.org]</td>
-        <td width="60%" bgcolor="#eeeeee">Printer Manager, CD burning application</td>
-    </tr>
-    <tr>
-        <td width="40%">Micka&euml;l Graf [korbinus<?php at(); ?>xfce.org]</td>
-        <td width="60%">Calendar</td>
-    </tr>
-    <tr>
-        <td width="40%" bgcolor="#eeeeee">Olivier Fourdan [fourdan<?php at(); ?>xfce.org]</td>
-        <td width="60%" bgcolor="#eeeeee">Window Manager</td>
-    </tr>
+<?php
+}
+?>
 </table>
 
 <h2 id="server">Server and website </h2>
@@ -72,18 +43,20 @@ function at(){
         <th align="left"><h3>Contributor</h3></th>
         <th align="left"><h3>Contribution</h3></th>
     </tr>
+<?php
+foreach ($credits['server-and-website'] as $v)
+{
+  $color = ($color + 1) % 2;
+?>
     <tr>
-        <td width="40%" bgcolor="#eeeeee">Auke Kok [sofar<?php at(); ?>foo-projects.org]</td>
-        <td width="60%" bgcolor="#eeeeee">Server maintainence</td>
+        <td width="40%"<?php echo ($color) ? ' bgcolor="#eeeeee"' : ''; ?>>
+                <?php echo $v[0]; ?> [<?php echo $v[1], at(), $v[2]; ?>]</td>
+        <td width="60%"<?php echo ($color) ? ' bgcolor="#eeeeee"' : ''; ?>>
+                <?php echo $v[3]; ?></td>
     </tr>
-    <tr>
-        <td width="40%">Brian J. Tarricone [kelnos<?php at(); ?>xfce.org]</td>
-        <td width="60%">Bugzilla administrator</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Nick Schermer [nick<?php at(); ?>xfce.org]</td>
-        <td bgcolor="#eeeeee">Webdesigner</td>
-    </tr>
+<?php
+}
+?>
 </table>
 
 <br />
@@ -96,61 +69,36 @@ function at(){
         <th colspan="2"><h3>Active Contributors</h3></th>
     </tr>
     <tr>
-        <td width="50%" bgcolor="#eeeeee">Darren Salt [linux<?php at(); ?>youmustbejoking.demon.co.uk]</td>
-        <td width="50%" bgcolor="#eeeeee">Jannis Pohlmann [jannis<?php at(); ?>xfce.org]</td>
-    </tr>
-    <tr>
-        <td>Nick Schermer [nick<?php at(); ?>xfce.org]</td>
-        <td>Stephan Arts [stephan<?php at(); ?>xfce.org]</td>
+<?php
+foreach ($credits['contributors-active'] as $v)
+{
+  $line = ($line + 1) % 2;
+  $color = ($line) ? ($color + 1) % 2 : $color;
+?>
+        <td width="40%"<?php echo ($color) ? ' bgcolor="#eeeeee"' : ''; ?>><?php echo $v[0]; ?> [<?php echo $v[1], at(), $v[2]; ?>]</td>
+
+    <?php echo (!$line) ? '</tr>' : ''; ?>
+
+    <?php echo (!$line) ? '<tr>' : ''; ?>
+<?php
+}
+?>
+    <?php echo ($line) ? '<td>&nbsp;</td>' : ''; ?>
     </tr>
     <tr>
         <th colspan="2"><h3>Previous Contributors</h3></th>
     </tr>
+<?php
+foreach ($credits['contributors-previous'] as $v)
+{
+  $color = ($color + 1) % 2;
+?>
     <tr>
-        <td bgcolor="#eeeeee">Bernhard Walle [Bernhard.Walle<?php at(); ?>gmx.de]</td>
+        <td width="40%"<?php echo ($color) ? ' bgcolor="#eeeeee"' : ''; ?>><?php echo $v[0]; ?> [<?php echo $v[1], at(), $v[2]; ?>]</td>
     </tr>
-    <tr>
-    	<td>Biju Chacko [botsie<?php at(); ?>xfce.org]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Craig Betts [craig.betts<?php at(); ?>dfrc.nasa.gov]</td>
-    </tr>
-	<tr>
-		<td>Edscott Wilson Garcia [edscott<?php at(); ?>xfce.org]</td>
-	</tr>
-    <tr>
-        <td bgcolor="#eeeeee">Ejvend Nielsen [prophet<?php at(); ?>sphere-x.net]</td>
-    </tr>
-    <tr>
-        <td>Erik Touve [etouve<?php at(); ?>earthlink.net]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Jens Guballa [j.guballa<?php at(); ?>t-online.de]</td>
-    </tr>
-    <tr>
-        <td>Jens Luedicke [perldude<?php at(); ?>xfce.org]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Joakim Andreasson [joakim.andreasson<?php at(); ?>gmx.net]</td>
-    </tr>
-    <tr>
-        <td>Karsten Luetkewitz [phrep<?php at(); ?>plskthx.org]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Martin Loschwitz [madkiss<?php at(); ?>debian.org]</td>
-    </tr>
-    <tr>
-        <td>Michael Mosier [michael<?php at(); ?>spyonit.com]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Thomas Leonard [tal00r<?php at(); ?>ecs.soton.ac.uk]</td>
-    </tr>
-    <tr>
-        <td>Tobias Henle [tobias<?php at(); ?>page23.de]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Xavier Maillard [zedek<?php at(); ?>gnu.org]</td>
-    </tr>
+<?php
+}
+?>
 </table>
 
 <br />
@@ -170,171 +118,47 @@ function at(){
     <tr>
         <th align="left"><h3>Translations supervision</h3></th>
     </tr>
+<?php
+foreach ($credits['translators-supervision'] as $v)
+{
+  $color = ($color + 1) % 2;
+?>
     <tr>
-        <td bgcolor="#eeeeee">Daichi Kawahata [daichi<?php at(); ?>xfce.org]</td>
+        <td width="40%"<?php echo ($color) ? ' bgcolor="#eeeeee"' : ''; ?>><?php echo $v[0]; ?> [<?php echo $v[1], at(), $v[2]; ?>]</td>
     </tr>
-    <tr>
-        <td>Maximilian Schleiss [maxschleiss<?php at(); ?>solnet.ch]</td>
-    </tr>
-	<tr>
-		<td bgcolor="#eeeeee">Pau Rul·lan Ferragut [paurullan<?php at(); ?>bulma.net]</td>
-	</tr>	
+<?php
+}
+?>
 </table>
 <table width="99%">
     <tr>
         <th width="20%" align="left"><h3>Language</h3></th>
         <th width="80%" align="left"><h3>Contributor(s)</h3></th>
     </tr>
+<?php
+ob_start ();
+at ();
+$at = ob_get_clean ();
+foreach ($credits['translators'] as $v)
+{
+  $color = ($color + 1) % 2;
+?>
     <tr>
-        <td bgcolor="#eeeeee">Arabic [ar]</td>
-        <td bgcolor="#eeeeee">Arabeyes Team [http://wiki.arabeyes.org/Xfce]</td>
+        <td width="40%"<?php echo ($color) ? ' bgcolor="#eeeeee"' : ''; ?>><?php echo $v[0]; ?> [<?php echo $v[1]; ?>]</td>
+        <td width="40%"<?php echo ($color) ? ' bgcolor="#eeeeee"' : ''; ?>>
+<?php
+    $count = 0;
+    foreach ($v[2] as $w)
+    {
+      $count++;
+      $contact = (sizeof ($w) > 2) ? $w[1].$at.$w[2] : $w[1];
+      echo ($count > 1) ? ", " : "";
+      echo "{$w[0]} [$contact]";
+    }
+?>
+        </td>
     </tr>
-    <tr>
-        <td>Belarusian [be]</td>
-        <td>Alexander Nyakhaychyk [nyakhaychyk<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Catalan [ca]</td>
-        <td bgcolor="#eeeeee">Carles Muñoz Gorriz [carlesmu<?php at(); ?>internautas.org], Ivà Burgos [debian.public<?php at(); ?>gmail.com], Pau Rul·lan Ferragut [paurullan<?php at(); ?>bulma.net]</td>
-    </tr>
-    <tr>
-        <td>Czech [cs]</td>
-        <td>Michal Várady [miko.vaji<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Danish [da]</td>
-        <td bgcolor="#eeeeee">Niels Rasmussen [nielsrasmus<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td>German [de]</td>
-        <td>Benedikt Meurer [benny<?php at(); ?>xfce.org], Borja Schwember [mail<?php at(); ?>borja.de], Enrico tröger [enrico.troeger<?php at(); ?>uvena.de], Fabian Nowak [timystery<?php at(); ?>arcor.de], Moritz Heiber [moe<?php at(); ?>xfce.org], Nico Schümann [nico<?php at(); ?>prog.nico22.de]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Dzongkha [dz]</td>
-        <td bgcolor="#eeeeee">Tenzin Dendup [tenzin.dendup<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td>Greek [el]</td>
-        <td>Stathis Kamperis [ekamperi<?php at(); ?>auth.gr], Stavros Giannouris [stavrosg<?php at(); ?>hellug.gr]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">English [en_GB]</td>
-        <td bgcolor="#eeeeee">Jeff Bailes [thepizzaking<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td>Esperanto [eo]</td>
-        <td>Antono Vasiljev [antono.vasiljev<?php at(); ?>gmail.com], Jarbas Araujo [jarbasaraujojr<?php at(); ?>yahoo.com.br], Sylvain Vedrenne [gnu_sylvain<?php at(); ?>xfce.org]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Spanish [es]</td>
-        <td bgcolor="#eeeeee">Rudy Godoy [rudy<?php at(); ?>stone-head.org]</td>
-    </tr>
-    <tr>
-        <td>Estonian [et]</td>
-        <td>Peeter Vois [Peeter.Vois<?php at(); ?>mail.ee]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Basque [eu]</td>
-        <td bgcolor="#eeeeee">Piarres Beobide Egaña [pi<?php at(); ?>beobide.net]</td>
-    </tr>
-    <tr>
-        <td>Finnish [fi]</td>
-        <td>Jari Rahkonen [jari.rahkonen<?php at(); ?>pp1.inet.fi]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">French [fr]</td>
-		<td bgcolor="#eeeeee">Gérald Barré [g.barre<?php at(); ?>free.fr], Jean-François Wauthy [pollux<?php at(); ?>xfce.org], Maximilian Schleiss [maxschleiss<?php at(); ?>solnet.ch], Mike Massonnet [mmassonnet<?php at(); ?>gmail.com], Olivier Fourdan [fourdan<?php at(); ?>xfce.org]</td>
-    </tr>
-    <tr>
-        <td>Galician [gl]</td>
-        <td>Leandro Regueiro [leandro.regueiro<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Hebrew [he]</td>
-        <td bgcolor="#eeeeee">Dotan Kamber [kamberd<?php at(); ?>yahoo.com], Yuval Tanny [tanai<?php at(); ?>int.gov.il]</td>
-    </tr>
-    <tr>
-        <td>Hungarian [hu]</td>
-        <td>Szerv&Atilde;&iexcl;c Attila [sas<?php at(); ?>321.hu]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Italian [it]</td>
-        <td bgcolor="#eeeeee">Fabio Riga [usul<?php at(); ?>aruba.it], Roberto Pariset [r.pariset<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td>Japanese [ja]</td>
-        <td>Daichi Kawahata [daichi<?php at(); ?>xfce.org], Hirotaka Moue [admin<?php at(); ?>mail.dayomon.net]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Georgian [ka]</td>
-        <td bgcolor="#eeeeee">Dimitri Gogelia [delphi.knight<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td>Korean [ko]</td>
-        <td>ByungHyun Choi [byunghyun.choi<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Kurdish [ku]</td>
-        <td bgcolor="#eeeeee">Erdal Ronahi [erdal.ronahi<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td>Lithuanian [lt]</td>
-        <td>Mantas Zapolskas [mantaz<?php at(); ?>users.sf.net]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Dutch [nl]</td>
-        <td bgcolor="#eeeeee">Jasper Huijsmans [jasper<?php at(); ?>xfce.org], Stephan Arts [stephan<?php at(); ?>xfce.org], Vincent Tunru [imnotb<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td>Punjabi [pa]</td>
-        <td>Amanpreet Singh Alam [aalam<?php at(); ?>users.sf.net]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Polish [pl]</td>
-        <td bgcolor="#eeeeee">Piotr Maliński [riklaunim<?php at(); ?>gmail.com], Szymon Kałasz [szymon_maestro<?php at(); ?>gazeta.pl]</td>
-    </tr>
-    <tr>
-        <td>Portuguese [pt_BR]</td>
-        <td>Adriano Winter Bess [awbess<?php at(); ?>gmail.com], Rodrigo Coacci [rcoacci<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Romanian [ro]</td>
-        <td bgcolor="#eeeeee">Mişu Moldovan [dumol<?php at(); ?>gnome.ro]</td>
-    </tr>
-    <tr>
-        <td>Russian [ru]</td>
-        <td>Andrey Fedoseev [andrey.fedoseev<?php at(); ?>gmail.com], Maxim Zenin [webmechanics<?php at(); ?>gmail.com], Sergey Fedoseev [fedoseev.sergey<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Slovak [sk]</td>
-        <td bgcolor="#eeeeee">Roman Moravcik [roman.moravcik<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td>Albanian [sq]</td>
-        <td>Besnik Bleta [besnik<?php at(); ?>programeshqip.org]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Swedish [sv]</td>
-        <td bgcolor="#eeeeee">Alexander Toresson [alexander.toresson<?php at(); ?>gmail.com], Daniel Nylander [po<?php at(); ?>danielnylander.se]</td>
-    </tr>
-    <tr>
-        <td>Turkish [tr]</td>
-        <td>Eren Türkay [Turkay.eren<?php at(); ?>gmail.com], Türker Sezer [turkersezer<?php at(); ?>tsdesign.info]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Ukrainian [uk]</td>
-        <td bgcolor="#eeeeee">Dmitry Nikitin [luckas_fb<?php at(); ?>mail.ru], Maxim V. Dziumanenko [dziumanenko<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td>Vietnamese [vi]</td>
-        <td>Phan Vĩnh Thịnh [teppi82<?php at(); ?>gmail.com]</td>
-    </tr>
-    <tr>
-        <td bgcolor="#eeeeee">Chinese Simplified [zh_CN]</td>
-        <td bgcolor="#eeeeee">Huang Huan [unicon221<?php at(); ?>gmail.com], Hui Ni [shuizhuyuanluo<?php at(); ?>126.com], Jick Nan [jick.nan<?php at(); ?>gmail.com], Shaodong Di [yhlfh<?php at(); ?>yahoo.com.cn], Wu Li [wurisky<?php at(); ?>gmail.com], Yongtao Yang [yongtao.yang<?php at(); ?>telia.com]</td>
-	    </tr>
-    <tr>
-        <td>Chinese Traditional [zh_TW]</td>
-        <td>Cosmo Chene [cosmolax<?php at(); ?>gmail.com], Hydonsingore Cia [hydonsingore<?php at(); ?>mail.educities.edu.tw]</td>
-    </tr>
+<?php
+}
+?>
 </table>
---
+++ include/credits.php
@@ -0,0 +1,305 @@
+<?php
+/* $Id: content.php 668 2007-01-30 15:30:11Z nick $ */
+/* -
+ * Copyright (c) 2007 Mike Massonnet <mmassonnet@xfce.org>
+ *
+ * This php code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This php code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this php code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
+$credits['project-lead'] = array (
+    array ('Olivier Fourdan', 'fourdan', 'xfce.org')
+    );
+
+$credits['core-developers'] = array (
+    array ('Benedikt Meurer', 'benny', 'xfce.org', 'Thunar, Terminal, Session Manager, Libexo',),
+    array ('Brian J. Tarricone', 'kelnos', 'xfce.org', 'Desktop Manager, Media Player',),
+    array ('Danny Milosavljevic', 'dannym', 'xfce.org', 'Volume Control',),
+    array ('Eduard Roccatello', 'eduard', 'xfce.org', 'Application Finder',),
+    array ('Erik Harrison', 'erikharrison ', 'xfce.org', 'Text Editor',),
+    array ('François Le Clainche', 'fleclainche', 'wanadoo.fr', 'Webmaster and artwork',),
+    array ('Giuseppe Torelli', 'colossus', 'xfce.org', 'Archive manager',),
+    array ('Jasper Huijsmans', 'jasper', 'xfce.org', 'Panel',),
+    array ('Jean-François Wauthy', 'pollux', 'xfce.org', 'Printer Manager, CD burning application',),
+    array ('Mickaël Graf', 'korbinus', 'xfce.org', 'Calendar',),
+    array ('Olivier Fourdan', 'fourdan', 'xfce.org', 'Window Manager')
+    );
+
+$credits['server-and-website'] = array (
+    array ('Auke Kok', 'sofar', 'foo-projects.org', 'Server maintainence'),
+    array ('Brian J. Tarricone', 'kelnos', 'xfce.org', 'Bugzilla administrator'),
+    array ('Nick Schermer', 'nick', 'xfce.org', 'Webdesigner')
+    );
+
+$credits['contributors-active'] = array (
+    array ('Darren Salt', 'linux', 'youmustbejoking.demon.co.uk'),
+    array ('Jannis Pohlmann', 'jannis', 'xfce.org'),
+    array ('Nick Schermer', 'nick', 'xfce.org'),
+    array ('Stephan Arts', 'stephan', 'xfce.org')
+    );
+
+$credits['contributors-previous'] = array (
+    array ('Bernhard Walle', 'Bernhard.Walle', 'gmx.de'),
+    array ('Biju Chacko', 'botsie', 'xfce.org'),
+    array ('Craig Betts', 'craig.betts', 'dfrc.nasa.gov'),
+    array ('Edscott Wilson Garcia', 'edscott', 'xfce.org'),
+    array ('Ejvend Nielsen', 'prophet', 'sphere-x.net'),
+    array ('Erik Touve', 'etouve', 'earthlink.net'),
+    array ('Jens Guballa', 'j.guballa', 't-online.de'),
+    array ('Jens Luedicke', 'perldude', 'xfce.org'),
+    array ('Joakim Andreasson', 'joakim.andreasson', 'gmx.net'),
+    array ('Karsten Luetkewitz', 'phrep', 'plskthx.org'),
+    array ('Martin Loschwitz', 'madkiss', 'debian.org'),
+    array ('Michael Mosier', 'michael', 'spyonit.com'),
+    array ('Thomas Leonard', 'tal00r', 'ecs.soton.ac.uk'),
+    array ('Tobias Henle', 'tobias', 'page23.de'),
+    array ('Xavier Maillard', 'zedek', 'gnu.org')
+    );
+
+$credits['goodies'] = array (
+    array ('Jannis Pohlmann', 'jannis', 'xfce.org')
+    );
+
+$credits['translators-supervision'] = array (
+    array ('Daichi Kawahata', 'daichi', 'xfce.org'),
+    array ('Maximilian Schleiss', 'maxschleiss', 'solnet.ch'),
+    array ('Pau Rul·lan Ferragut', 'paurullan', 'bulma.net')
+    );
+
+$credits['translators'] = array (
+    array ('Arabic', 'ar',
+        array (
+            array ('Arabeyes Team', 'http://wiki.arabeyes.org/Xfce')
+            )
+        ),
+    array ('Belarusian', 'be',
+        array (
+            array ('Alexander Nyakhaychyk', 'nyakhaychyk', 'gmail.com')
+            )
+        ),
+    array ('Catalan', 'ca',
+        array (
+            array ('Carles Muñoz Gorriz', 'carlesmu', 'internautas.org'), 
+            array ('Ivà Burgos', 'debian.public', 'gmail.com'), 
+            array ('Pau Rul·lan Ferragut', 'paurullan', 'bulma.net')
+            )
+        ),
+    array ('Czech', 'cs',
+        array (
+            array ('Michal Várady', 'miko.vaji', 'gmail.com')
+            )
+        ),
+    array ('Danish', 'da',
+        array (
+            array ('Niels Rasmussen', 'nielsrasmus', 'gmail.com')
+            )
+        ),
+    array ('German', 'de',
+        array (
+            array ('Benedikt Meurer', 'benny', 'xfce.org'),
+            array ('Borja Schwember', 'mail', 'borja.de'),
+            array ('Enrico tröger', 'enrico.troeger', 'uvena.de'),
+            array ('Fabian Nowak', 'timystery', 'arcor.de'),
+            array ('Moritz Heiber', 'moe', 'xfce.org'),
+            array ('Nico Schümann', 'nico', 'prog.nico22.de')
+            )
+        ),
+    array ('Dzongkha', 'dz',
+        array (
+            array ('Tenzin Dendup', 'tenzin.dendup', 'gmail.com')
+            )
+        ),
+    array ('Greek', 'el',
+        array (
+            array ('Stathis Kamperis', 'ekamperi', 'auth.gr'),
+            array ('Stavros Giannouris', 'stavros', 'ghellug.gr')
+            )
+        ),
+    array ('English', 'en_GB',
+        array (
+            array ('Jeff Bailes', 'thepizzaking', 'gmail.com')
+            )
+        ),
+    array ('Esperanto', 'eo',
+        array (
+            array ('Antono Vasiljev', 'antono.vasiljev', 'gmail.com'),
+            array ('Jarbas Araujo', 'jarbasaraujojr', 'yahoo.com.br'),
+            array ('Sylvain Vedrenne', 'gnu_sylvain', 'xfce.org')
+            )
+        ),
+    array ('Spanish', 'es',
+        array (
+            array ('Rudy Godoy', 'rudy', 'stone-head.org')
+            )
+        ),
+    array ('Estonian', 'et',
+        array (
+            array ('Peeter Vois', 'Peeter.Vois', 'mail.ee')
+            )
+        ),
+    array ('Basque', 'eu',
+        array (
+            array ('Piarres Beobide Egaña', 'pi', 'beobide.net')
+            )
+        ),
+    array ('Finnish', 'fi',
+        array (
+            array ('Jari Rahkonen', 'jari.rahkonen', 'pp1.inet.fi')
+            )
+        ),
+    array ('French', 'fr',
+        array (
+            array ('Gérald Barré', 'g.barre', 'free.fr'),
+            array ('Jean-François Wauthy', 'pollux', 'xfce.org'),
+            array ('Maximilian Schleiss', 'maxschleiss', 'solnet.ch'),
+            array ('Mike Massonnet', 'mmassonnet', 'gmail.com'),
+            array ('Olivier Fourdan', 'fourdan', 'xfce.org')
+            )
+        ),
+    array ('Galician', 'gl',
+        array (
+            array ('Leandro Regueiro', 'leandro.regueiro', 'gmail.com')
+            )
+        ),
+    array ('Hebrew', 'he',
+        array (
+            array ('Dotan Kamber', 'kamberd', 'yahoo.com'),
+            array ('Yuval Tanny', 'tanai', 'int.gov.il')
+            )
+        ),
+    array ('Hungarian', 'hu',
+        array (
+            array ('Szervác Attila', 'sas', '321.hu')
+            )
+        ),
+    array ('Italian', 'it',
+        array (
+            array ('Fabio Riga', 'usul', 'aruba.it'),
+            array ('Roberto Pariset', 'r.pariset', 'gmail.com')
+            )
+        ),
+    array ('Japanese', 'ja',
+        array (
+            array ('Daichi Kawahata', 'daichi', 'xfce.org'),
+            array ('Hirotaka Moue', 'admin', 'mail.dayomon.net')
+            )
+        ),
+    array ('Georgian', 'ka',
+        array (
+            array ('Dimitri Gogelia', 'delphi.knight', 'gmail.com')
+            )
+        ),
+    array ('Korean', 'ko',
+        array (
+            array ('ByungHyun Choi', 'byunghyun.choi', 'gmail.com')
+            )
+        ),
+    array ('Kurdish', 'ku',
+        array (
+            array ('Erdal Ronahi', 'erdal.ronahi', 'gmail.com')
+            )
+        ),
+    array ('Lithuanian', 'lt',
+        array (
+            array ('Mantas Zapolskas', 'mantazusers.sf.net')
+            )
+        ),
+    array ('Dutch', 'nl',
+        array (
+            array ('Jasper Huijsmans', 'jasper', 'xfce.org'),
+            array ('Stephan Arts', 'stephan', 'xfce.org'),
+            array ('Vincent Tunru', 'imnotb', 'gmail.com')
+            )
+        ),
+    array ('Punjabi', 'pa',
+        array (
+            array ('Amanpreet Singh Alam', 'aalam', 'users.sf.net')
+            )
+        ),
+    array ('Polish', 'pl',
+        array (
+            array ('Piotr Maliński', 'riklaunim', 'gmail.com'),
+            array ('Szymon Kałasz', 'szymon_maestro', 'gazeta.pl')
+            )
+        ),
+    array ('Portuguese', 'pt_BR',
+        array (
+            array ('Adriano Winter Bess', 'awbess', 'gmail.com'),
+            array ('Rodrigo Coacci', 'rcoacci', 'gmail.com')
+            )
+        ),
+    array ('Romanian', 'ro',
+        array (
+            array ('Mişu Moldovan', 'dumol', 'gnome.ro')
+            )
+        ),
+    array ('Russian', 'ru',
+        array (
+            array ('Andrey Fedoseev', 'andrey.fedoseev', 'gmail.com'),
+            array ('Maxim Zenin', 'webmechanics', 'gmail.com'),
+            array ('Sergey Fedoseev', 'fedoseev.sergey', 'gmail.com')
+            )
+        ),
+    array ('Slovak', 'sk',
+        array (
+            array ('Roman Moravcik', 'roman.moravcik', 'gmail.com')
+            )
+        ),
+    array ('Albanian', 'sq',
+        array (
+            array ('Besnik Bleta', 'besnik', 'programeshqip.org')
+            )
+        ),
+    array ('Swedish', 'sv',
+        array (
+            array ('Alexander Toresson', 'alexander.toresson', 'gmail.com'),
+            array ('Daniel Nylander', 'po', 'danielnylander.se')
+            )
+        ),
+    array ('Turkish', 'tr',
+        array (
+            array ('Eren Türkay', 'Turkay.eren', 'gmail.com'),
+            array ('Türker Sezer', 'turkersezer', 'tsdesign.info')
+            )
+        ),
+    array ('Ukrainian', 'uk',
+        array (
+            array ('Dmitry Nikitin', 'luckas_fb', 'mail.ru'),
+            array ('Maxim V. Dziumanenko', 'dziumanenko', 'gmail.com')
+            )
+        ),
+    array ('Vietnamese', 'vi',
+        array (
+            array ('Phan Vĩnh Thịnh', 'teppi82', 'gmail.com')
+            )
+        ),
+    array ('Chinese Simplified', 'zh_CN',
+        array (
+            array ('Huang Huan', 'unicon221', 'gmail.com'), 
+            array ('Hui Ni', 'shuizhuyuanluo', '126.com'), 
+            array ('Jick Nan', 'jick.nan', 'gmail.com'), 
+            array ('Shaodong Di', 'yhlfh', 'yahoo.com.cn'), 
+            array ('Wu Li', 'wurisky', 'gmail.com'), 
+            array ('Yongtao Yang', 'yongtao.yangtelia.com')
+            )
+        ),
+    array ('Chinese Traditional', 'zh_TW',
+        array (
+            array ('Cosmo Chene', 'cosmolax', 'gmail.com'),
+            array ('Hydonsingore Cia', 'hydonsingore', 'mail.educities.edu.tw')
+            )
+        )
+    );
+
