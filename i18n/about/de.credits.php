d1db4f08b7e7a9c4419a7b9a5046c4ce2f0e4f0b
Maximilian Schleiss <maxschleiss@solnet.ch>
2007-04-20 20:40:59 +0000

    Updated the new credits pages for a few languages. (unfinished work\!)
    (Old svn revision: 741)

---
+++ i18n/about/de.credits.php
@@ -0,0 +1,62 @@
+<?php
+$credits['core-developers-i18n'] = array (
+    'benny' => 'Thunar, Terminal, Sitzungsverwaltung, Libexo',
+    'kelnos' => 'Arbeitsoberflächenverwaltung, Medienspieler',
+    'dannym' => 'Lautstärkeregler',
+    'eduard' => 'Programmsuche',
+    'erikharrison ' => 'Texteditor',
+    'fleclainche' => 'Webmaster und Artwork',
+    'colossus' => 'Archivierungsprogramm',
+    'jasper' => 'Leiste',
+    'pollux' => 'Druckerverwaltung, CD-Brennprogramm',
+    'korbinus' => 'Kalender',
+    'fourdan' => 'Fensterverwaltung'
+    );
+
+$credits['server-and-website-i18n'] = array (
+    'sofar' => 'Server-Instandhaltung',
+    'kelnos' => 'Bugzilla-Administrator',
+    'nick' => 'Webdesigner'
+    );
+
+$credits['translators-i18n'] = array (
+    'ar' => 'Arabisch',
+    'be' => 'Belarusian',
+    'ca' => 'Katalanisch',
+    'cs' => 'Tschechisch',
+    'da' => 'Dänisch',
+    'de' => 'Deutsch',
+    'dz' => 'Dzongkha',
+    'el' => 'Griechisch',
+    'en_GB' => 'Englisch',
+    'eo' => 'Esperanto',
+    'es' => 'Spanisch',
+    'et' => 'Estonisch',
+    'eu' => 'Baskisch',
+    'fi' => 'Finnisch',
+    'fr' => 'Französisch',
+    'gl' => 'Galician',
+    'he' => 'Hebräisch',
+    'hu' => 'Ungarisch',
+    'it' => 'Italienisch',
+    'ja' => 'Japanisch',
+    'ka' => 'Georgian',
+    'ko' => 'Koreanisch',
+    'ku' => 'Kurdisch',
+    'lt' => 'Litauisch',
+    'nl' => 'Niederländisch',
+    'pa' => 'Punjabi',
+    'pl' => 'Polnisch',
+    'pt_BR' => 'Portugiesisch',
+    'ro' => 'Rumänisch',
+    'ru' => 'Russisch',
+    'sk' => 'Slowakisch',
+    'sq' => 'Albanian',
+    'sv' => 'Schwedisch',
+    'tr' => 'Türkisch',
+    'uk' => 'Ukrainisch',
+    'vi' => 'Vietnamesisch',
+    'zh_CN' => 'Chinesisch',
+    'zh_TW' => 'Chinesisch'
+    );
+?>
---
+++ i18n/about/nl.credits.php
@@ -0,0 +1,62 @@
+<?php
+$credits['core-developers-i18n'] = array (
+    'benny' => 'Thunar, Terminal, Session Manager, Libexo',
+    'kelnos' => 'Desktop Manager, Media Player',
+    'dannym' => 'Volume Control',
+    'eduard' => 'Application Finder',
+    'erikharrison ' => 'Text Editor',
+    'fleclainche' => 'Webmaster and artwork',
+    'colossus' => 'Archive manager',
+    'jasper' => 'Panel',
+    'pollux' => 'Printer Manager, CD burning application',
+    'korbinus' => 'Calendar',
+    'fourdan' => 'Window Manager'
+    );
+
+$credits['server-and-website-i18n'] = array (
+    'sofar' => 'Server maintainence',
+    'kelnos' => 'Bugzilla administrator',
+    'nick' => 'Webdesigner'
+    );
+
+$credits['translators-i18n'] = array (
+    'ar' => 'Arabisch',
+    'be' => 'Wit-Russisch',
+    'ca' => 'Catalaans',
+    'cs' => 'Tsjechisch',
+    'da' => 'Deens',
+    'de' => 'Duits',
+    'dz' => 'Dzongkha',
+    'el' => 'Grieks',
+    'en_GB' => 'Engels',
+    'eo' => 'Esperanto',
+    'es' => 'Spaans',
+    'et' => 'Estisch',
+    'eu' => 'Baskisch',
+    'fi' => 'Fins',
+    'fr' => 'Frans',
+    'gl' => 'Galicisch',
+    'he' => 'Hebreeuws',
+    'hu' => 'Hongaars',
+    'it' => 'Italiaans',
+    'ja' => 'Japans',
+    'ka' => 'Georgisch',
+    'ko' => 'Koreaans',
+    'ku' => 'Koerdisch',
+    'lt' => 'Litouws',
+    'nl' => 'Nederlands',
+    'pa' => 'Punjabi',
+    'pl' => 'Pools',
+    'pt_BR' => 'Portugees',
+    'ro' => 'Roemeens',
+    'ru' => 'Russisch',
+    'sk' => 'Slowaaks',
+    'sq' => 'Albanees',
+    'sv' => 'Zweeds',
+    'tr' => 'Turks§',
+    'uk' => 'Oekraïens',
+    'vi' => 'Vietnamees',
+    'zh_CN' => 'Vereenvoudigd Chinees',
+    'zh_TW' => 'Traditioneel Chinees'
+    );
+?>
