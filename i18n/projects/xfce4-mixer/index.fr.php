5601076d0476bf3b1733ae7d1a6723f9f0377cf2
Mike Massonnet <mmassonnet@xfce.org>
2007-04-24 17:48:22 +0000

    correction de qques typos
    (Old svn revision: 743)

--- i18n/projects/xfce4-mixer/index.fr.php
+++ i18n/projects/xfce4-mixer/index.fr.php
@@ -1,9 +1,9 @@
 <div style="float:right; margin:0 0 0.5em 0.5em;"><img src="/images/projects/xfce4-mixer.png" alt="xfce4-mixer" width="48" height="48" /></div>
 
-<h1>contrôle du volume</h1>
-<p>Le contrôle du volume, dans l'environnement de bureau Xfce, se fait via xfce4-mixer, une application servant à régler les volumes des différentes sorties de la carte son. Ce paquet contient un greffon de coontrôle du volume pour le panneau Xfce mais aussi une application de réglages des différents volumes.</p>
+<h1>Contrôle du volume</h1>
+<p>Le contrôle du volume, dans l'environnement de bureau Xfce, se fait via Xfce4 mixer, une application servant à régler les volumes des différentes sorties de la carte son. Ce paquet contient un greffon de contrôle du volume pour le panneau Xfce mais aussi une application de réglages des différents volumes.</p>
 
-<p>L'application de contrôle des volumes supporte les ressources suivantes :</p>
+<p>L'application de contrôle des volumes supporte les pilotes suivantes :</p>
 
 <ul>
   <li><strong>OSS - Open Sound System</strong><br />
@@ -17,7 +17,7 @@
 </ul>
 
 <h2>Liens externes</h2>
-<p>Cette application fait partie intégrante de l'environnement de bureau Xfce.</p>
+<p>Cette application fait partie intégrante de l'environnement du bureau Xfce.</p>
 <h3>Subversion</h3>
 <p>
   <a class="external" target="_blank" href="http://svn.xfce.org/listing.php?repname=xfce4&amp;path=%2Fxfce4-mixer%2F&amp;rev=0&amp;sc=0#/xfce4-mixer/">WebSVN</a><br />
