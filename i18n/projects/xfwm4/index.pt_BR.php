8d198c9c113fd58b991628472719630b8efd6587
Maximilian Schleiss <maxschleiss@solnet.ch>
2007-07-03 23:21:17 +0000

    Added Brazilian Portuguese translations for the projects pages.
    (Old svn revision: 757)

---
+++ i18n/projects/xfwm4/index.pt_BR.php
@@ -0,0 +1,35 @@
+<div style="float:right; margin:0 0 0.5em 0.5em;"><img src="/images/projects/xfwm4.png" alt="xfwm4" width="48" height="48" /></div>
+
+<h1>Gerenciador de Janelas</h1>
+<p>O gerenciador de janelas do Xfce 4 gerencia o posicionamento das janelas
+  das aplicações na tela, provê belas decorações de janelas, gerencia
+  áreas de trabalho ou desktops virtuais, e suporta o modo multi-tela
+  nativamente. Provê seu próprio gerenciador de composição (da extensão de
+  composição Xorg) para transparências e sombras reais. O gerenciador de janelas do Xfce 4
+  inclui um editor de atalhos do teclado para comandos específicos do usuário e
+  manipulações básicas de janelas, e provê um diálogo para ajustes avançados.</p>
+
+<h2>Funcionalidades</h2>
+<ul>
+  <li>Suporte Multihead nativo para Xinerama e Twinview.</li>
+  <li>Suporte à muitos formatos de imagem para decoração de janelas (xpm, png, svg, bmp, jpg,
+    gif).</li>
+  <li>Editor de atalhos para manipulação de janelas e áreas de trabalho, e para
+    comandos específicos do usuário.</li>
+  <li>Possibilidades de foco avançadas.</li>
+  <li>Resistência ou magnetização de janelas e da área de trabalho.</li>
+  <li>Compositor próprio para janelas transparentes e sombras.</li>
+  <li>Roda do mouse sobre o desktop troca de área de trabalho.</li>
+</ul>
+
+<h2>Endereços Externos</h2>
+<h3>Subversion</h3>
+<p><a class="external" target="_blank" href="http://svn.xfce.org/listing.php?repname=xfce4&amp;path=%2Fxfwm4%2F&amp;rev=0&amp;sc=0#/xfwm4/">WebSVN</a><br />
+  <a class="external" target="_blank" href="http://svn.xfce.org/svn/xfce/xfwm4/">SVN</a></p>
+<p><code class="code">svn co http://svn.xfce.org/svn/xfce/xfwm4/trunk xfwm4</code></p>
+
+<h2>Autor</h2>
+<ul>
+  <li>Olivier Fourdan [fourdan at xfce.org]</li>
+</ul>
+
