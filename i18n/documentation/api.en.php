02ecab5aff69e57df2e9510d781f4e71a5a266f2
Nick Schermer <nick@xfce.org>
2007-07-10 09:17:09 +0000

    update
    (Old svn revision: 764)

---
+++ i18n/documentation/api.en.php
@@ -0,0 +1,16 @@
+<h1>Xfce API documentation</h1>
+<p>The following API docs cover the libraries in Xfce 4.4. The documents are cross-referenced with the
+GTK+ reference manuals, which you can find <a href="http://gtk.org/api/">here</a>.</p>
+
+<ul>
+    <li><a href="/documentation/api/libxfce4mcs/">libxfce4mcs</a> (<a href="/documentation/api/libxfce4mcs-docs.tar.gz">tarball</a>)</li>
+    <li><a href="/documentation/api/libxfce4util/">libxfce4util</a> (<a href="/documentation/api/libxfce4util-docs.tar.gz">tarball</a>)</li>
+    <li><a href="/documentation/api/libxfcegui4/">libxfcegui4</a> (<a href="/documentation/api/libxfcegui4-docs.tar.gz">tarball</a>)</li>
+    <li><a href="/documentation/api/libxfce4menu/">libxfce4menu</a> (<a href="/documentation/api/libxfce4menu-docs.tar.gz">tarball</a>)</li>
+    <li><a href="/documentation/api/libxfce4panel/">libxfce4panel</a> (<a href="/documentation/api/libxfce4panel-docs.tar.gz">tarball</a>)</li>
+    <li><a href="/documentation/api/libsqueeze/">libsqueeze</a> (<a href="/documentation/api/libsqueeze-docs.tar.gz">tarball</a>)</li>
+    <li><a href="/documentation/api/exo/">exo</a> (<a href="/documentation/api/exo-docs.tar.gz">tarball</a>)</li>
+    <li><a href="/documentation/api/thunar-vfs/">thunar-vfs</a> (<a href="/documentation/api/thunar-vfs-docs.tar.gz">tarball</a>)</li>
+    <li><a href="/documentation/api/thunarx/">thunarx</a> (<a href="/documentation/api/thunarx-docs.tar.gz">tarball</a>)</li>
+    <li><a href="/documentation/api/libxfprint/">libxfprint</a> (<a href="/documentation/api/libxfprint-docs.tar.gz">tarball</a>)</li>
+</ul>
