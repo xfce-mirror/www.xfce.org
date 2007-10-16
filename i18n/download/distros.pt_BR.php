d3d056eb969087782f1fb632e7e659f68ac9b043
Mike Massonnet <mmassonnet@xfce.org>
2007-10-16 11:25:29 +0000

    Add pt_BR download/distributions page (Fernando Maróstica <fmarostica@fmarostica.org>)
    (Old svn revision: 788)

---
+++ i18n/download/distros.pt_BR.php
@@ -0,0 +1,159 @@
+<h1>Distribuições que utilizam o Xfce como padrão</h1>
+<p>Aqui está uma lista das distribuições que utilizam o Xfce como ambiente de desktop padrão.</p>
+
+<h2>SAM Linux Desktop</h2>
+<table border="0" cellspacing="10" cellpadding="0">
+	<tr>
+		<td>
+			<a href="http://www.sam-linux.org/" target="_blank" title="Visite o website do Sam Linux">
+			<img src="/images/download/distros/sam.png" alt="Sam Linux" border="0"/></a>
+                </td>
+		<td>
+			SAM Linux Desktop é um LiveCD que contém um sistema operacional Linux completo para desktop.
+			Não é necessário realizar a instalação do sistema em seu disco rígido, você pode executá-lo através
+			da unidade de CD-ROM e da memória RAM.
+                </td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>
+			<a href="http://www.sam-linux.org/" target="_blank" title="Visite o website do Sam Linux">Website</a> | 
+			<a href="http://distrowatch.com/sam" target="_blank" title="Visite a página do Sam Linux em DistroWatch">DistroWatch</a>
+                </td>
+	</tr>
+</table>
+
+<h2>Xubuntu</h2>
+<table border="0" cellspacing="10" cellpadding="0">
+	<tr>
+		<td>
+			<a href="http://www.xubuntu.org/" target="_blank" title="Visite o website do Xubuntu">
+			<img src="/images/download/distros/xubuntu.png" alt="Xubuntu" border="0"/></a>
+                </td>
+		<td>
+			Xubuntu é uma distribuição GNU/Linux baseada no Ubuntu. Entretando ao contrário do Ubuntu, o Xubuntu
+                        é uma distribuição otimizada para as máquinas que possuem poucos recursos e utiliza o leve e rápido
+			ambiente de desktop Xfce. Quando possível a distribuição inclui apenas aplicações GTK+.
+                </td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>
+			<a href="http://www.xubuntu.org/" target="_blank" title="Visite o website do Xubuntu">Website</a> | 
+			<a href="http://distrowatch.com/xubuntu" target="_blank" title="Visite a página do Xubuntu em DistroWatch">DistroWatch</a>
+                </td>
+	</tr>
+</table>
+
+<h2>Xfld</h2>
+<table border="0" cellspacing="10" cellpadding="0">
+	<tr>
+		<td>
+			<a href="http://www.xfld.org/" target="_blank" title="Visite o website do Xfld">
+			<img src="/images/download/distros/xfld.png" alt="Xfld" border="0"/></a>
+                </td>
+		<td>
+			Xfld - 'Xfce live demo', é um LiveCD, que demonstra a última versão do ambiente de desktop Xfce
+			e fornece um completo sistema operacional GNU/Linux (baseado no Ubuntu), que pode ser executado
+			diretamente apartir da unidade de CD-ROM.
+                </td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>
+			<a href="http://www.xfld.org/" target="_blank" title="Visite o website do Xfld">Website</a> | 
+			<a href="http://distrowatch.com/xfld" target="_blank" title="Visite a página do Xfld em DistroWatch">DistroWatch</a>
+                </td>
+	</tr>
+</table>
+
+<h2>Zenwalk</h2>
+<table border="0" cellspacing="10" cellpadding="0">
+	<tr>
+		<td>
+			<a href="http://www.zenwalk.org/" target="_blank" title="Visite o website do Zenwalk">
+			<img src="/images/download/distros/zenwalk.png" alt="Zenwalk" border="0"/></a>
+                </td>
+                <td>
+			Zenwalk é uma distribuição GNU/Linux baseada no Slackware. A meta do Zenwalk é ser leve e rápido
+			utilizando apenas uma aplicação por tarefa que combina com o ambiente de desktop Xfce.
+			Zenwalk utiliza a mais recente tecnologia Linux, com um completo ambiente de programação e bibliotecas
+			que fornece uma plataforma ideal para programadores.
+			A perspectiva modular do Zenwalk oferece uma maneira simples para converter o Zenwalk Linux num moderno
+                        servidor (e.g LAMP, mensagens, compartilhamento de arquivos). 
+                </td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>
+			<a href="http://www.zenwalk.org/" target="_blank" title="Visite o website do Zenwalk">Website</a> | 
+			<a href="http://distrowatch.com/zenwalk" target="_blank" title="Visite a página do Zenwalk em DistroWatch">DistroWatch</a>
+                </td>
+	</tr>
+</table>
+
+<h2>Lunar Linux</h2>
+<table border="0" cellspacing="10" cellpadding="0">
+	<tr>
+		<td>
+			<a href="http://www.lunar-linux.org/" target="_blank" title="Visite o website do Lunar Linux">
+			<img src="/images/download/distros/lunar.png" alt="Lunar Linux" border="0"/></a>
+                </td>
+                <td>
+			Lunar Linux é uma distribuição baseada em código-fonte. Apesar de não existir um ambiente de desktop padrão
+                        no Lunar, ele possui um excelente suporte ao Xfce, principalmente porque alguns desenvolvedores do Xfce 
+			também são desenvolvedores do Lunar. <br />
+			Outro ponto considerável: todos os servidores do projeto Xfce utilizam Lunar Linux!
+                </td> 
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>
+			<a href="http://www.lunar-linux.org/" target="_blank" title="Visite o website do Lunar Linux">Website</a> | 
+			<a href="http://distrowatch.com/lunar" target="_blank" title="Visite a página do Lunar Linux em DistroWatch">DistroWatch</a>
+                </td>
+	</tr>
+</table>
+
+<h2>Kate OS</h2>
+<table border="0" cellspacing="10" cellpadding="0">
+        <tr>
+                <td>
+			<a href="http://www.kateos.org/" target="_blank" title="Visite o website do Kate OS">
+			<img src="/images/download/distros/kateos.png" alt="Kate OS" border="0"/></a>
+                </td>
+                <td>
+                        Kate OS é uma distribuição GNU/Linux polonesa, a distribuição é binária, leve e utiliza o ambiente de desktop Xfce como padrão.
+                </td>
+        </tr>
+        <tr>
+                <td>&nbsp;</td>
+                <td>
+			<a href="http://www.kateos.org/" target="_blank" title="Visite o website do Kate OS">Website</a> | 
+			<a href="http://distrowatch.com/kateos" target="_blank" title="Visite a página do Kate OS em DistroWatch">DistroWatch</a>
+                </td>
+        </tr>
+</table>
+
+<h2>Kwort Linux</h2>
+<table border="0" cellspacing="10" cellpadding="0">
+        <tr>
+                <td>
+			<a href="http://www.kwort.org/" target="_blank" title="Visite o website do Kwort Linux">
+			<img src="/images/download/distros/kwortlinux.png" alt="Kwort Linux" border="0"/></a>
+                </td>
+                <td>
+                	Kwort é uma distribuição GNU/Linux moderna, rápida e fácil de usar que combina o Xfce
+                	e algumas outras ferramentas úteis para desktop poderoso.
+                	Kwort é baseado na distribuição Slackware, que é robusta, limpa e fácil de estender.
+                </td>
+
+        </tr>
+        <tr>
+                <td>&nbsp;</td>
+                <td>
+			<a href="http://www.kwort.org/" target="_blank" title="Visite o website do Kwort Linux">Website</a> |
+			<a href="http://distrowatch.com/kwort" target="_blank" title="Visite a página do Kwort Linux em DistroWatch">DistroWatch</a>
+		</td>
+        </tr>
+</table>
