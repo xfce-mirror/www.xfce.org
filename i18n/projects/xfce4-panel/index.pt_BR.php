6c3b74356011424405486f41be6c0ca2666b30df
Mike Massonnet <mmassonnet@xfce.org>
2007-10-17 08:49:37 +0000

    Update pt_BR translation for the Xfce4 panel webpage project.
    (Old svn revision: 789)

---
+++ i18n/projects/xfce4-panel/index.pt_BR.php
@@ -0,0 +1,58 @@
+<div style="float:right; margin:0 0 0.5em 0.5em;"><img src="/images/projects/xfce4-panel.png" alt="xfce4-panel" width="48" height="48" /></div>
+
+<h1>Painel</h1>
+<p>O painel do Xfce versão 4 oferece suporte a múltiplos painéis, com diversas opções de
+posicionamento, aparência, transparência e comportamento. Fornece diversos itens
+por padrão, como lançadores de aplicação com menus destacáveis, páginador gráfico,
+lista de tarefas, relógio,  bandeja de sistema, um alternador de desktop que exibe
+e oculta o desktop e ainda mais.
+Através da utilização de uma janela de diálogo o painel oferece uma maneira fácil
+de adicionar itens e também permite que o itens possam ser movidos para painéis diferentes.
+É compatível com os numerosos plugins para o painel disponível na
+<a href="http://goodies.xfce.org/">página do projeto Goodies do Xfce</a>.</p>
+
+<h2>Características</h2>
+<ul>
+  <li><strong>Múltiplos painéis</strong> com suporte a diversas telas.</li>
+</ul>
+<ul>
+  <li><strong>Aparência personalizada</strong>:
+    posição,
+    tamanho,
+    ocultar automaticamente e 
+    transparência utilizando a extenção composite do Xorg.</li>
+</ul>
+<ul>
+  <li><strong>Plugins</strong>:
+    menu, 
+    lançadores de aplicação, 
+    lista de tarefas, 
+    relógio, 
+    bandeja do sistema, 
+    paginador gráfico, 
+    alternador de desktop e
+    sair do sistema ou bloquear tela.</li>
+</ul>
+<ul>
+  <li><strong>Plugins avançados para o painel</strong>: 
+    monitoramento do sistema e rede,
+    gerenciador da área de transferência,
+    notas,
+    entrada de linha de comando,
+    data e hora,
+    e outros plugins úteis para monitorar a bateria do laptop, estado do Wi-Fi,
+    ou ainda monitorar a frequência da CPU...</li>
+</ul>
+
+<h2>Links Externos</h2>
+<p><a href="http://goodies.xfce.org/projects/panel-plugins/start" target="_blank" class="external">
+Plugins para o painel</a> na página do projeto Goodies</p>
+<h3>Subversion</h3>
+<p><a class="external" target="_blank" href="http://svn.xfce.org/listing.php?repname=xfce4&amp;path=%2Fxfce4-panel%2F&amp;rev=0&amp;sc=0#/xfce4-panel/">WebSVN</a><br />
+<a class="external" target="_blank" href="http://svn.xfce.org/svn/xfce/xfce4-panel/">SVN</a></p>
+<p><code class="code">svn co http://svn.xfce.org/svn/xfce/xfce4-panel/trunk xfce4-panel</code></p>
+
+<h2>Autor</h2>
+<ul>
+  <li>Jasper Huijsmans [jasper at xfce.org]</li>
+</ul>
