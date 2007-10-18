2a0f337ff327a0a34651bb493fb4196349d8d3f5
Mike Massonnet <mmassonnet@xfce.org>
2007-10-18 16:49:18 +0000

    Update pt_BR translation for the xfprint project.
    (Old svn revision: 792)

---
+++ i18n/projects/xfprint/pt_BR.nav.php
@@ -0,0 +1,17 @@
+<ul>
+	<li><a href="/projects#core">Principais Componentes (Núcleo)</a>
+		<ul>
+		    <li><a href="/projects/xfdesktop">Gerenciador de Desktop</a></li>
+		    <li><a href="/projects/xfce4-panel">Painel</a></li>
+		    <li><a href="/projects/xfprint">Assistente de Impressão</a></li>
+		    <li><a href="/projects/xfce4-session">Gerenciador de Sessão</a></li>
+		    <li><a href="/projects/xfce-mcs-manager">Gerenciador de Configurações</a></li>
+		    <li><a href="/projects/xfce-utils">Utilitários e Scripts</a></li>
+		    <li><a href="/projects/xfwm4">Gerenciador de Janela</a></li>
+		    <li><a href="/projects/libraries">Bibliotecas do Xfce</a></li>
+		</ul>
+	</li>
+	<li><a href="/projects#applications">Aplicações</a></li>
+	<li><a href="/projects#framework">Framework</a></li>
+	<li><a href="/projects#art">Arte e Gráficos</a></li>
+</ul>
