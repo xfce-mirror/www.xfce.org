f9cfdc659258d14452d13bc842933fb51baf718b
Mike Massonnet <mmassonnet@xfce.org>
2007-10-18 17:22:41 +0000

    Fix Basque typos.
    (Old svn revision: 793)

--- i18n/projects/libraries/eu.nav.php
+++ i18n/projects/libraries/eu.nav.php
@@ -1,17 +1,17 @@
-﻿<ul>
+<ul>
 	<li><a href="/projects#core">Muin Osagaiak</a>
 		<ul>
 		    <li><a href="/projects/xfdesktop">Idazmahai Kudeatzailea</a></li>
 		    <li><a href="/projects/xfce4-panel">Panela</a></li>
-		    <li><a href="/projects/xfprint">Inrpimatze Laguntzailea</a></li>
+		    <li><a href="/projects/xfprint">Inprimatze Laguntzailea</a></li>
 		    <li><a href="/projects/xfce4-session">Saio Kudeatzailea</a></li>
-		    <li><a href="/projects/xfce-mcs-manager">Ezarpen kudeatzailea</a></li>
+		    <li><a href="/projects/xfce-mcs-manager">Ezarpen Kudeatzailea</a></li>
 		    <li><a href="/projects/xfce-utils">Lanabes eta Script-ak</a></li>
 		    <li><a href="/projects/xfwm4">Leiho Kudeatzailea</a></li>
 		    <li><a href="/projects/libraries">Xfce Liburutegiak</a></li>
 		</ul>
 	</li>
 	<li><a href="/projects#applications">Aplikazioak</a></li>
-	<li><a href="/projects#framework">Lan-ingurunea</a></li>
+	<li><a href="/projects#framework">Lan Ingurunea</a></li>
 	<li><a href="/projects#art">Arte eta Grafikoak</a></li>
 </ul>
