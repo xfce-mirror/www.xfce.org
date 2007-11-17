2f930db2878a3451f7bfb2247c8a400666e20618
Stephan Arts <stephan@xfce.org>
2007-11-17 11:14:14 +0000

    Update dutch translation
    (Old svn revision: 805)

--- i18n/documentation/changelogs/4.4.2.nl.php
+++ i18n/documentation/changelogs/4.4.2.nl.php
@@ -16,6 +16,7 @@ function bug ($id){
 <h1>Xfce 4.4.2 Changelog</h1>
 
 <h3>Utilities Library <em>(libxfce4util)</em>:</h3>
+<!-- DONE -->
 <ul>
     <li>Mogelijke buffer overflow opgelost (gerapporteerd door Vegard Nosum op de mailing-lijst).</li>
     <li>Haakjes van AC_INIT versie informatie verwijderd om rond een bug te werken in intltool 0.35.x en 0.36.x.</li>
@@ -25,32 +26,30 @@ function bug ($id){
         Manatsawin (th), Tegegne Tefera (am), RPrieditis (lv).</li>
 </ul>
 
-<h3>Widget Library <em>(libxfcegui4)</em>:</h3>
+<h3>Grafische Bibliotheek <em>(libxfcegui4)</em>:</h3>
+<!-- DONE -->
 <ul>
-    <li>Free list returned by gtk_container_get_children().</li>
-<!-- -->
+    <li>Geef de vrij die wordt teruggegeven door gtk_container_get_children().</li>
     <li>Haakjes van AC_INIT versie informatie verwijderd om rond een bug te werken in intltool 0.35.x en 0.36.x.</li>
-<!-- -->
-    <li>Allocate a copy of passed cliend id, program name and working directory
-        in session management, in case the application frees the data.</li>
-    <li>Properly deal with %-starting 'field codes' in commands from .desktop files</li>
-<!-- -->
+    <li>In het Sessie beheer wordt een kopie van de client-id,
+        programma naam en werk-map toegewezen. Voor het geval dat een applicatie de data vrijgeeft</li>
+    <li>Correct omgaan met 'veld-codes' die starten met een % in commando's uit .desktop bestanden</li>
     <li>Bijgewerkte vertalingen: Maximilian Schleiss (nl), Dimitri Gogelia (ka),
         Pablo Vieira and Jose Vitor Lopes e Silva (pt_BR).</li>
     <li>Nieuwe vertalingen: Terje Uriansrud (nb_NO), Nuno Miguel (pt_PT),
         Tegegne Tefera (am), Manatsawin (th), RPrieditis (lv).</li>
 </ul>
 
-<h3>Window Manager <em>(xfwm4)</em>:</h3>
+<h3>Venster Beheerder <em>(xfwm4)</em>:</h3>
 <ul>
     <li>Be more relax with transients, allow transients to be sticky independently
-        of their parent window (<?php bug ('3296') ?>)</li>
-    <li>Fix xfwm4 hanging with gtk+-2.11.x (<?php bug ('3346') ?>)</li>
+        of their parent window (<?php bug ('3296') ?>).</li>
+    <li>Fix xfwm4 hanging with gtk+-2.11.x (<?php bug ('3346') ?>).</li>
 <!-- -->
     <li>Stop een lek in muis-knop grijper wanneer het thema wordt veranderd.</li>
 <!-- -->
     <li>Fix dialogs and modals without parents not being automatically
-        centered like before (<?php bug ('3278') ?>)</li>
+        centered like before (<?php bug ('3278') ?>).</li>
     <li>Fix modifier mask not working with all keymaps (<?php bug ('3194') ?>)</li>
     <li>Fix wrong count of key shortcuts causing switch to last workspace
         on modifier key press if no window is focused (<?php bug ('3191') ?>)</li>
@@ -74,8 +73,8 @@ function bug ($id){
     <li>Fix desktop settings only getting applied to the first screen in
         non-Xinerama dualhead setups (<?php bug('3467') ?>).</li>
     <li>Fix spurious drag when double-clicking a volume icon that fails to
-        mount (<?php bug('3426') ?>).
-    <li>Clean out stale entries in file icon position file
+        mount (<?php bug('3426') ?>).</li>
+    <li>Clean out stale entries in icon position file
         (<?php bug('3267') ?>).</li>
     <li>Some minor memory leak fixes (some still remain, likely).</li>
     <li>Fix --disable-menu-editor configure option.</li>
@@ -123,8 +122,8 @@ function bug ($id){
     <li>Make the theme list expand for more natural resize (<?php bug ('3659') ?>).</li>
     <li>Font DPI is now configurable in the User Interface Settings (<?php bug ('3164') ?>).</li>
     <li>Fix modifier mask not working with all keymaps (<?php bug ('3194') ?>)</li>
-    <li>Add support for xinput devices when setting left/right handed mouse.</li>
 <!-- -->
+    <li>Voeg ondersteuning toe voor xinput apparaten wanneer links-/rechtshandige muis wordt ingesteld.</li>
     <li>Bijgewerkte vertalingen: Fabio Riga (it), Nuno Miguel (pt_PT),
         Tegegne Tefera (am), Vincent Tunru (nl), Dimitri Gogelia (ka),
         Vladimir Melo (ka), Alexander Nyakhaychyk (be), Jari Rahkonen (fi),
@@ -135,6 +134,7 @@ function bug ($id){
 </ul>
 
 <h3>Textverwerker <em>(mousepad)</em>:</h3>
+<!-- DONE -->
 <ul>
     <li>Test voor ondersteuning van -Wall, -Werror en -errwarn=%all (<?php bug ('2921') ?>).</li>
     <li>Haakjes van AC_INIT versie informatie verwijderd om rond een bug te
@@ -151,6 +151,7 @@ function bug ($id){
 </ul>
 
 <h3>Sessie Beheerder<em>(xfce4-session)</em>:</h3>
+<!-- DONE -->
 <ul>
     <li>Bijgewerkte vertalingen: Pau Rul-lan Ferragut (ca), Fabio Riga (it),
         Terje Uriansrud (nb_NO), Nuno Miguel (pt_PT), Tegegne Tefera (am),
@@ -158,7 +159,8 @@ function bug ($id){
     <li>Nieuwe vertalingen: RPrieditis (lv).</li>
 </ul>
 
-<h3>Printing Helper <em>(xfprint)</em>:</h3>
+<h3>Print Assistent<em>(xfprint)</em>:</h3>
+<!-- DONE -->
 <ul>
     <li>Bijgewerkte vertalingen: Stephan Arts (nl), Fabio Riga (it),
         Terje Uriansrud (nb_NO), Nuno Miguel (pt_PT), Tegegne Tefera (am),
@@ -167,17 +169,17 @@ function bug ($id){
 </ul>
 
 <h3>Development Tools <em>(xfce4-dev-tools)</em>:</h3>
+<!-- DONE -->
 <ul>
-    <li>Add script (xdt-commit) for generating commit messages from ChangeLogs
-        on the fly.</li>
-<!-- -->
+    <li>Voeg script toe (xdt-commit) voor het on-the-fly genereren van
+        commit-berichten vanuit ChangeLogs</li>
     <li>Haakjes van AC_INIT versie informatie verwijderd om rond een bug te
         werken in intltool 0.35.x en 0.36.x.</li>
 </ul>
 
 <h3>Utilities <em>(xfce-utils)</em>:</h3>
 <ul>
-    <li>Put back a default DPI value as Xorg (<?php bug ('3164') ?>, <?php bug ('3158') ?>)</li>
+    <li>Put back a default DPI value as Xorg (<?php bug ('3164') ?>, <?php bug ('3158') ?>).</li>
 <!-- -->
     <li>Gebruik gnome-screensaver als xscreensaver niet beschikbaar is (<?php bug ('3131') ?>)</li>
     <li>Tiepfout in de XDG_DATA_DIRS pad definitie (<?php bug ('2967') ?>).</li>
@@ -189,9 +191,9 @@ function bug ($id){
 </ul>
 
 <h3>Volume Control <em>(xfce4-mixer)</em>:</h3>
+<!-- DONE -->
 <ul>
-    <li>Fix parallel build of xfce4-mixer (<?php bug ('2892') ?>)</li>
-<!-- -->
+    <li>Maak parallelle build van xfce4-mixer (<?php bug ('2892') ?>).</li>
     <li>Bijgewerkte vertalingen: Stephan Arts (nl), Fabio Riga (it),
         Terje Uriansrud (nb_NO), Nuno Miguel (pt_PT), Dimitri Gogelia (ka),
         Luiz Armesto (pt_BR).</li>
@@ -217,11 +219,13 @@ function bug ($id){
 </ul>
 
 <h3>GTK theme engine <em>(gtk-xfce-engine-2)</em>:</h3>
+<!-- DONE -->
 <ul>
     <li>Voeg geen iconrc bestand toe aan gtkrc.</li>
 </ul>
 
 <h3>Programma zoeker<em>(xfce4-appfinder)</em>:</h3>
+<!-- DONE -->
 <ul>
     <li>Haakjes van AC_INIT versie informatie verwijderd om rond een bug te
         werken in intltool 0.35.x en 0.36.x.</li>
