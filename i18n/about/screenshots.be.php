19efe01dea37065c16b99b90eb31fd8c9219b07d
Mike Massonnet <mmassonnet@xfce.org>
2008-02-23 02:52:03 +0000

    Update screenshot pages, add a new screenshot and hide my old one -- mike
    (Old svn revision: 830)

--- i18n/about/screenshots.be.php
+++ i18n/about/screenshots.be.php
@@ -1,11 +1,14 @@
 <h1>Здымкі экрана</h1>
 <p>Некалькі здымкаў Xfce ў парадку вэрсіяў. </p>
 
-<h2>Xfce 4.4 (бэта)</h2>
+<h2>Xfce 4.4</h2>
 <p>
 <a href="/images/about/screenshots/4.4-1.png"><img src="/images/about/screenshots/4.4-1-small.png" alt="" width="200" height="125" hspace="2" vspace="2" border="0" /></a>
 <a href="/images/about/screenshots/4.4-2.png"><img src="/images/about/screenshots/4.4-2-small.png" alt="" width="200" height="125" hspace="2" vspace="2" border="0" /></a>
 <a href="/images/about/screenshots/4.4-3.png"><img src="/images/about/screenshots/4.4-3-small.png" alt="" width="200" height="125" hspace="2" vspace="2" border="0" /></a>
+<a href="/images/about/screenshots/4.4-4.png"><img src="/images/about/screenshots/4.4-4-small.png" alt="" width="200" height="150" hspace="2" vspace="2" border="0" /></a>
+<a href="/images/about/screenshots/4.4-6.png"><img src="/images/about/screenshots/4.4-6-small.png" alt="" width="200" height="150" hspace="2" vspace="2" border="0" /></a>
+<a href="/images/about/screenshots/4.4-8.png"><img src="/images/about/screenshots/4.4-8-small.png" alt="" width="200" height="150" hspace="2" vspace="2" border="0" /></a>
 </p>
 
 <h2>Xfce 4.2</h2>
