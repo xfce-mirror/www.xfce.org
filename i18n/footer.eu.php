db5891a18f784958b0a5e775fd35c48de97120a1
Auke Kok <sofar@foo-projects.org>
2008-04-14 18:04:39 +0000

    Removing 2x.com image from the footer of every page.
    (Old svn revision: 847)

--- i18n/footer.eu.php
+++ i18n/footer.eu.php
@@ -6,7 +6,6 @@
 				<div id="footer-sponsors">
 					<h2 class="hidden">Logo-ak</h2>
 					<ul>
-						<li><a href="http://www.2x.com/" title="2X Bezero arin konputazio softwarea" target="_blank"><img src="/layout/friends/2x_small.jpg" alt="2X" border="1" /></a></li>
 						<li><a href="http://www.os-cillation.com/" title="os-cillation" target="_blank"><img src="/layout/friends/os-cillation.jpg" alt="os-cillation" border="1" /></a></li>
 						<li><a href="http://sourceforge.net/" title="SourceForge.net" target="_blank"><img src="/layout/friends/sf.jpg" alt="SourceForge.net" border="1" /></a></li>
 					</ul>
--- i18n/footer.zh_CN.php
+++ i18n/footer.zh_CN.php
@@ -6,7 +6,6 @@
 				<div id="footer-sponsors">
 					<h2 class="hidden">Logos</h2>
 					<ul>
-						<li><a href="http://www.2x.com/" title="2X Thin client computing software" target="_blank"><img src="/layout/friends/2x_small.jpg" alt="2X" border="1" /></a></li>
 						<li><a href="http://www.os-cillation.com/" title="os-cillation" target="_blank"><img src="/layout/friends/os-cillation.jpg" alt="os-cillation" border="1" /></a></li>
 						<li><a href="http://sourceforge.net/" title="SourceForge.net" target="_blank"><img src="/layout/friends/sf.jpg" alt="SourceForge.net" border="1" /></a></li>
 					</ul>
