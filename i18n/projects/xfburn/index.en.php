55683ba9ad789d49c0188bf456558a3c445f409b
Mario Danic <mario.danic@gmail.com>
2008-04-17 10:42:21 +0000

    Xfburn can now burn DVDs
    (Old svn revision: 850)

--- i18n/projects/xfburn/index.en.php
+++ i18n/projects/xfburn/index.en.php
@@ -1,8 +1,8 @@
 <div style="float:right; margin:0 0 0.5em 0.5em;"><img src="/images/projects/xfburn.png" alt="xfburn" width="48" height="48" /></div>
 
 <h1>Xfburn</h1>
-<p>Xfburn is a simple CD burning tool based on <a href="http://libburnia-project.org/">libburnia</a> libraries. It can blank
-  CD-RW, copy CDs, burn and create iso images, and burn personal composition of data.</p>
+<p>Xfburn is a simple CD/DVD burning tool based on <a href="http://libburnia-project.org/">libburnia</a> libraries. It can blank
+  CD-RW, copy CDs and DVDs, burn and create iso images, and burn personal composition of data.</p>
 
 <h2>External Links</h2>
 <h3>Subversion</h3>
