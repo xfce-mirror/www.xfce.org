6f9a0f04722e8bc3e65d5de8f30e2d55ded9274f
Nick Schermer <nick@xfce.org>
2008-04-26 09:59:35 +0000

    bla
    (Old svn revision: 859)

--- include/creditsnew.php
+++ include/creditsnew.php
@@ -18,7 +18,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-
 $credits['project-lead'] = array (
     array ('Olivier Fourdan', 'fourdan', 'xfce.org')
     );
