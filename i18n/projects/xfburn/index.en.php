dc12b36e035226a7fc39adf319179107b08b2f89
Mario Danic <mario.danic@gmail.com>
2008-08-27 21:16:11 +0000

    Done once again
    (Old svn revision: 880)

--- i18n/projects/xfburn/index.en.php
+++ i18n/projects/xfburn/index.en.php
@@ -3,6 +3,7 @@
 <h1>Xfburn</h1>
 <p>Xfburn is a simple CD/DVD burning tool based on <a href="http://libburnia-project.org/">libburnia</a> libraries. It can blank CD-RWs, burn and create iso images, as well as burn personal compositions of data to either CD or DVD. It Is currently 
 under <b>heavy</b> development.</p>
+<p><a class="external" target="_blank" href="http://goodies.xfce.org/projects/applications/xfburn"><b>More on Xfburn</b></a></p>
 
 <h2>External Links</h2>
 <p><a class="external" target="_blank" href="http://goodies.xfce.org/projects/applications/xfburn">More on Xfburn</a></p>
