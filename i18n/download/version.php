2fee37e51e7b66caddadb6cd605b86f3ccace475
Brian Tarricone <brian@tarricone.org>
2008-09-14 09:05:55 +0000

    add tx-us and sf.net mirrors for unstable, add sf.net unstable release id
    (Old svn revision: 896)

--- i18n/download/version.php
+++ i18n/download/version.php
@@ -5,7 +5,7 @@ $sf_release_id = "558731";
 
 /* uncomment these 2 to have an unstable release appear */
 //$version_unstable = "4.5.90";
-//$sf_release_id_unstable = "";
+//$sf_release_id_unstable = "626165";
 
 /* list of Xfce server */
 $servers = array (
@@ -13,17 +13,17 @@ $servers = array (
   "BE - www.xfce.org"   => "http://mocha.xfce.org/archive/xfce-$version/",
   "DE - de.xfce.org"    => "http://www.de.xfce.org/archive/xfce-$version/",
   "FR - p0llux.be"      => "http://www.p0llux.be/xfce/xfce-$version/",
-  /*"DE - Hannelore"      => "http://hannelore.f1.fhtw-berlin.de/mirrors/xfce4/xfce-$version/",*/
-  "US - us.xfce.org"    => "http://www.us.xfce.org/archive/xfce-$version/",
+//  "DE - Hannelore"      => "http://hannelore.f1.fhtw-berlin.de/mirrors/xfce4/xfce-$version/",
+  "US - tx-us.xfce.org"    => "http://www.tx-us.xfce.org/archive/xfce-$version/",
   "US - ca-us.xfce.org" => "http://www.ca-us.xfce.org/archive/xfce-$version/",
 );
 
 $servers_unstable = array(
-//  "SourceForge.net"     => "http://sourceforge.net/project/showfiles.php?group_id=19869&package_id=187881&release_id=$sf_release_id_unstable",
+  "SourceForge.net"     => "http://sourceforge.net/project/showfiles.php?group_id=19869&package_id=187881&release_id=$sf_release_id_unstable",
   "BE - www.xfce.org"   => "http://mocha.xfce.org/archive/xfce-$version_unstable/",
-//  "DE - de.xfce.org"    => "http://www.de.xfce.org/archive/xfce-$version_unstable/",
+  "DE - de.xfce.org"    => "http://www.de.xfce.org/archive/xfce-$version_unstable/",
 //  "FR - p0llux.be"      => "http://www.p0llux.be/xfce/xfce-$version_unstable/",
-//  "US - us.xfce.org"    => "http://www.us.xfce.org/archive/xfce-$version_unstable/",
+  "US - tx-us.xfce.org"    => "http://www.tx-us.xfce.org/archive/xfce-$version_unstable/",
   "US - ca-us.xfce.org" => "http://www.ca-us.xfce.org/archive/xfce-$version_unstable/",
 );
 
