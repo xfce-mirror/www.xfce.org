a11cb777e437c3497a3123b864b54845c9328ede
Stephan Arts <stephan@xfce.org>
2008-09-14 12:03:56 +0000

    Add news that 4.5.90 is released
    (Old svn revision: 899)

--- i18n/news/en.news.php
+++ i18n/news/en.news.php
@@ -28,6 +28,43 @@ $news = array (
     ),
 */
     array (
+        'id'      => 15,
+        'date'    => '14 sep 2008',
+        'author'  => 'Stephan Arts, Jannis Pohlman and Brian Tarricone',
+        'title'   => 'Xfce 4.6 ALPHA (\'Pinkie\') released',
+        'content' => "Hello everybody,".
+                     "[br][br]".
+                     "After about 18 months of development, we are pleased to announce the release".
+                     "of Xfce 4.6 ALPHA, codename 'Pinkie'.".
+                     "[br][br]".
+                     "Xfce 4.6 is going to be the next major release of the Xfce desktop".
+                     "environment. The previous release was 4.4 with the last bugfix release".
+                     "being 4.4.2 released in December 2007."
+                     "[br][br]".
+                     "The release schedule for Xfce 4.6 is available to the public on".
+                     "[url]http://wiki.xfce.org/milestones_to_46[/url].".
+                     "[br][br][br]".  
+                     "Xfce 4.6 comes with a lot of new components, some of them replacing old".
+                     "code and some of them being completely new. A preliminary ChangeLog for".
+                     "the alpha release can be found at[br]".
+                     "[url]http://www.xfce.org/documentation/changelogs/4.5.90[/url]".  
+                     "There is also a page which contains general information about the".
+                     "components of Xfce 4.6 and changes that are supposed to go into 4.6:[br]".
+                     "[url]http://wiki.xfce.org/general_info_46[/url]".
+                     "As this is a development-release, we do not claim it is perfect.".
+                     "Here is a list of issues we have already identified:[br]".
+                     "[url]http://wiki.xfce.org/known_issues_46[/url]".
+                     "Getting excited? You can download xfce 4.6 alpha from ".  
+                     "[link=download/#unstable]here[/link].".  
+                     "[br][br]".
+                     "We hope you have a lot of fun trying out this new Xfce.".
+                     "If you find any issues, don't hesitate to check out [url]http://bugzilla.xfce.org/[/url]".
+                     "and look for the bug, or submit a new report if your issues are not already mentioned.".
+                     "[br][br]".
+                     "Kind regards,[br]".
+                     "The Xfce development team",
+    ),
+        array (
         'id'      => 14,
         'date'    => '4 mar 2008',
         'author'  => 'Brian Tarricone',
