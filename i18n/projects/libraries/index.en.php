39b8a2145f9b3441d67742b80f082a24c1b33639
Jerome Guelfucci <jeromeg@xfce.org>
2009-02-20 18:44:27 +0000

    Fix broken links and update libraries.
    (Old svn revision: 1020)

--- i18n/projects/libraries/index.en.php
+++ i18n/projects/libraries/index.en.php
@@ -1,15 +1,17 @@
 <div style="float:right; margin:0 0 0.5em 0.5em;"><img src="/images/projects/library.png" alt="libraries" width="48" height="48" /></div>
 
 <h1>Libraries</h1>
-<p>The core librairies provide common functionnalities to the Xfce environment. There are three libraries which provides
-  non-graphical helper functions, widgets and convenience functions for graphical user interfaces, and functions for 
-  settings management.</p>
+
+<p>
+The core librairies provide common functionnalities to the Xfce environment. There are three libraries which provides
+non-graphical helper functions, widgets and convenience functions for graphical user interfaces, and functions for 
+settings management.
+</p>
 
 <h1>Features</h1>
 <ul>
   <li><strong>libxfce4util</strong>: basic utilities.</li>
   <li><strong>libxfcegui4</strong>: unified widgets.</li>
-  <li><strong>libxfce4mcs</strong>: Xfce components.</li>
 </ul>
 
 <h2>External Links</h2>
@@ -22,11 +24,6 @@
 <p><a class="external" target="_blank" href="http://svn.xfce.org/index.cgi/xfce/browse/libxfcegui4/trunk">WebSVN</a><br />
   <a class="external" target="_blank" href="http://svn.xfce.org/svn/xfce/libxfcegui4/">SVN</a></p>
 <p><code class="code">svn co http://svn.xfce.org/svn/xfce/libxfcegui4/trunk libxfcegui4</code></p>
-
-<h3>libxfce4mcs</h3>
-<p><a class="external" target="_blank" href="http://svn.xfce.org/index.cgi/xfce/browse/libxfce4mcs/trunk">WebSVN</a><br />
-  <a class="external" target="_blank" href="http://svn.xfce.org/svn/xfce/libxfce4mcs/">SVN</a></p>
-<p><code class="code">svn co http://svn.xfce.org/svn/xfce/libxfce4mcs/trunk libxfce4mcs</code></p>
 
 <h2>Authors</h2>
 <ul>
