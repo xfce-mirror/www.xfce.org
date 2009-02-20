5b010d3cb120dbd9cd520bad6eafbbaa57ba41d1
Jerome Guelfucci <jeromeg@xfce.org>
2009-02-20 17:04:31 +0000

    (Old svn revision: 1007)

---
+++ i18n/documentation/index.eu.php
@@ -0,0 +1,41 @@
+<h1>Documentazioa</h1>
+<p>Orrialde honek Xfce erabiltzaile eta garatzaileentzako dokumentazioa du.</p>
+<table width="99%">
+  <tr>
+    <td width="49%" valign="top">
+      <h2><a href="/documentation/4.4/userguide/">Erabiltzaile Gida</a></h2>
+      <p>Barkatu baina (<a href="/development/">oraindik</a>) ez dago 4.4-ari buruzko dokumentaziorik.
+	 <a href="/documentation/4.2/">4.2 documentazioan</a> bilatu,
+	 <a href="http://forum.xfce.org" target="_blank">Xfce foroa</a> bisitatu edo
+	 <a href="/community/lists">eposta zerrendak</a> erabili ditzakezu zure arazoa konpontzen saiatzeko.</p>
+
+      <h2><a href="/documentation/api">Garatzaileak (API)</a></h2>
+      <ul>
+        <li><a href="/documentation/api/libxfce4mcs/">libxfce4mcs</a> (<a href="/documentation/api/libxfce4mcs-docs.tar.gz">tarball</a>)</li>
+        <li><a href="/documentation/api/libxfce4util/">libxfce4util</a> (<a href="/documentation/api/libxfce4util-docs.tar.gz">tarball</a>)</li>
+        <li><a href="/documentation/api/libxfcegui4/">libxfcegui4</a> (<a href="/documentation/api/libxfcegui4-docs.tar.gz">tarball</a>)</li>
+        <li><a href="/documentation/api/libxfce4menu/">libxfce4menu</a> (<a href="/documentation/api/libxfce4menu-docs.tar.gz">tarball</a>)</li>
+        <li><a href="/documentation/api/libxfce4panel/">libxfce4panel</a> (<a href="/documentation/api/libxfce4panel-docs.tar.gz">tarball</a>)</li>
+        <li><a href="/documentation/api/libsqueeze/">libsqueeze</a> (<a href="/documentation/api/libsqueeze-docs.tar.gz">tarball</a>)</li>
+        <li><a href="/documentation/api/exo/">exo</a> (<a href="/documentation/api/exo-docs.tar.gz">tarball</a>)</li>
+        <li><a href="/documentation/api/thunar-vfs/">thunar-vfs</a> (<a href="/documentation/api/thunar-vfs-docs.tar.gz">tarball</a>)</li>
+        <li><a href="/documentation/api/thunarx/">thunarx</a> (<a href="/documentation/api/thunarx-docs.tar.gz">tarball</a>)</li>
+        <li><a href="/documentation/api/libxfprint/">libxfprint</a> (<a href="/documentation/api/libxfprint-docs.tar.gz">tarball</a>)</li>
+      </ul>
+          <h2><a href="/documentation/archive">Artxiboa</a></h2>
+          <ul>
+            <li><a href="/documentation/4.2/">4.2 dokumentazioa</a></li>
+            <li><a href="/documentation/4.0/">4.0 dokumentazioa  </a></li>
+    </ul></td>
+	<td width="2%">&nbsp;</td>
+    <td width="49%" valign="top">
+      <h2><a href="http://wiki.xfce.org/faq" target="_blank">FAQ-ak</a></h2>
+      <p>Xfce-ri buruz sarri egindako galderak <a href="http://wiki.xfce.org/faq target="_blank">Xfce wiki</a>-an
+	 aurkitu ditzakezu.</p>
+      <h2><a href="/documentation/requirements">Beharrak</a></h2>
+          <p>Xfce <a href="/documentation/requirements">dependentzia</a> guztien xehetasunak.</p>
+      <h2><a href="/documentation/changelogs/">Aldaketa erregistroak</a></h2>
+    <p>All Xfce 4 <a href="/documentation/changelogs/">aldaketa erregistroak</a>, bertsioz sailkaturik.</p>
+    </td>
+  </tr>
+</table>
