75b9df62e64a7829517635fe6517fa58d504d170
Jerome Guelfucci <jeromeg@xfce.org>
2009-02-20 12:48:03 +0000

    Add some items.
    (Old svn revision: 989)

--- i18n/about/tour.en.php
+++ i18n/about/tour.en.php
@@ -49,70 +49,105 @@ you to set the system sound quickly using the mouse scroll wheel.
 
 <p align="center"><img src="/images/about/tour/4.6/xfce4-mixer.png" alt="New sound mixer" /></p>
 
+<h2>Improved session manager <a name="session-manager"></a></h2>
+
+<p align="justify">
+<b>Xfce 4.6</b> comes with an enhanced session manager: your session should be started faster
+and the setting dialog has been reworked to ease the management of session aware applications.
+</p>
+
+<p align="center"><img src="/images/about/tour/4.6/xfce4-session-settings.png" alt="Session settings dialog" /></p>
+
+<p align="justify">
+It also brings a long wanted feature: <b>suspend</b> and <b>hibernate</b> support. The logout
+dialog now has two additionnal buttons to suspend your computer or hibernate it.
+</p>
+
+<p align="center"><img src="/images/about/tour/4.6/xfce4-session-logout.png" alt="Session settings dialog" /></p>
+
+<h2>Window manager <a name="window-manager"></a></h2>
+
+<p align="justify">As usual, <b>Xfwm4</b> has matured a lot during this release cycle:
+many bugs have been fixed, multidisplay support and performances have been
+improved and some nice features have also been implemented.</p>
+
+<p align="justify"><b>Xfwm4</b> is now able to detect windows that do not respond and offers
+you to terminate them.</p>
+
+<p align="center"><img src="/images/about/tour/4.6/xfwm4-net-ping.png" alt="Dialog to terminate busy applications" /></p>
+
+<h2>File manager <a name="file-manager"></a></h2>
+
+Todo: bug fixes, mouse forward and backward buttons, performance improvements, wallpaper plugin.
+
+Screenshot: wallpaper plugin.
+
 <h2>New configuration dialogs <a name="configuration-dialogs"></a></h2>
 
 <p align="justify">
-Xfce 4.6 features an improved user interface, <b>Xfce Settings Manager</b>,
+Xfce 4.6 features a new settings interface, <b>Xfce Settings Manager</b>,
 which allows you to set your desktop in an easier way : the dialogs accessible by double
 clicking on the icons have been designed to be more compact, to be more intuitive so that
-you can customize your desktop quickly.
+you can customize your desktop quickly and in a more intuitive way.
 </p>
 
 <p align="center"><img src="/images/about/tour/4.6/xfce4-settings-manager.png" alt="Xfce4 Settings Manager" /></p>
 
 <h3>Accessibility settings <a name="accessibility"></a></h3>
-
 <p align="center"><img src="/images/about/tour/4.6/xfce4-accessibility-settings.png" alt="Accessibility settings dialog" /></p>
 
 <p align="justify">
-The <b>Accessibility settings</b> dialog allows you to set the accessibility related mouse and keyboard options, such as sticky or bounce keys, or mouse emulation.
+The <b>Accessibility settings</b> dialog allows you to set the accessibility related
+mouse and keyboard options, such as sticky or bounce keys, or mouse emulation.
 </p>
 
-
-
 <h3>Appearance settings <a name="appearance"></a></h3>
-
 <p align="center"><img src="/images/about/tour/4.6/xfce4-appearance-settings.png" alt="Appearance settings dialog" /></p>
 
-<p align="justify">The <b>Appearance settings</b> dialog allows you to set the widgets style, the icon theme, font, toolbar and menu options.</p>
-
-
+<p align="justify">The <b>Appearance settings</b> dialog allows you to set the widgets
+style, the icon theme, font, toolbar and menu options.</p>
 
 <h3>Display settings <a name="display"></a></h3>
-
 <p align="center"><img src="/images/about/tour/4.6/xfce4-display-settings.png" alt="Display settings dialog" /></p>
 
-<p align="justify">The <b>Display settings</b> dialog allows you to set the resolution, the refresh rate and the rotation for each screen that is plugged in.</p>
-
+<p align="justify">The <b>Display settings</b> dialog allows you to set the resolution, the
+refresh rate and the rotation for each screen that is plugged in.</p>
 
 
 <h3>Keyboard settings <a name="keyboard"></a></h3>
-
 <p align="center"><img src="/images/about/tour/4.6/xfce4-keyboard-settings-layout.png" alt="Keyboard settings dialog, layout tab" /></p>
 
-<p align="justify">The <b>Keyboard settings</b> dialog allows you to set the keyboard preferences such as key repeating, keyboard shortcuts and your keyboard layout.</p>
+<p align="justify">The <b>Keyboard settings</b> dialog allows you to set the keyboard
+preferences such as key repeating, keyboard shortcuts and your keyboard layout.
+</p>
 
 <p align="center"><img src="/images/about/tour/4.6/xfce4-keyboard-settings-shortcuts.png" alt="Keyboard settings dialog, shortcuts tab" /></p>
 
-<p align="justify">You can now set shortcuts in a simplified way and shortcut conflicts are detected automatically.</p>
-
-
+<p align="justify">You can now set shortcuts in a simplified way and shortcut conflicts
+are detected automatically.</p>
 
 <h3>Mouse settings <a name="mouse"></a></h3>
-
 <p align="center"><img src="/images/about/tour/4.6/xfce4-mouse-settings.png" alt="Mouse settings dialog" /></p>
 
-<p align="justify">The <b>Mouse settings</b> dialog allows you to configure the different mouses plugged to your computer : button order, acceleration, double clic speed, mouse theme...</p>
+<p align="justify">The <b>Mouse settings</b> dialog allows you to configure the different
+mouses plugged to your computer : button order, acceleration, double clic speed, mouse theme...</p>
 
+<h3>Desktop settings <a name="desktop-settings"></a></h3>
 
-<h2>Feedback<a name="feedback"></a></h2>
+<p align="center"><img src="/images/about/tour/4.6/xfdesktop-settings.png" alt="Desktop settings dialog" /></p>
+
+<p align="justify">The <b>Desktop settings</b> dialog is now much more compact, it allows
+you to set per screen settings: wallpaper, brightness, desktop menu, displayed icons...</p>
+
+<h2>New Application finder <a name="appfinder"></a></h2>
 
 <p align="justify">
-Please post comments on this article in my <a href="http://xfce-diary.blogspot.com/2007/01/visual-tour-of-xfce-440.html">blog</a> and use
-the <a href="/community/lists">xfce</a> mailinglist if you have questions about Xfce 4.4.0 or
-trouble with the installation.
+<b>Xfce 4.6</b> also comes with a brand new application finder which features a cleaner user
+interface. It easy also easier to use it with the keyboard and it monitors installed applications
+to update the list on the fly.
 </p>
 
+<p align="center"><img src="/images/about/tour/4.6/xfce4-appfinder.png" alt="New application finder" /></p>
 
 <h2>Links<a name="links"></a></h2>
 
