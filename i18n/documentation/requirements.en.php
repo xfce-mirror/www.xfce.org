0bb4c0579ca8d5f91f63a168aaa37bb3f94a69c4
Jerome Guelfucci <jeromeg@xfce.org>
2009-02-20 17:43:08 +0000

    Update the build requirements.
    (Old svn revision: 1010)

--- i18n/documentation/requirements.en.php
+++ i18n/documentation/requirements.en.php
@@ -1,7 +1,25 @@
 <h1>System requirements for Xfce</h1>
-<p>All packages (except xfce4-dev-tools) depend on both gtk+&nbsp;&gt;=&nbsp;2.6 and glib&nbsp;&gt;=&nbsp;2.6.4.
-The order in the dependency table is also the recommended build order.<br />
-It's also recommended to build all Xfce components from the same release version.</p>
+
+<p>
+All packages (except xfce4-dev-tools) depend on both gtk+&nbsp;&gt;=&nbsp;2.10
+and glib&nbsp;&gt;=&nbsp;2.14.
+</p>
+
+<p>
+It is also recommended to build all Xfce components from the same release version.
+</p>
+
+<h2>Build order</h2>
+
+<ol>
+  <li>xfce4-dev-tools</li>
+  <li>libxfce4util</li>
+  <li>xfconf</li>
+  <li>libxfcegui4, libxfce4menu, libexo</li>
+  <li>xfce4-panel</li>
+  <li>thunar</li>
+  <li>xfce4-settings, xfce4-session, xfdesktop, xfwm4, xfce-utils, xfce4-mixer, xfprint, xfce4-appfinder</li>
+</ol>
 
 <h2>Package dependencies table:</h2>
 <table width="100%">
@@ -12,73 +30,83 @@ It's also recommended to build all Xfce components from the same release version
   </tr>
   <tr bgcolor="#eeeeee">
     <td>xfce4-dev-tools</td>
-    <td>autoconf, automake, gettext, libtool, pkgconfig, intltool</td>
+    <td>autoconf, automake, intltool, pkgconfig </td>
     <td>&nbsp;</td>
   </tr>
   <tr>
     <td>libxfce4util</td>
-    <td>pkgconfig</td>
+    <td>glib, gobject </td>
+    <td>&nbsp;</td>
+  </tr>
+   <tr>
+    <td>xfconf</td>
+    <td>libxfce4util, dbus, dbus-glib, gtk+ </td>
     <td>&nbsp;</td>
   </tr>
   <tr bgcolor="#eeeeee">
     <td>libxfcegui4</td>
-    <td>libxfce4util</td>
-    <td>libstartup-notification</td>
+    <td>libxfce4util, gtk+, libglade</td>
+    <td>libstartup-notification, libgladeui, xfconf</td>
   </tr>
-  <tr>
-    <td>libxfce4mcs</td>
-    <td>libx11, libsm, libxfce4util</td>
-    <td>libstartup-notification</td>
-  </tr>
-  <tr bgcolor="#eeeeee">
-    <td>libexo</td>
-    <td>libxfce4util</td>
-    <td>xfce-mcs-manager, python &gt;= 2.2</td>
-  </tr>
-  <tr>
-    <td>xfce-mcs-manager</td>
-    <td>libxfcegui4, libxfce4mcs</td>
+    <tr bgcolor="#eeeeee">
+    <td>libxfce4menu</td>
+    <td>libxfce4util, gtk+</td>
     <td>&nbsp;</td>
   </tr>
   <tr bgcolor="#eeeeee">
-    <td>xfce-mcs-plugins</td>
-    <td>libx11, libxfcegui4, xfce-mcs-manager</td>
-    <td>libxcursor, libxxf86misc, libxkbfile, libxrandr, libxxf86vm</td>
+    <td>libexo</td>
+    <td>libxfce4util, gtk+, p5-uri </td>
+    <td>&nbsp;</td>
   </tr>
   <tr>
     <td>xfce4-panel</td>
-    <td>libx11, libsm, libxfce4mcs, libxfce4util, libxfcegui4, xfce-mcs-manager</td>
+    <td>libxfce4util, libxfcegui4, libexo, cairo, libwnck</td>
     <td>libstartup-notification</td>
   </tr>
   <tr bgcolor="#eeeeee">
     <td>thunar</td>
-    <td>libexo&nbsp;&gt;=&nbsp;0.3.1.4, libpng&nbsp;&gt;=&nbsp;1.2.0, libxfce4util</td>
-    <td>dbus&nbsp;&gt;=&nbsp;0.34, gconf&nbsp;&gt;=&nbsp;2.4.0, libjpeg</td>
+    <td>libxfce4util, libexo, libpng, gtk+</td>
+    <td>libexif, dbus, gconf, libstartup-notification, freetype, libjpeg, gamin/fam, hal-storage, libxfce4panel, xfconf</td>
+  </tr>
+  <tr bgcolor="#eeeeee">
+    <td>xfce4-settings</td>
+    <td>libxfce4util, libxfcegui4, libexo, xfconf, libglade, dbus-glib, libXi, libXrandr</td>
+    <td>libXcursor, libnotify</td>
   </tr>
   <tr>
     <td>xfce4-session</td>
-    <td>libx11, libsm, libxfce4util, libxfce4mcs, libxfcegui4, xfce-mcs-manager </td>
-    <td>libgnome&nbsp;&gt;=&nbsp;2.4.0</td>
+    <td>libxfce4util, libxfcegui4, p5-xml-parser, libwnck</td>
+    <td>libgnome&nbsp;&gt;=&nbsp;2.4.0, dbus</td>
   </tr>
   <tr bgcolor="#eeeeee">
     <td>xfwm4</td>
-    <td>libx11, libxpm, libsm, libxfce4mcs, libxfce4util, libxfcegui4, xfce-mcs-manager </td>
-    <td>libxrender, libxrandr, libxcomposite, libxfixes, libxdamage</td>
+    <td>libxfce4util, libxfcegui4, libxfconf, dbus-glib, libwnck</td>
+    <td>libstartup-notification</td>
   </tr>
   <tr>
     <td>xfdesktop</td>
-    <td>libx11, libsm, libxfce4util, libxfcegui4, libxfce4mcs, xfce-mcs-manager, xfce4-panel</td>
-    <td>thunar&nbsp;&gt;=&nbsp;0.2.0, libexo&nbsp;&gt;=&nbsp;0.3.1</td>
+    <td>libxfce4util, libxfcegui4, libxfconf, libwnck, libglade</td>
+    <td>dbus-glib, thunar-vfs, libexo, thunarx, libxfce4menu, libxfce4panel</td>
+  </tr>
+    <tr bgcolor="#eeeeee">
+    <td>xfce4-appfinder</td>
+    <td>libxfce4util, libxfce4menu, gtk+ </td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>xfce4-mixer</td>
+    <td>libxfce4util, libxfcegui4, libxfce4panel, gst-plugins-base-0.10</td>
+    <td>&nbsp;</td>
   </tr>
   <tr bgcolor="#eeeeee">
     <td>xfce-utils</td>
-    <td>libx11, libxfce4mcs, libxfce4util, libxfcegui4, xfce-mcs-manager</td>
-    <td>gdm</td>
+    <td>libxfce4util, libxfcegui4, perl w/XML::Parser</td>
+    <td>gdm, dbus</td>
   </tr>
   <tr>
     <td>xfprint</td>
-    <td>libxfcegui4, libxfce4util, libxfce4mcs, xfce-mcs-manager</td>
-    <td>cups, bsdlpr</td>
+    <td>libxfce4util, libxfcegui4, xfconf</td>
+    <td>cups, lpr</td>
   </tr>
   <tr bgcolor="#eeeeee">
     <td>gtk-xfce-engine-2</td>
@@ -100,30 +128,10 @@ It's also recommended to build all Xfce components from the same release version
     <td>&nbsp;</td>
     <td>&nbsp;</td>
   </tr>
-  <tr bgcolor="#eeeeee">
-    <td>xfburn</td>
-    <td>libxfcegui4, libexo&nbsp;&gt;=&nbsp;0.3.0</td>
-    <td>cdrecord, readcd, cdrdao, mkisofs</td>
-  </tr>
   <tr>
     <td>xfcalendar</td>
-    <td>perl, libxfce4mcs, libxfcegui4, xfce-mcs-manager </td>
+    <td>perl, libxfce4mcs, libxfcegui4</td>
     <td>ical, db&nbsp;&gt;=&nbsp;4.0</td>
-  </tr>
-  <tr bgcolor="#eeeeee">
-    <td>xfce4-appfinder</td>
-    <td>libxfcegui4</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td>xfce4-mixer</td>
-    <td>libxml-2&nbsp;&gt;=&nbsp;2.4.0, libxfce4util, libxfcegui4, libxfce4mcs, xfce-mcs-manager</td>
-    <td>ossaudio, alsa, sun, sgi, audio</td>
-  </tr>
-  <tr bgcolor="#eeeeee">
-    <td>xfce4-terminal</td>
-    <td>libxfcegui4, libxfce4mcs, vte&nbsp;&gt;=&nbsp;0.11.0, xfce-mcs-manager</td>
-    <td>&nbsp;</td>
   </tr>
   <tr>
     <td>xfmedia</td>
@@ -131,6 +139,8 @@ It's also recommended to build all Xfce components from the same release version
     <td>libexo&nbsp;&gt;=&nbsp;0.2.0, libstartup-notification, dbus&nbsp;&gt;=&nbsp;0.22, taglib </td>
   </tr>
 </table>
+
+<p>The build dependencies tree is also available as a <a href="/images/documentation/xfce-bdeps.svg">SVG file</a>.</p>
 
 <h2>Recommended packages:</h2>
 <ul>
