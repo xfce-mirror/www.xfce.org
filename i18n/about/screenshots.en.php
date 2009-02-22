a3d58ba493ce8f924cb40673d7c2c33dcf23ce7e
Jannis Pohlmann <jannis@xfce.org>
2009-02-22 20:02:16 +0000

    Another screenshot.
    (Old svn revision: 1081)

--- i18n/about/screenshots.en.php
+++ i18n/about/screenshots.en.php
@@ -7,6 +7,7 @@
 <a href="/images/about/screenshots/4.6-2.png"><img src="/images/about/screenshots/4.6-2-small.png" alt="" width="200" height="150" hspace="2" vspace="2" border="0" /></a>
 <a href="/images/about/screenshots/4.6-3.png"><img src="/images/about/screenshots/4.6-3-small.png" alt="" width="200" height="150" hspace="2" vspace="2" border="0" /></a>
 <a href="/images/about/screenshots/4.6-4.png"><img src="/images/about/screenshots/4.6-4-small.png" alt="" width="200" height="150" hspace="2" vspace="2" border="0" /></a>
+<a href="/images/about/screenshots/4.6-5.png"><img src="/images/about/screenshots/4.6-5-small.png" alt="" width="200" height="150" hspace="2" vspace="2" border="0" /></a>
 </p>
 
 <h2>Xfce 4.4</h2>
