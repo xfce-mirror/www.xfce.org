6ee26b1a4f2be156f720ab4c68a91d4fa2eebc0f
Jerome Guelfucci <jeromeg@xfce.org>
2009-02-25 17:29:38 +0000

    Fix a typo in French.
    (Old svn revision: 1128)

--- i18n/documentation/archive.fr.php
+++ i18n/documentation/archive.fr.php
@@ -1,2 +1,2 @@
 <h1>Archives</h1>
-<p>Les archives contiennent toutes les documentations des versions antérieurs de Xfce.</p>
+<p>Les archives contiennent toutes les documentations des versions antérieures de Xfce.</p>
