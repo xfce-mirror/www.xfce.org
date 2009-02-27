3992c4ef0486051a5ff613382c65499127872e3d
Stephan Arts <stephan@xfce.org>
2009-02-27 00:19:21 +0000

    Modify api-docs tarball url's
    (Old svn revision: 1146)

--- i18n/documentation/index.en.php
+++ i18n/documentation/index.en.php
@@ -11,15 +11,15 @@
 
       <h2><a href="/documentation/4.6/api">Developers (API)</a></h2>
       <ul>
-        <li><a href="/documentation/4.6/api/xfconf/">xfconf</a> (<a href="/documentation/4.6/api/xfconf-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/4.6/api/libxfce4util/">libxfce4util</a> (<a href="/documentation/4.6/api/libxfce4util-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/4.6/api/libxfcegui4/">libxfcegui4</a> (<a href="/documentation/4.6/api/libxfcegui4-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/4.6/api/libxfce4menu/">libxfce4menu</a> (<a href="/documentation/4.6/api/libxfce4menu-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/4.6/api/libxfce4panel/">libxfce4panel</a> (<a href="/documentation/4.6/api/libxfce4panel-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/4.6/api/exo/">exo</a> (<a href="/documentation/4.6/api/exo-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/4.6/api/thunar-vfs/">thunar-vfs</a> (<a href="/documentation/4.6/api/thunar-vfs-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/4.6/api/thunarx/">thunarx</a> (<a href="/documentation/4.6/api/thunarx-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/4.6/api/libxfprint/">libxfprint</a> (<a href="/documentation/4.6/api/libxfprint-docs.tar.gz">tarball</a>)</li>
+        <li><a href="/documentation/4.6/api/xfconf/">xfconf</a> (<a href="/documentation/4.6/api/xfconf-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/4.6/api/libxfce4util/">libxfce4util</a> (<a href="/documentation/4.6/api/libxfce4util-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/4.6/api/libxfcegui4/">libxfcegui4</a> (<a href="/documentation/4.6/api/libxfcegui4-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/4.6/api/libxfce4menu/">libxfce4menu</a> (<a href="/documentation/4.6/api/libxfce4menu-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/4.6/api/libxfce4panel/">libxfce4panel</a> (<a href="/documentation/4.6/api/libxfce4panel-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/4.6/api/exo/">exo</a> (<a href="/documentation/4.6/api/exo-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/4.6/api/thunar-vfs/">thunar-vfs</a> (<a href="/documentation/4.6/api/thunar-vfs-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/4.6/api/thunarx/">thunarx</a> (<a href="/documentation/4.6/api/thunarx-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/4.6/api/libxfprint/">libxfprint</a> (<a href="/documentation/4.6/api/libxfprint-docs.tar.bz2">tarball</a>)</li>
       </ul>
           <h2><a href="/documentation/archive">Archive</a></h2>
           <ul>
--- i18n/documentation/index.sq.php
+++ i18n/documentation/index.sq.php
@@ -11,15 +11,15 @@
 
       <h2><a href="/documentation/api">Zhvilluesa (API)</a></h2>
       <ul>
-        <li><a href="/documentation/api/libxfce4mcs/">libxfce4mcs</a> (<a href="/documentation/api/libxfce4mcs-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/api/libxfce4util/">libxfce4util</a> (<a href="/documentation/api/libxfce4util-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/api/libxfcegui4/">libxfcegui4</a> (<a href="/documentation/api/libxfcegui4-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/api/libxfce4menu/">libxfce4menu</a> (<a href="/documentation/api/libxfce4menu-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/api/libxfce4panel/">libxfce4panel</a> (<a href="/documentation/api/libxfce4panel-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/api/libsqueeze/">libsqueeze</a> (<a href="/documentation/api/libsqueeze-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/api/exo/">exo</a> (<a href="/documentation/api/exo-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/api/thunar-vfs/">thunar-vfs</a> (<a href="/documentation/api/thunar-vfs-docs.tar.gz">tarball</a>)</li>
-        <li><a href="/documentation/api/thunarx/">thunarx</a> (<a href="/documentation/api/thunarx-docs.tar.gz">tarball</a>)</li>
+        <li><a href="/documentation/api/libxfce4mcs/">libxfce4mcs</a> (<a href="/documentation/api/libxfce4mcs-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/api/libxfce4util/">libxfce4util</a> (<a href="/documentation/api/libxfce4util-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/api/libxfcegui4/">libxfcegui4</a> (<a href="/documentation/api/libxfcegui4-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/api/libxfce4menu/">libxfce4menu</a> (<a href="/documentation/api/libxfce4menu-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/api/libxfce4panel/">libxfce4panel</a> (<a href="/documentation/api/libxfce4panel-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/api/libsqueeze/">libsqueeze</a> (<a href="/documentation/api/libsqueeze-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/api/exo/">exo</a> (<a href="/documentation/api/exo-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/api/thunar-vfs/">thunar-vfs</a> (<a href="/documentation/api/thunar-vfs-docs.tar.bz2">tarball</a>)</li>
+        <li><a href="/documentation/api/thunarx/">thunarx</a> (<a href="/documentation/api/thunarx-docs.tar.bz2">tarball</a>)</li>
       </ul>
           <h2><a href="/documentation/archive">Arkivë</a></h2>
           <ul>
