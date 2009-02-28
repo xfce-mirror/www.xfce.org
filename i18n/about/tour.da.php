00abdc7bc31073eef1f338524b3a4eae3390ea8d
Maximilian Schleiss <maxschleiss@solnet.ch>
2009-02-28 15:33:59 +0000

    Danish translation added to the tour
    (Old svn revision: 1212)

---
+++ i18n/about/tour.da.php
@@ -0,0 +1,227 @@
+<h1>En visuel gennemgang af Xfce 4.6.0</h1> 
+
+<p>
+Den længe ventede version 4.6.0 af skrivebordsmiljøet Xfce er endelig tilgængelig.
+Vi vil prøve at fremhæve nogle af de nye funktioner, som er blevet tilføjet siden 
+den sidste stabile udgivelse.
+</p>
+
+<h2>Forbedret oplevelse af skrivebordet med Xfdesktop<a name="xfdesktop"></a></h2>
+
+<p>Siden introduktionen af skrivebordsikoner i Xfce 4.4, har brugerne udtrykt behovet
+for at kunne markere flere ikoner (rubber banding). Med <b>Xfce 4.6</b>,
+<b>Xfdesktop</b>håndteringen implementerer endelig denne funktion: Du kan markere flere ikoner,
+flytte dem, fjerne dem...</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfdesktop-rubberbanding.png" alt="Markering af flere ikoner" /></p>
+
+<p>
+<b>Xfce 4.6</b> introducerer også en helt ny skrivebordsmenu, som tillader dig at manipulere
+filer som med <b>Thunar</b>filhåndteringens kontekstmenu, men kan også åbne programmer, afslutte din session
+eller tilgå hjælpen.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfdesktop-menu.png" alt="Ny skrivebordsmenu" /></p>
+
+<h2>Panel <a name="panel"></a></h2>
+
+<p>Mange fejl, kendt gennem længere tid, er blevet rettet i <b>Xfce4 Panel</b>,
+Specielt for opsætninger med flerskærme, men denne nye udgivelse bringer også forbedrede sæt
+af udvidelsesmoduler til panelet.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfce4-panel-clock.png" alt="Nyt binært ur" /></p>
+
+<p>
+<b>Udvidelsesmodulet til uret</b> er blevet omskrevet til at bruge mindre ressourcer og rette nogle
+grafikfejl, men der er også en ny tilstand for nørden i dig til uret: Binært ur! Det nye
+<b>udvidelsesmodul til påmindelsesområdet</b> tillader dig at skjule påmindelsesikoner,
+som du ikke ønsker skal vises for at holde dit påmindelsesområde ryddeligt og læsbart.
+</p>
+
+<h2>Ny lydmikser <a name="sound-mixer"></a></h2>
+
+<p>
+<b>Mikser til Xfce4</b> er blevet omskrevet fra bunden af for at bruge
+<a href="http://www.gstreamer.net/">Gstreamer</a> som understøtter flere lydsystemer, 
+brugergrænsefladen er mere poleret, du kan håndtere adskillige lydkort og et udvidelsesmodul
+til panelet tillader dig at indstille systemlyden hurtigt ved at rulle musens hjul.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfce4-mixer.png" alt="Ny lydmikser" /></p>
+
+<h2>Forbedret sessionshåndtering <a name="session-manager"></a></h2>
+
+<p>
+<b>Xfce 4.6</b> kommer med en udvidet sessionshåndtering: din session skulle starte hurtigere
+og indstillingsvinduet er blevet omarbejdet for at lette håndteringen af sessionsprogrammerne.
+Hertil kommer, at sessionshåndteringen nu vil genstarte sessionsprogrammer som brød ned automatisk
+så du ikke bliver uden skrivebord, panel, vindueshåndtering, etc., hvis et nedbrud opstår.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfce4-session-settings.png" alt="Vindue til sessionsindstilling" /></p>
+
+<p>
+Den bringer også en længe ventet funktion: Ud af boksen understøttelse af <b>hvile</b> og <b>dvale</b>. Vinduet til
+logud har nu to ekstra knapper for at bringe din computer i hvile- eller dvaletilstand.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfce4-session-logout.png" alt="Vindue til sessionslogud" /></p>
+
+<h2>Window manager <a name="Vindueshåndtering"></a></h2>
+
+<p>
+Som sædvanligt er <b>Xfwm4</b> modnet meget gennem denne udgivelserunde:
+mange fejl er blevet rettet, understøttelse og ydelse til flerskærm er
+forbedret og nogle fine funktioner er også blevet tilføjet.</p>
+
+<p><b>Xfwm4</b> er nu i stand til at genkende vinduer som ikke reagerer, og
+tilbyder dig at afslutte dem.</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfwm4-net-ping.png" alt="Vindue til at afslutte låste programmer" /></p>
+
+<p>
+Der er også en ny <b>Handlingsmenu</b> med yderligere punkter for hurtigt at flytte eller ændre vinduers størrelse,
+til at have dem i front eller bag andre vinduer, eller til at have dem i fuldskærm.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfwm4-new-menu.png" alt="Ny handlingsmenu" /></p>
+
+<p>
+En ny <b>fyld</b>handling er blevet tilføjet, den udvider et givet vindue til den tilrådige
+plads uden at overlappe andre tilstødende vinduer.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfwm4-fill-operation.png" alt="Fyldhandling" /></p>
+
+<p><b>Sammensætteren</b> er blevet optimeret til af formindske vinduesrystelse ved
+ændring af størrelse.</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfwm4-resize.png" alt="Ryste fri ved vinduestørrelsesændring" /></p>
+
+<p>
+Ydermere, nogle <b>indstillinger til finjustering (tweak)</b> er blevet tilføjet: Som eksempel kan du nu
+slå blinkende vinduer fra, når de modtager et vigtigt tips.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfwm4-new-tweaks.png" alt="Nye indstillinger til finjustering (tweak)" /></p>
+
+<h2>File manager <a name="Filhåndtering"></a></h2>
+
+<p>
+Der et blevet rettet en hel række af fejl, forbedret ydelse i <b>Thunar</b>. Den kan også
+bruge musens fremad- og tilbageknap - hvis tilstede - til at navigere og et udvidelsesmodul,
+som tillader dig at indstille et billede som tapet, er blevet tilføjet.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/thunar-wallpaper-plugin.png" alt="Indstil et billede fra en Thunar-mappe som tapet" /></p>
+
+<p><b>Thunar</b> følger nu specifikationen 
+<a href="http://freedesktop.org/wiki/Software/xdg-user-dirs">XDG-brugerkataloger</a>
+som tillader dig at have tematiserede og afgrænsede brugermapper til at lagre din
+musik, dokumenter, videoer, skabeloner...
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/thunar-xdg-user-dirs.png" alt="Thunar-menu til brugerkataloger" /></p>
+
+<p>
+<b>Thunar</b> vil også vise et halvgennemsigtigt ikon for drevene og arkiver
+som ikke er monteret, så du kan skelne dem fra de monterede.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/thunar-mounting.png" alt="halvgennemsigtigte ikoner for ikke monterede drev og arkiver" /></p>
+
+<p>
+Og sidst men ikke mindst, <b>Thunar</b> understøtter nye krypterede enheder!
+</p>
+
+
+<p class="image"><img src="/images/about/tour/4.6/thunar-encrypted.png" alt="Thunar understøtter krypterede enheder" /></p>
+
+<h2>Nye indstillingsvinduer <a name="configuration-dialogs"></a></h2>
+
+<p>
+Xfce 4.6 indeholder en ny indstillingsgrænseflade, <b>indstillingshåndtering til Xfce</b>,
+som tillader dig at indstille dit skrivebord på en nemmere måde: Vinduerne, som er tilgængelige
+med et enkelt klik på ikonerne, er blevet designet til at være mere kompakte, og tillader dig
+at tilpasse dit skrivebord hurtigt og på en mere naturlig måde.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfce4-settings-manager.png" alt="Indstillingshåndtering til Xfce4" /></p>
+
+<h3>Tilgængelighedsindstillinger<a name="accessibility"></a></h3>
+<p class="image"><img src="/images/about/tour/4.6/xfce4-accessibility-settings.png" alt="Vindue til tilgængelighedsindstillinger" /></p>
+
+
+<p>
+Vinduet til <b>indstillingerne til tilgængelighed</b> tillader dig at indstille tilgængelighedsrelaterede muse-
+og tastaturindstillinger, som som klæbe- eller rystetaster eller museemulering.
+</p>
+
+<h3>Indstillinger til udseende <a name="appearance"></a></h3>
+<p class="image"><img src="/images/about/tour/4.6/xfce4-appearance-settings.png" alt="Indstillingsvinduer til udseende" /></p>
+
+<p>
+Vinduet til <b>indstillinger til udseende</b> tillader dig at indstille
+kontrollerstilen, ikontemaet, skriftstypen og værktøjslinjen samt menuindstillingerne.
+</p>
+
+<h3>Skærmindstillinger <a name="display"></a></h3>
+<p class="image"><img src="/images/about/tour/4.6/xfce4-display-settings.png" alt="Vinduet til skærmindstillinger" /></p>
+
+<p>Vinduet til <b>skærmindstillinger</b> tillader dig at indstille opløsningen, 
+opdateringsfrekvensen og roteringen for individuelle skærme, som er tilsluttet.</p>
+
+
+<h3>Tastaturindstillinger <a name="keyboard"></a></h3>
+<p class="image"><img src="/images/about/tour/4.6/xfce4-keyboard-settings-layout.png" alt="Vinduet til tastaturindstillinger, layoutfaneblad" /></p>
+
+<p>Vinduet til <b>tastaturindstillinger</b> tillader dig at indstille
+tastaturindstillingerne såsom tastgentagelse, tastaturgenveje og dit tastaturlayout.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfce4-keyboard-settings-shortcuts.png" alt="Vinduet til tastaturindstillinger, genvejefaneblad" /></p>
+
+<p>
+Du kan nu indstille genveje på en simpel måde og genveje som konflikter
+genkendes automatisk.
+</p>
+
+
+<h3>Museindstillinger <a name="mouse"></a></h3>
+<p class="image"><img src="/images/about/tour/4.6/xfce4-mouse-settings.png" alt="Vinduet til museindstillinger" /></p>
+
+<p>
+Vinduet til <b>museindstillinger</b> tillader dig at indstille de forskellige mus,
+som er tilsluttet din computer : Knaporden, acceleration, dobbeltklik hastighed, tema til musen...
+</p>
+
+<h3>Skrivebordsindstillinger <a name="desktop-settings"></a></h3>
+
+<p class="image"><img src="/images/about/tour/4.6/xfdesktop-settings.png" alt="Vinduet til skrivebordsindstillinger" /></p>
+
+<p>
+Vinduet til <b>skrivebordsindstillinter</b> er nu meget mere kompakt, den tillader
+dig at indstille dine indstillinger per skærm: tapet, lysstyrke, skrivebordsmenu, viste ikoner...</p>
+
+<h2>Ny programfinder <a name="appfinder"></a></h2>
+
+<p>
+<b>Xfce 4.6</b> kommer også med en splinterny programfinder som indeholder en mere klar brugerflade.
+Den er også nemmere at bruge med tastaturet og den overvåger installerede programmer for at opdatere
+listen med det samme. Den tillader dig at oprette panelstartere hurtigt ved at trække et programikon
+til oprettelsesvinduet for starter.
+</p>
+
+<p class="image"><img src="/images/about/tour/4.6/xfce4-appfinder.png" alt="Ny programfinder" /></p>
+
+<h2>Henvisninger<a name="links"></a></h2>
+
+<ul>
+<li><a href="http://www.xfce.org/">Hjemmeside til Xfce</a></li>
+<li><a href="http://thunar.xfce.org/">Hjemmeside til Thunar</a></li>
+</ul>
+
+<h2>Credits<a name="bidragydere"></a></h2>
+
+<p>Skrevet af Jérôme Guelfucci, skærmbilleder af Jannis Pohlmann, februar 2009</p>
