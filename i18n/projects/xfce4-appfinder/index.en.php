7311d7bce75d25d66d8727f46a30780c445a9d91
Jerome Guelfucci <jeromeg@xfce.org>
2009-04-08 11:27:38 +0000

    Fix some typos.
    (Old svn revision: 1258)

--- i18n/projects/xfce4-appfinder/index.en.php
+++ i18n/projects/xfce4-appfinder/index.en.php
@@ -1,13 +1,17 @@
-<div style="float:right; margin:0 0 0.5em 0.5em;"><img src="/images/projects/xfce4-appfinder.png" alt="xfce4-appfinder" width="48" height="48" /></div>
+<div style="float:right; margin:0 0 0.5em 0.5em;">
+  <img src="/images/projects/xfce4-appfinder.png" alt="xfce4-appfinder" width="48" height="48" />
+</div>
 
 <h1>Application Finder</h1>
 
-<p class="center"><img src="/images/projects/screenshots/xfce4-appfinder.png" alt="Application Finder" /></p>
+<p class="center">
+  <img src="/images/projects/screenshots/xfce4-appfinder.png" alt="Application Finder" />
+</p>
 
 <p>
-This is an application finder for the Xfce Desktop Environment. It is a useful program that helps you applications
-on the system, launch them and drag them to panel launcher dialogs. It provides easy keyboard navigation and can
-serve as a replacement for xfrun4.
+  This is an application finder for the Xfce Desktop Environment. It is a useful program
+  that allows you to find applications on the system and launch them. It provides easy
+  keyboard navigation and can be used as a replacement for xfrun4.
 </p>
 
 <h2>External Links</h2>
@@ -19,9 +23,11 @@ serve as a replacement for xfrun4.
 <a class="external" href="http://svn.xfce.org/svn/xfce/xfce4-appfinder/">SVN</a>
 </p>
 
-<p><code class="code">svn co http://svn.xfce.org/svn/xfce/xfce4-appfinder/trunk xfce4-appfinder</code></p>
+<p>
+  <code class="code">svn co http://svn.xfce.org/svn/xfce/xfce4-appfinder/trunk xfce4-appfinder</code>
+</p>
 
-<h1>Author</h1>
+<h1>Authors</h1>
 <ul>
   <li>Jannis Pohlmann [jannis at xfce.org]</li>
   <li>Jasper Huijsmans [jasper at xfce.org]</li>
