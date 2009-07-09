6a0c9346cab85d394aa3af3ad211dc6689313523
Jerome Guelfucci <jeromeg@xfce.org>
2009-07-09 09:11:05 +0000

    Fix bug #4996
    (Old svn revision: 1297)

--- i18n/community/lists.en.php
+++ i18n/community/lists.en.php
@@ -1,5 +1,8 @@
 <h1>Xfce Mailing Lists</h1>
-<p><strong>NOTE:</strong> In order to post to these lists, you must first subscribe. </p>
+
+<p>
+  <strong>NOTE:</strong> In order to post to these lists, you must first subscribe.
+</p>
 
 <table width="99%" cellpadding="2">
     <tr>
@@ -23,32 +26,37 @@
 		<td>To coordinate the translation of Xfce into new languages.</td>
     </tr>
     <tr>
-		<td bgcolor="#eeeeee"><a href="http://foo-projects.org/mailman/listinfo/xfce4-dev/">xfce4-dev</a></td>
-		<td bgcolor="#eeeeee">A mailing list for developers of Xfce.</td>
+    <td bgcolor="#eeeeee"><a href="http://foo-projects.org/mailman/listinfo/xfce-i18n-de">xfce-i18n-de</a></td>
+    <td bgcolor="#eeeeee">To coordinate the German translation of Xfce.</td>
+    <tr>
+		<td><a href="http://foo-projects.org/mailman/listinfo/xfce4-dev/">xfce4-dev</a></td>
+		<td>A mailing list for developers of Xfce.</td>
     </tr>
     <tr>
-		<td><a href="http://foo-projects.org/mailman/listinfo/xfce4-commits/">xfce4-commits</a></td>
-		<td>To be notified of Xfce4 SVN commits.</td>
+		<td bgcolor="#eeeeee"><a href="http://foo-projects.org/mailman/listinfo/xfce4-commits/">xfce4-commits</a></td>
+		<td bgcolor="#eeeeee">To be notified of Xfce4 SVN commits.</td>
     </tr>
     <tr>
-		<td bgcolor="#eeeeee"><a href="http://foo-projects.org/mailman/listinfo/goodies-dev/">goodies-dev</a></td>
-		<td bgcolor="#eeeeee">Xfce Goodies development discussion.</td>
+		<td><a href="http://foo-projects.org/mailman/listinfo/goodies-dev/">goodies-dev</a></td>
+		<td>Xfce Goodies development discussion.</td>
     </tr>
     <tr>
-		<td><a href="http://foo-projects.org/mailman/listinfo/goodies-commits/">goodies-commits</a></td>
-		<td>To be notified of goodies SVN commits.</td>
+		<td bgcolor="#eeeeee"><a href="http://foo-projects.org/mailman/listinfo/goodies-commits/">goodies-commits</a></td>
+		<td bgcolor="#eeeeee">To be notified of goodies SVN commits.</td>
     </tr>
     <tr>
-		<td bgcolor="#eeeeee"><a href="http://foo-projects.org/mailman/listinfo/xfce-bugs/">xfce-bugs</a></td>
-		<td bgcolor="#eeeeee">Discussion of bugs in Xfce products. You can report a bug <a href="http://bugzilla.xfce.org" target="_blank" class="external">here</a>.</td>
+		<td><a href="http://foo-projects.org/mailman/listinfo/xfce-bugs/">xfce-bugs</a></td>
+		<td>Discussion of bugs in Xfce products. You can report a bug <a href="http://bugzilla.xfce.org" target="_blank" class="external">here</a>.</td>
     </tr>
     <tr>
-		<td><a href="http://foo-projects.org/mailman/listinfo/thunar-dev/">thunar-dev</a></td>
-		<td>A mailing list for development of the Thunar file manager</td>
+		<td bgcolor="#eeeeee"><a href="http://foo-projects.org/mailman/listinfo/thunar-dev/">thunar-dev</a></td>
+		<td bgcolor="#eeeeee">A mailing list for development of the Thunar file manager</td>
     </tr>
 </table>
-<p></p>
+
 <h1>Search the mailing lists</h1>
+
 <p>
-    You can search the mailing lists with the Google Search in the header. After you've searched you can 'refine' your search by clicking on the 'Mailing lists' link.
+  You can search the mailing lists with the Google Search in the header. After you've searched you can 
+  filter the results using the 'Mailing lists' link.
 </p>
