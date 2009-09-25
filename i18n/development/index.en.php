b79fc62f90beb56c3463a697e05884609e77c22e
Jérôme Guelfucci <jeromeg@xfce.org>
2009-09-25 21:45:59 +0200

    Refactor the development page. Some links are still missing.

--- i18n/development/index.en.php
+++ i18n/development/index.en.php
@@ -1,52 +1,65 @@
-<h1>Development</h1>
-<p>If you are looking for a way to contribute to Xfce,
-please consider joining the <a href="../community/lists">mailing lists</a> and <a href="http://forum.xfce.org">forum</a> regarding one of these items.</p>
+<h2>Development</h2>
 
-<table width="99%" border="0" cellspacing="10" cellpadding="0">
-<tr>
-		<td width="48" valign="top">
-			<img src="/images/development/i18n.png" alt="i18n" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="http://i18n.xfce.org">Translations</a></h3>
-			<p>Xfce is translated in many different languages, if your language of choice is missing, please help us translate Xfce.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/development/docs.png" alt="docs" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/documentation/">Documentation</a></h3>
-			<p>We always welcome additional documentation about Xfce.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/development/artwork.png" alt="art" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/about/artwork">Artwork</a></h3>
-			<p>Want to create your own Xfce look? You can by following our step-by-step example <a href="http://wiki.xfce.org/howto">howto's</a>. Or you can just take a look at <a href="http://www.xfce-look.org/">xfce-look</a> to see how others have customized their Xfce.</p>
-		</td>
-	</tr>
-	<tr>
-		<td width="48" valign="top">
-			<img src="/images/development/goodies.png" alt="goodies" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="http://goodies.xfce.org">Goodies</a></h3>
-			<p>The Xfce Goodies Project includes additional software and artwork that are related to the Xfce desktop, but not part of the official release.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/development/devel.png" alt="dev" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="http://foo-projects.org/mailman/listinfo/xfce4-dev">Development</a></h3>
-			<p>Feel free to join us developing the next major branch of Xfce, or fix bugs in existing releases.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/development/docs.png" alt="docs" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="http://wiki.xfce.org">Wiki</a></h3>
-			<p>Everybody is able to improve and extend the content of the Xfce <a href="http://wiki.xfce.org">wiki</a>.</p>
-		</td>
-	</tr>
-</table>
+<p>
+Xfce welcomes any kind of contribution. We need every skills, not 
+only coders: translations, documentation or artwork are also very
+ important and need your help!
+</p>
+
+<h3>Contribute to the development of the Xfce core</h3>
+
+<img src="/images/development/devel.png" alt="dev" width="48" height="48" />
+
+<p>
+Feel free to join us developing the next major branch of Xfce, or fix bugs in existing releases. The
+<a href="http://bugzilla.xfce.org">bugzilla</a> and the <a href="">roadmap</a> are a good start to get
+ideas on what needs some love.
+</p>
+
+<p>
+If you need more details or want to get news about the current development, suscribe the
+<a href="http://foo-projects.org/mailman/listinfo/xfce4-dev">development mailing list</a>.
+The <a href="http://wiki.xfce.org/">Xfce wiki</a> also contains a lot of additionnal information.
+</p>
+
+<h3>Goodies</h3>
+
+<img src="/images/development/goodies.png" alt="goodies" width="48" height="48" />
+
+<p>
+The <a href="http://goodies.xfce.org">Xfce goodies</a> are independant projects related to the Xfce desktop, but they are not part of the
+official release. They also need your help: you should contact the maintainer of the project you want to contribute to, but you can
+also start your own project. The <a href="">Goodies development mailing list</a> is there for you if you have any questions.
+</p>
+
+<h3>Translations</h3>
+
+<img src="/images/development/i18n.png" alt="i18n" width="48" height="48" />
+
+<p>
+Translations are currently managed using <a href="http://wiki.xfce.org/translations/transifex">Transifex</a>.
+The <a href="http://i18n.xfce.org/">Xfce i18n wiki</a> contains all instructions on how to contribute 
+to the translations. If you have any questions, the 
+<a href="http://foo-projects.org/mailman/listinfo/xfce-i18n/">Xfce i18n mailing list</a>
+is here for you!
+</p>
+
+<h3>Documentation</h3>
+
+<img src="/images/development/docs.png" alt="docs" width="48" height="48" />
+
+<p>
+As you may have noticed, the <a href="/documentation/">Xfce documentation</a> needs a lot of work.
+A new documentation is being worked on, if you want to join this new effort, please contact us using the
+<a href="http://foo-projects.org/mailman/listinfo/xfce4-dev">development mailing list</a>.
+</p> 
+
+<h3>Artwork</h3>
+
+<img src="/images/development/artwork.png" alt="art" width="48" height="48" />
+
+<p>
+We need volounteers for the <a href="/about/artwork">Xfce Artwork</a>. Design new icons, wallpapers, or Xfwm4 themes by following our 
+step-by-step example <a href="http://wiki.xfce.org/howto">howto's</a>. Or you can just take a look at 
+<a href="http://www.xfce-look.org/">xfce-look</a> to see how others have customized their Xfce desktop.
+</p>
