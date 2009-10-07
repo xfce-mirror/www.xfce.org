ee4b6cea6174df1ed89ccfa9227306f9e16d1fae
Jérôme Guelfucci <jeromeg@xfce.org>
2009-10-07 11:02:34 +0200

    Apply patch  by Diego Ongaro to improve dev page.

--- i18n/development/index.en.php
+++ i18n/development/index.en.php
@@ -1,31 +1,34 @@
 <h2>Development</h2>
 
 <p>
-Xfce is only made possible with the help of volunteers with all kinds 
-of skills and talent: coding is of course one of the main ways of helping, but
-translators, documentators or designers are also welcome!
+Xfce is made possible thanks to the help of volunteers with all kinds
+of skills and talents. Coding is just one way to help &ndash;
+translators, documenters, and designers are also welcome!
+This page outlines some of the areas where you could help out.
 </p>
 
-<h3 class="dev" style="background-image: url(/images/development/devel.png);">Contribute to the development of the Xfce core</h3>
+<h3 class="dev" style="background-image: url(/images/development/devel.png);">Xfce core</h3>
 
 <p>
-Feel free to join us developing the next major branch of Xfce, or fix bugs in existing releases. The
-<a href="http://bugzilla.xfce.org">bugzilla</a> and the <a href="http://wiki.xfce.org/releng/4.8/schedule">roadmap</a>
-are a good start to get ideas on what needs some love.
+Feel free to join us in developing the next release of Xfce or in helping fix bugs in existing releases.
+<a href="http://bugzilla.xfce.org">Bugzilla</a> and the <a href="http://wiki.xfce.org/releng/4.8/schedule">roadmap</a>
+are good sources of ideas on what needs some love.
 </p>
 
 <p>
-If you need more details or want to get news about the current development, suscribe the
+If you are interested in more details or news about current development efforts, subscribe to the
 <a href="http://foo-projects.org/mailman/listinfo/xfce4-dev">development mailing list</a>.
-The <a href="http://wiki.xfce.org/">Xfce wiki</a> also contains a lot of additionnal information.
+The <a href="http://wiki.xfce.org/">wiki</a> also contains a lot of useful information.
 </p>
 
 <h3 class="dev" style="background-image: url(/images/development/goodies.png);">Goodies</h3>
 
 <p>
-The <a href="http://goodies.xfce.org">Xfce goodies</a> are independant projects related to the
-Xfce desktop, but they are not part of the official release. They also need your help: you should
-contact the maintainer of the project you want to contribute to, but you can also start your own project. 
+The <a href="http://goodies.xfce.org">Xfce goodies</a> are independent projects related to the
+Xfce desktop. Though they are not part of the official release, they complement
+and enhance the overall desktop, and they also need your help!
+If you want to contribute to one of these, contact that project's maintainer,
+or you could start your own pet project.
 The <a href="http://foo-projects.org/mailman/listinfo/goodies-dev/">Goodies development mailing list</a>
 is there for you if you have any questions.
 </p>
@@ -33,35 +36,37 @@ is there for you if you have any questions.
 <h3 class="dev" style="background-image: url(/images/development/i18n.png);">Translations</h3>
 
 <p>
-Translations are currently managed using <a href="http://wiki.xfce.org/translations/transifex">Transifex</a>.
-The <a href="http://i18n.xfce.org/">Xfce i18n wiki</a> contains all instructions on how to contribute 
-to the translations. If you have any questions, the 
+Xfce is translated into dozens of languages. Translations are currently managed using
+<a href="http://wiki.xfce.org/translations/transifex">Transifex</a>,
+and the <a href="http://i18n.xfce.org/">i18n wiki</a> contains instructions on how to contribute.
+If you have any questions, the
 <a href="http://foo-projects.org/mailman/listinfo/xfce-i18n/">Xfce i18n mailing list</a>
-is here for you!
+is there for you!
 </p>
 
 <h3 class="dev" style="background-image: url(/images/development/docs.png);">Documentation</h3>
 
 <p>
-As you may have noticed, the <a href="/documentation/">Xfce documentation</a> needs a lot of work.
-A new documentation is being worked on, if you want to join this new effort, please contact us using the
+As you may have noticed, the <a href="/documentation/">Xfce documentation</a> needs some love!
+If you want to join the new documentation effort, please contact us using the
 <a href="http://foo-projects.org/mailman/listinfo/xfce4-dev">development mailing list</a>.
-</p> 
+</p>
 
 <h3 class="dev" style="background-image: url(/images/development/artwork.png);">Artwork</h3>
 
 <p>
-We need volounteers for the <a href="/about/artwork">Xfce Artwork</a>. Design new icons, wallpapers, or Xfwm4 themes by following our 
-step-by-step example <a href="http://wiki.xfce.org/howto">howto's</a>. Or you can just take a look at 
-<a href="http://www.xfce-look.org/">Xfce Look</a> to see how others have customized their Xfce desktop.
+The <a href="/about/artwork">Xfce Artwork</a> project needs volunteers! Design new icons,
+wallpapers, or window manager themes by following our
+step-by-step example <a href="http://wiki.xfce.org/howto">howto's</a>. Or see how others
+have customized their Xfce desktops on <a href="http://www.xfce-look.org/">Xfce Look</a>.
 </p>
 
-<h3 class="dev" style="background-image: url(/images/projects/library.png);">The Xfce framework</h3>
+<h3 class="dev" style="background-image: url(/images/projects/library.png);">Xfce framework</h3>
 
 <p>
-Xfce provides a framework to develop applications which integrate efficiently in the Xfce desktop environment. This
-framework also eases the development of applications by providing common and useful functions which are not present
-in the Glib/Gtk stack.
+Xfce provides a framework for developing applications that integrate well in the Xfce desktop environment. This
+framework also provides common and useful functionality not present in the GLib/GTK stack, further easing
+application development.
 </p>
 
 <p>
@@ -70,18 +75,18 @@ provide a collection of scripts and M4 macros that are required to build the Xfc
 </p>
 
 <p>
-The <a href="/projects/libraries/">three base libraries of Xfce</a> (libxfce4util, libxfcegui4 and libxfce4menu) are used by almost 
-any Xfce application. They provide convenience functions, customized widgets as the titled dialogs or
-implementations of specifications.
+The <a href="/projects/libraries/">base libraries of Xfce</a> are used by almost
+every Xfce application. Among other things, they provide convenience functions and specialized display widgets.
 </p>
 
 <p>
-While the core Xfce libraries are targeted at desktop development, <a href="/projects/exo/">exo</a> is targeted at application developement. It
-provides custom widgets, a job framework and a lot of useful functions for developing applications.
+While the core Xfce libraries are targeted at desktop development, <a href="/projects/exo/">exo</a>
+is targeted at application development. It provides custom widgets, a job framework, and a lot of
+other useful functionality for developing applications.
 </p>
 
 <p>
-Moreover, we have <a href="http://pyxfce.xfce.org/">Python</a>, 
-<a href="http://spuriousinterrupt.org/projects/xfce4-perl">Perl</a> and 
-<a href="http://xfc.xfce.org/">C++</a> bindings for those libraries.
+Xfce also includes <a href="http://pyxfce.xfce.org/">Python</a>,
+<a href="http://spuriousinterrupt.org/projects/xfce4-perl">Perl</a>, and
+<a href="http://xfc.xfce.org/">C++</a> bindings to its libraries.
 </p>
