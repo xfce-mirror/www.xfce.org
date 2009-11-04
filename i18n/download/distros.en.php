d38963c8be33824316fa99ba9251d8e6d1e97d3b
Jérôme Guelfucci <jeromeg@xfce.org>
2009-11-04 23:41:20 +0100

    Improve the page look.

--- i18n/download/distros.en.php
+++ i18n/download/distros.en.php
@@ -3,29 +3,27 @@
 <p>Here is a list of distributions using Xfce as their native desktop.</p>
 
 
-<h3 class="dev" style="background-image: url(/images/download/distros/fedora.png);">Fedora Xfce Spin</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/fedora.png);">
+<a href="http://fedoraproject.org/wiki/SIGs/Xfce/XfceLive">Fedora Xfce Spin</a>
+</h3>
 
 <p>
  The Fedora Xfce Spin is live cd which is also installable that showcases the Xfce Desktop Environment 
  featuring a mix of high productivity centric applications.
 </p>
 
-<a href="http://fedoraproject.org/wiki/SIGs/Xfce/XfceLive" title="Visit the Fedora Xfce Spin wiki page">Website</a> |
- <a href="http://distrowatch.com/fedora" title="Visit the Fedora page at DistroWatch">DistroWatch</a>
-
-
-<h3 class="dev" style="background-image: url(/images/download/distros/xubuntu.png);">Xubuntu</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/xubuntu.png);">
+<a href="http://www.xubuntu.org/">Xubuntu</a>
+</h3>
 
 <p>
   Xubuntu is a Linux distribution based on Ubuntu. Unlike its parent Xubuntu uses the light-weight Xfce 
   desktop environment. The distribution includes only GTK+ applications where possible.
 </p>
 
-<a href="http://www.xubuntu.org/" title="Visit the Xubuntu website">Website</a> |
-<a href="http://distrowatch.com/xubuntu" title="Visit the Xubuntu page at DistroWatch">DistroWatch</a>
-
-
-<h3 class="dev" style="background-image: url(/images/download/distros/zenwalk.png);">Zenwalk</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/zenwalk.png);">
+<a href="http://www.zenwalk.org/">Zenwalk</a>
+</h3>
 
 <p>
   Zenwalk is an operating system based on Slackware. The goal of Zenwalk is being slim and fast using only 
@@ -35,11 +33,9 @@
   finely-tuned modern server (e.g. LAMP, messaging, file sharing).
 </p>
 
-<a href="http://www.zenwalk.org/" title="Visit the Zenwalk website">Website</a> |
- <a href="http://distrowatch.com/zenwalk" title="Visit the Zenwalk page at DistroWatch">DistroWatch</a>
-
-
-<h3 class="dev" style="background-image: url(/images/download/distros/mythbuntu.png);">Mythbuntu</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/mythbuntu.png);">
+<a href="http://www.mythbuntu.org/">Mythbuntu</a>
+</h3>
 
 <p>
   Mythbuntu is an community supported add-on for Ubuntu focused upon setting up
@@ -47,11 +43,9 @@
   system or for integration with an existing MythTV network.
 </p>
  
-<a href="http://www.mythbuntu.org/" title="Visit the Mythbuntu web page">Website</a> | 
- <a href="http://distrowatch.com/mythbuntu" title="Visit the Mythbuntu page at DistroWatch">DistroWatch</a></td>
-
-
-<h3 class="dev" style="background-image: url(/images/download/distros/dreamlinux.png);">Dreamlinux</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/dreamlinux.png);">
+<a href="http://www.dreamlinux.com.br/">Dreamlinux</a>
+</h3>
       
 <p>
   Dreamlinux is a modern and modular GNU/Linux system that can be run directly
@@ -59,10 +53,9 @@
   and USB Drive).
 </p>
 
-<a href="http://www.dreamlinux.com.br/" title="Visit the Dreamlinux web page">Website</a> | 
- <a href="http://distrowatch.com/dreamlinux" title="Visit the Dreamlinux page at DistroWatch">DistroWatch</a>
-
-<h3 class="dev" style="background-image: url(/images/download/distros/vector.png);">VectorLinux</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/vector.png);">
+<a href="http://www.vectorlinux.com/">VectorLinux</a>
+</h3>
 
 <p>
   Speed, performance, stability -- these are attributes that set VectorLinux apart in the
@@ -72,11 +65,9 @@
   operating system available anywhere.
 </p>
 
-<a href="http://www.vectorlinux.com/" title="Visit the VectorLinux website">Website</a> |
- <a href="http://distrowatch.com/vectorlinux" title="Visit the VectorLinux page at DistroWatch">DistroWatch</a>
-
-
-<h3 class="dev" style="background-image: url(/images/download/distros/wolvix.png);">Wolvix GNU/Linux</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/wolvix.png);">
+<a href="http://wolvix.org/">Wolvix GNU/Linux</a>
+</h3>
 
 <p>
   Wolvix is a desktop oriented GNU/Linux distribution based on Slackware. It
@@ -86,33 +77,27 @@
   everyday computing tasks, creativity, work and enjoyment.
 </p>
 
-<a href="http://wolvix.org/" title="Visit the Wolvix GNU/Linux website">Website</a> |
- <a href="http://distrowatch.com/wolvix" title="Visit the Wolvix GNU/Linux page at DistroWatch">DistroWatch</a>
-
-
-<h3 class="dev" style="background-image: url(/images/download/distros/kwortlinux.png);">Kwort Linux</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/kwortlinux.png);">
+<a href="http://www.kwort.org/">Kwort Linux</a>
+</h3>
 
 <p>
   Kwort is a modern, fast and easy to use Linux distribution that combines Xfce and some other 
   useful tools to a powerful desktop. Kwort is based on Slackware, so it's robust, clean and easy to extend.
 </p>
 
-<a href="http://www.kwort.org/" title="Visit the Kwort Linux website">Website</a> |
- <a href="http://distrowatch.com/kwort" title="Visit the Kwort Linux page at DistroWatch">DistroWatch</a>
-
-
-<h3 class="dev" style="background-image: url(/images/download/distros/kateos.png);">Kate OS</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/kateos.png);">
+<a href="http://www.kateos.org/">Kate OS</a>
+</h3>
 
 <p>
   Kate OS is a polish GNU/Linux distribution. It is a lightweight binary distribution featuring Xfce
   as the default desktop environment.
 </p>
 
-<a href="http://www.kateos.org/" title="Visit the Kate OS website">Website</a> |
- <a href="http://distrowatch.com/kateos" title="Visit the Kate OS page at DistroWatch">DistroWatch</a>
-
-
-<h3 class="dev" style="background-image: url(/images/download/distros/myahos.png);">Myah OS</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/myahos.png);">
+<a href="http://myah.org/">Myah OS</a>
+</h3>
 
 <p>
   Myah OS is a performance desktop operating system. Made for home use with a focus on 
@@ -121,44 +106,35 @@
   by Jeremiah Cheatham.
 </p>
 
-<a href="http://myah.org/" title="Visit the Myah OS website">Website</a> |
- <a href="http://distrowatch.com/myah" title="Visit the Myah OS page at DistroWatch">DistroWatch</a>
-    
-
 <h2>Xfce Live-CD distributions</h2>
 
-<h3 class="dev" style="background-image: url(/images/download/distros/sam.png);">SAM Linux Desktop</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/sam.png);">
+<a href="http://www.sam-linux.org/">SAM Linux Desktop</a>
+</h3>
 
 <p>
   SAM Linux Desktop is a self-booting live CD containing a full linux desktop OS. You don't have to install 
   it on your harddisk, it runs completely from your cd drive and your RAM.
 </p>
 
-<a href="http://www.sam-linux.org/" title="Visit the SAM Linux website">Website</a> | 
- <a href="http://distrowatch.com/sam" title="Visit the SAM Linux page at DistroWatch">DistroWatch</a></td>
-
-
-<h3 class="dev" style="background-image: url(/images/download/distros/pureoslight.png);">PureOSlight</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/pureoslight.png);">
+<a href="http://www.pureos.org/">PureOSlight</a>
+</h3>
  
 <p>
   PureOSlight is a Debian-based liveCD (Debian testing) for french-speaking users. It's built with Linux-live
   scripts from Tomas Matejicek (Slax).
 </p>
 
-<a href="http://www.pureos.org/" title="Visit the PureOSlight website">Website</a>
-
-
-
 <h2>General distributions with a good Xfce support</h2>
 
-<h3 class="dev" style="background-image: url(/images/download/distros/Lunar.png);">Lunar Linux</h3>
+<h3 class="dev" style="background-image: url(/images/download/distros/lunar.png);">
+<a href="http://www.lunar-linux.org/">Lunar Linux</a>
+</h3>
  
 <p>
   Lunar Linux is a source-based distribution. Although there is no native desktop environment 
   in Lunar, it has excellent support for Xfce, mainly because a couple of Xfce developers are 
   also Lunar developers. Another noticeable point: all Xfce servers are running Lunar Linux!
 </p>
-
-<a href="http://www.lunar-linux.org/" title="Visit the Lunar Linux website">Website</a> |
- <a href="http://distrowatch.com/lunar" title="Visit the Lunar Linux page at DistroWatch">DistroWatch</a></td>
 
