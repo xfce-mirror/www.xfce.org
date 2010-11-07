2164fb4b122e64ecd6ad885afd9ab829ad792d60
Jannis Pohlmann <jannis@xfce.org>
2010-11-07 17:29:54 +0100

    Update NEWS page for the 4.8pre1 release.

--- i18n/news/en.news.php
+++ i18n/news/en.news.php
@@ -29,6 +29,51 @@ $news = array (
 */
 
     array (
+        'id'      => 24,
+        'date'    => "7 nov 2010",
+        'author'  => "The Xfce development team",
+        'title'   => "Xfce 4.8pre1 Released",
+        'content' => "The Xfce development team is proud to announce the first preview release for Xfce 4.8. ".
+                     "Together with this preview release, the Xfce project announces the feature freeze for the ".
+                     "final 4.8 release which is set to be pushed out to the world on January 16th, 2011.".
+                     "[br][br]".
+                     "This release incorporates major changes to the core of the Xfce desktop environment and ".
+                     "hopefully succeeds in fulfilling a number of long time requests. Among the most notable ".
+                     "updates is that we have ported the entire Xfce core (Thunar, xfdesktop and thunar-volman ".
+                     "in particular) from ThunarVFS to GIO, bringing remote filesystems to the Xfce desktop. ".
+                     "The panel has been rewritten from scratch and provides better launcher management and ".
+                     "improved multi-head support. The list of new panel features is too long to mention in ".
+                     "its entirety here. Thanks to the new menu library garcon (formerly known as libxfce4menu, ".
+                     "but rewritten once again) we now support menu editing via a third-party menu editor such ".
+                     "as Alacarte (we do not ship our own yet). Our core libraries have been streamlined a bit, ".
+                     "a good examplle being the newly introduced libxfce4ui library which is meant to replace ".
+                     "libxfcegui4.".
+                     "[br][br]".
+                     "Perhaps the most important achievement we will accomplish with Xfce 4.8 is that, despite ".
+                     "suffering from the small size of the development team from time to time, the core of the ".
+                     "desktop environment has been aligned with today's desktop technologies such as GIO, ".
+                     "ConsoleKit, PolicyKit, udev and many more. A lot of old cruft like has been stripped from ".
+                     "the core as well, as has happened with HAL and ThunarVFS (which is still around for ".
+                     "compatibility reasons).".
+                     "[br][br]".
+                     "Thanks to the awesome Transifex translation platform, our language teams have been able to ".
+                     "update their translations at an incredible pace.  Please include them when praising this ".
+                     "release!".
+                     "[br][br]".
+                     "A complete list of all changes since the latest stable release is available ".
+                     "[link=http://mocha.xfce.org/documentation/changelogs/4.8pre1]here[/link].".
+                     "[br][br]".
+                     "Below you will find download information for Xfce4.8pre1. Please give our mirrors a few ".
+                     "hours to synchronize. We hope you will enjoy this release, feel encouraged to blog and ".
+                     "tweet about it! Feedback is welcome in all forms. Bugs can be reported in our ".
+                     "[link=http://bugzilla.xfce.org]bug tracker[/link] as usual. We need your help to make ".
+                     "Xfce 4.8 our best release ever!".
+                     "[br][br]".
+                     "Kind regards and thanks to everyone who has contributed to this release,".
+                     "[br]".
+                     "The Xfce development team."
+    ),
+    array (
         'id'      => 23,
         'date'    => "23 may 2010",
         'author'  => "The Xfce Development Team",
