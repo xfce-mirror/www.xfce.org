be2249f6038ed6ec4f6fcdffc95a0af582fbaac6
Nick Schermer <nick@xfce.org>
2010-11-29 21:48:56 +0100

    Style project page.

--- pages/projects/index.php
+++ pages/projects/index.php
@@ -1,196 +1,169 @@
 <?php
-	$top=true;
-?>
-<h1>Projects</h1>
 
+$head['title'] = R_('Projects');
+$head['stylesheet'] = array ('/style/projects.css');
+
+?>
+<h1><?php echo $head['title'] ?></h1>
 <p>
-The Xfce project contains several separated projects for each part of the desktop.
-Every project has it's own project page to provide additionnal information.
+  <?php E_('The Xfce project contains several separated projects for each part of the desktop. Every project has it\'s own project page to provide additionnal information.') ?>
 </p>
 
-<h2 id="core">Core Components</h2>
-<table width="99%" border="0" cellpadding="0" cellspacing="10">
-	<tr>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfwm4.png" alt="xfwm4" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/xfwm4/">Window Manager</a></h3>
-			<p>It manages the placement of application windows on the screen,
-			provides window decorations and manages workspaces or virtual desktops.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfce4-panel.png" alt="xfce4-panel" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/xfce4-panel/">Panel</a></h3>
-      <p>The Panel is part of the Xfce Desktop Environment and features program launchers, panel 
-      menus, a clock, a desktop switcher and more.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfdesktop.png" alt="xfdesktop" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/xfdesktop/">Desktop Manager</a></h3>
-			<p>This program sets the background image and provides a root window menu,
-			desktop icons or minimized icons and a windows list.</p>
-		</td>
-	</tr>
-	<tr>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfce4-session.png" alt="xfce4-session" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/xfce4-session/">Session Manager</a></h3>
-			<p>The session manager controls the startup and shutdown of the Xfce Desktop Environment.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfprint.png" alt="xfprint" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top"><h3><a href="/projects/xfprint/">Printing Helper</a></h3>
-			<p>
-      Provides a graphical frontend for printing. Xfprint can use either
-			CUPS, BSD-LPR or a file as destination, and printers are retrieved from the
-			printing system you chose.
-      </p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfce4-settings.png" alt="xfce4-settings" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-		<h3><a href="/projects/xfce4-settings/">Settings Manager</a></h3>
-			<p>
-      It allows you to configure every detail of your desktop environment.
-      </p>
-		</td>
-	</tr>
-	<tr>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfce4-utils.png" alt="xfce4-utils" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/xfce-utils/">Utilities and Scripts </a></h3>
-    <p>Essential utilities and scripts. Provides a credits and license dialog, a dialog for 
-    launching applications and several important scripts that are used by other packages.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/library.png" alt="library" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/libraries/">Xfce Libraries</a></h3>
-			<p>The three base libraries of Xfce (libxfce4util, libxfcegui4 and libxfce4menu)
-			are used by almost any Xfce application. </p>
-		</td>
-		<td width="48" valign="top">
-      <img src="/images/projects/library.png" alt="xfconf" width="48" height="48" />
-    </td>
-		<td width="33%" valign="top">
-      <h3><a href="/projects/xfconf/">Xfconf</a></h3>
-      <p>
-        Simple client-server configuration storage and query system.
-      </p>
-    </td>
-	</tr>
-</table>
+<h2 id="core"><?php E_('Core Components') ?></h2>
 
-<h2 id="applications">Applications</h2>
-<table width="99%" border="0" cellpadding="0" cellspacing="10">
-	<tr>
-		<td width="48" valign="top">
-			<img src="/images/projects/mousepad.png" alt="mousepad" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/mousepad/">Mousepad</a></h3>
-			<p>Mousepad is a text editor for Xfce based on Leafpad. The initial reason for
-			Mousepad was to provide printing support, which would have been
-			difficult for Leafpad for various reasons.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/terminal.png" alt="terminal" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/terminal/">Terminal</a></h3>
-			<p>Terminal is a modern terminal emulator for the Unix/Linux desktop - primarly for the Xfce
-			desktop environment.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/thunar.png" alt="thunar" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/thunar/">Thunar Filemanager </a></h3>
-			<p>Thunar is a new modern file manager for the Xfce Desktop Environment. Thunar has been designed
-			from the ground up to be fast and easy-to-use.</p>
-		</td>
-	</tr>
-	<tr>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfburn.png" alt="xfburn" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/xfburn/">Xfburn</a></h3>
-			<p>Xfburn is a frontend burning application based on <a href="http://libburnia-project.org/">libburnia</a> libraries to create and burn CD's and DVD's.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/orage.png" alt="orage" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/orage/">Orage</a></h3>
-			<p>A simple calendar application with reminders.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfce4-appfinder.png" alt="xfce4-appfinder" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/xfce4-appfinder/">Application Finder</a></h3>
-			<p>Allows you to search, launch and find information about applications installed on your system.</p>
-		</td>
-	</tr>
-	<tr>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfce4-mixer.png" alt="xfce4-mixer" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3><a href="/projects/xfce4-mixer/">Mixer</a></h3>
-			<p>A modern sound mixer based on GStreamer.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/ristretto.png" alt="ristretto" width="48" height="48" />
-		</td>
-		<td valign="top">
-			<h3><a href="/projects/ristretto">Image Viewer</a></h3>
-			<p>Ristretto is a lightweight image viewer.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/midori.png" alt="midori" width="48" height="48" />
-		</td>
-		<td valign="top">
-			<h3><a href="http://www.twotoasts.de/index.php?/pages/midori_summary.html">Web Browser</a></h3>
-			<p>Midori is a lightweight web browser.</p>
-		</td>
-	</tr>
-</table>
+<div class="group">
+  <div class="module">
+    <h3><a href=""><?php E_('Window Manager') ?></a></h3>
+    <p style="background-image: url('/images/projects/xfwm4.png')">
+      <?php E_('It manages the placement of application windows on the screen, provides window decorations and manages workspaces or virtual desktops.') ?>
+    </p>
+  </div>
 
-<h2 id="art">Art and Graphics</h2>
-<table width="99%" border="0" cellpadding="0" cellspacing="10">
-	<tr>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfce-gtk-engine.png" alt="xfce-gtk-engine" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3>GTK+ Theme Engine</h3>
-			<p>A theme engine for GTK+. Not required for the desktop, but it's a nice theme
-			engine so you might just as well give it a try.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/images/projects/xfce4-icon-theme.png" alt="xfce4-icon-theme" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">
-			<h3>Icon Theme</h3>
-			<p>Default icon theme for Xfce. It includes both PNG and SVG icons.</p>
-		</td>
-		<td width="48" valign="top">
-			<img src="/layout/images/spacer.gif" alt="spacer" width="48" height="48" />
-		</td>
-		<td width="33%" valign="top">&nbsp;</td>
-	</tr>
-</table>
+  <div class="module">
+    <h3><a href=""><?php E_('Panel') ?></a></h3>
+    <p style="background-image: url('/images/projects/xfce4-panel.png')">
+      <?php E_('The Panel is part of the Xfce Desktop Environment and features program launchers, panel menus, a clock, a desktop switcher and more.') ?>
+    </p>
+  </div>
+
+  <div class="clearboth"></div>
+</div>
+
+<div class="group">
+  <div class="module">
+    <h3><a href=""><?php E_('Desktop Manager') ?></a></h3>
+    <p style="background-image: url('/images/projects/xfdesktop.png')">
+      <?php E_('This program sets the background image and provides a root window menu, desktop icons or minimized icons and a windows list.') ?>
+    </p>
+  </div>
+
+  <div class="module">
+    <h3><a href=""><?php E_('Session Manager') ?></a></h3>
+    <p style="background-image: url('/images/projects/xfce4-session.png')">
+      <?php E_('The session manager controls the startup and shutdown of the Xfce Desktop Environment.') ?>
+    </p>
+  </div>
+
+  <div class="clearboth"></div>
+</div>
+
+<div class="group">
+  <div class="module">
+    <h3><a href=""><?php E_('Settings Manager') ?></a></h3>
+    <p style="background-image: url('/images/projects/xfce4-settings.png')">
+      <?php E_('It allows you to configure every detail of your desktop environment.') ?>
+    </p>
+  </div>
+
+  <div class="module">
+    <h3><a href=""><?php E_('Utilities and Scripts') ?></a></h3>
+    <p style="background-image: url('/images/projects/xfce-utils.png')">
+      <?php E_('Essential utilities and scripts. Provides a credits and license dialog, a dialog for launching applications and several important scripts that are used by other packages.') ?>
+    </p>
+  </div>
+
+  <div class="clearboth"></div>
+</div>
+
+<div class="group">
+  <div class="module">
+    <h3><a href=""><?php E_('Xfce Libraries') ?></a></h3>
+    <p style="background-image: url('/images/projects/library.png')">
+      <?php E_('The three base libraries of Xfce (libxfce4util and libxfce4ui) are used by almost any Xfce application.') ?>
+    </p>
+  </div>
+
+  <div class="module">
+    <h3><a href=""><?php E_('Xfconf') ?></a></h3>
+    <p style="background-image: url('/images/projects/library.png')">
+      <?php E_('Simple client-server configuration storage and query system.') ?>
+    </p>
+  </div>
+
+  <div class="clearboth"></div>
+</div>
+
+<div class="group">
+  <div class="module">
+    <h3><a href=""><?php E_('Thunar Filemanager') ?></a></h3>
+    <p style="background-image: url('/images/projects/thunar.png')">
+      <?php E_('Thunar is a new modern file manager for the Xfce Desktop Environment. Thunar has been designed from the ground up to be fast and easy-to-use.') ?>
+    </p>
+  </div>
+
+  <div class="module">
+    <h3><a href=""><?php E_('Application Finder') ?></a></h3>
+    <p style="background-image: url('/images/projects/xfce4-appfinder.png')">
+      <?php E_('Allows you to search, launch and find information about applications installed on your system.') ?>
+    </p>
+  </div>
+
+  <div class="clearboth"></div>
+</div>
+
+<h2 id="applications"><?php E_('Applications') ?></h2>
+
+<div class="group">
+  <div class="module">
+    <h3><a href=""><?php E_('Mousepad') ?></a></h3>
+    <p style="background-image: url('/images/projects/mousepad.png')">
+      <?php E_('Mousepad is a text editor for Xfce based on Leafpad. The initial reason for Mousepad was to provide printing support, which would have been difficult for Leafpad for various reasons.') ?>
+    </p>
+  </div>
+
+  <div class="module">
+    <h3><a href=""><?php E_('Terminal') ?></a></h3>
+    <p style="background-image: url('/images/projects/terminal.png')">
+      <?php E_('Terminal is a modern terminal emulator for the Unix/Linux desktop - primarly for the Xfce desktop environment.') ?>
+    </p>
+  </div>
+
+  <div class="clearboth"></div>
+</div>
+
+<div class="group">
+  <div class="module">
+    <h3><a href=""><?php E_('Xfburn') ?></a></h3>
+    <p style="background-image: url('/images/projects/xfburn.png')">
+      <?php E_('Xfburn is a frontend burning application based on <a href="http://libburnia-project.org/">libburnia</a> libraries to create and burn CD\'s and DVD\'s.') ?>
+    </p>
+  </div>
+
+  <div class="module">
+    <h3><a href=""><?php E_('Orage') ?></a></h3>
+    <p style="background-image: url('/images/projects/orage.png')">
+      <?php E_('A simple calendar application with reminders.') ?>
+    </p>
+  </div>
+
+  <div class="clearboth"></div>
+</div>
+
+<div class="group">
+  <div class="module">
+    <h3><a href=""><?php E_('Mixer') ?></a></h3>
+    <p style="background-image: url('/images/projects/xfce4-mixer.png')">
+      <?php E_('A modern sound mixer based on GStreamer.') ?>
+    </p>
+  </div>
+
+  <div class="module">
+    <h3><a href=""><?php E_('Image Viewer') ?></a></h3>
+    <p style="background-image: url('/images/projects/ristretto.png')">
+      <?php E_('Ristretto is a lightweight image viewer.<') ?>
+    </p>
+  </div>
+
+  <div class="clearboth"></div>
+</div>
+
+<div class="group">
+  <div class="module">
+    <h3><a href=""><?php E_('Web Browser') ?></a></h3>
+    <p style="background-image: url('/images/projects/midori.png')">
+      <?php E_('Midori is a lightweight web browser.') ?>
+    </p>
+  </div>
+
+  <div class="clearboth"></div>
+</div>
