2c7ff0425e20dd0186130579eeebf4d2cc6edc70
Jérôme Guelfucci <jeromeg@xfce.org>
2010-12-11 17:35:09 +0100

    Update the xfce4-panel project page.

--- pages/projects/xfce4-panel.php
+++ pages/projects/xfce4-panel.php
@@ -1,29 +1,41 @@
-<div style="float:right; margin:0 0 0.5em 0.5em;"><img src="/images/projects/xfce4-panel.png" alt="xfce4-panel" width="48" height="48" /></div>
+<h1>Xfce4 Panel</h1>
 
-<h2>Panel</h2>
+<h2>What is the Xfce4 panel?</h2>
+
+<p>
+  <?php E_("A panel is a bar which can hold many items such as application launchers, windows lists, clock, notification area, applications menu...") ?>
+</p>
+
+<p>
+  <?php E_("The Xfce panel can manage any number of panels and allows you to customize them by adding/removing items, choosing their position and size, setting background images or transparency levels...") ?>
+</p>
+
+<p>
+  <?php E_("The Xfce Panel comes with a great number of plugins but you can also install a lot of third party plugins. most of them are available through the Xfce Goodies project.") ?>
+</p>
 
 <p class="center"><img src="/images/projects/screenshots/xfce4-panel.png" alt="Panel" /></p>
 
-<p>
-The Xfce Panel supports multiple panels, with many options for their
-position, appearance, transparency and behavior. There are many items 
-available by default to customize your panels, like application launchers with 
-detachable menus, a graphical pager, a tasklist, a clock, a system tray, 
-a show/hide desktop switcher, and even more. It offers an easy way to add 
-items using a dialog, and to move items accross different panels. It's 
-compatible with the numerous panel plugins available from the 
-<a href="http://goodies.xfce.org/">Xfce Goodies</a> project.
-</p>
+<h2>Authors</h2>
 
-<h3>External Links</h3>
+<ul>
+  <li>Jasper Huijsmans [jasper at xfce.org]</li>
+  <li>Nick Schermer [nick at xfce.org]</li>
+</ul>
 
-<p>
-<a href="http://goodies.xfce.org/projects/panel-plugins/start" class="external">Panel plugins</a>
-from the Goodies Project page.</p>
+<h2>Useful links</h2>
 
-<?php include("git.address.php"); ?>
-
-<h3>Authors</h3>
-
-<?php include("authors.php"); ?>
-
+<ul>
+  <li>
+    <a href="http://goodies.xfce.org/projects/panel-plugins/start" class="external"><?php E_("Panel plugins from the Goodies Project page") ?></a>
+  </li>
+  <li>
+    <a href="http://archive.xfce.org/src/xfce/xfce4-panel/" class="external"><?php E_("Download the source tarballs of Xfce4 Panel releases") ?></a>
+  </li>
+  <li>
+    <a href="http://git.xfce.org/xfce/xfce4-panel/" class="external"><?php E_("Xfce4 Panel source code on git.xfce.org") ?></a>
+  </li>
+  <li>
+    <a href="http://bugzilla.xfce.org/buglist.cgi?resolution=---;classification=Xfce;query_format=advanced;product=Xfce4-Panel" class="external"><?php E_("Bugs reported against Xfce4 Panel") ?></a>
+  </li>
+</ul>
