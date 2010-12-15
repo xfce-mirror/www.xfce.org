11aff82317903c2254c0f1c44aabf4b88b3ad3b7
Jérôme Guelfucci <jeromeg@xfce.org>
2010-12-15 23:07:07 +0100

    Update Xfce4 Session project page.

--- pages/projects/xfce4-session.php
+++ pages/projects/xfce4-session.php
@@ -1,18 +1,27 @@
-<div style="float:right; margin:0 0 0.5em 0.5em;"><img src="/images/projects/xfce4-session.png" alt="xfprint" width="48" height="48" /></div>
-
-<h2>Session Manager</h2>
+<h1>Session Manager</h1>
 
 <p>
-The session manager allows the user to save sessions and restore them after
-login. It is capable of saving several different sessions. It also provides an easy way
-to log out, reboot, shutdown, hibernate or suspend your computer.
+  <?php E_("The session manager allows the user to save sessions and restore them after login. It is capable of saving several different sessions. It also provides an easy way to log out, reboot, shutdown, hibernate or suspend your computer.") ?>
 </p>
 
-<h3>External Links</h3>
+<h2>Authors</h2>
 
-<?php include("git.address.php"); ?>
+<ul>
+  <li>Benedikt Meurer [benny at xfce.org]</li>
+  <li>Brian Tarricone [kelnos at xfce.org]</li>
+</ul>
 
-<h3>Authors</h3>
+<h2>Useful links</h2>
 
-<?php include("authors.php"); ?>
+<ul>
+  <li>
+    <a href="http://archive.xfce.org/src/xfce/xfce4-session/" class="external"><?php E_("Download the source tarballs of Xfce4 Session releases") ?></a>
+  </li>
+  <li>
+    <a href="http://git.xfce.org/xfce/xfce4-session/" class="external"><?php E_("Xfce4 Session source code on git.xfce.org") ?></a>
+  </li>
+  <li>
+    <a href="http://bugzilla.xfce.org/buglist.cgi?resolution=---;classification=Xfce;query_format=advanced;product=Xfce4-Session" class="external"><?php E_("Bugs reported against Xfce4 Session") ?></a>
+  </li>
+</ul>
 
