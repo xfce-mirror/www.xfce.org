8f932f89502246f9f9f4f9a7ef015461e9fa81bb
Jérôme Guelfucci <jeromeg@xfce.org>
2010-12-18 13:32:50 +0100

    Update libxfce4 project page.

---
+++ pages/projects/libxfce4.php
@@ -0,0 +1,53 @@
+<h1><?php E_("What are libxfce4util and libxfce4ui?") ?></h1>
+
+<p>
+  <?php E_("The core libraries provide common functionalities to the Xfce environment. They are two libraries which provide non-graphical helper functions, widgets, convenience functions for graphical user interfaces  and functions for settings management.") ?>
+</p>
+
+<h2><?php E_("Features") ?></h2>
+
+<ul>
+  <li><strong>libxfce4util</strong><?php E_(": basic utilities.") ?></li>
+  <li><strong>libxfce4ui</strong><?php E_(": unified widgets and session management.") ?></li>
+</ul>
+
+<h2><?php E_("Authors") ?></h2>
+
+<ul>
+  <li>Benedikt Meurer [benny at xfce.org]</li>
+  <li>Jasper Huijsmans [jasper at xfce.org]</li>
+  <li>Olivier Fourdan [fourdan at xfce.org]</li>
+  <li>Brian J. Tarricone [kelnos at xfce.org]</li>
+  <li>Nick Schermer [nick at xfce.org]</li>
+</ul>
+
+<h2><?php E_("Useful links") ?></h2>
+
+<h3>Libxfce4util</h3>
+
+  <ul>
+    <li>
+      <a href="http://archive.xfce.org/src/xfce/libxfce4util/" class="external"><?php E_("Download the source tarballs of Libxfce4util releases") ?></a>
+    </li>
+    <li>
+      <a href="http://git.xfce.org/xfce/libxfce4util/" class="external"><?php E_("Libxfce4util source code on git.xfce.org") ?></a>
+    </li>
+    <li>
+      <a href="http://bugzilla.xfce.org/buglist.cgi?resolution=---;classification=Xfce;query_format=advanced;product=Libxfce4util" class="external"><?php E_("Bugs reported against Libxfce4util") ?></a>
+    </li>
+  </ul>
+
+<h3>Libxfce4ui</h3>
+
+  <ul>
+    <li>
+      <a href="http://archive.xfce.org/src/xfce/libxfce4ui/" class="external"><?php E_("Download the source tarballs of Libxfce4ui releases") ?></a>
+    </li>
+    <li>
+      <a href="http://git.xfce.org/xfce/libxfce4ui/" class="external"><?php E_("Libxfce4ui source code on git.xfce.org") ?></a>
+    </li>
+    <li>
+      <a href="http://bugzilla.xfce.org/buglist.cgi?resolution=---;classification=Xfce;query_format=advanced;product=Libxfce4ui" class="external"><?php E_("Bugs reported against Libxfce4ui") ?></a>
+    </li>
+  </ul>
+
