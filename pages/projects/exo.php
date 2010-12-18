943e0455ed192d78626f094800c4d8358959d81c
Jérôme Guelfucci <jeromeg@xfce.org>
2010-12-18 13:21:13 +0100

    Update exo project page.

--- pages/projects/exo.php
+++ pages/projects/exo.php
@@ -1,13 +1,28 @@
-<div style="float:right; margin:0 0 0.5em 0.5em;"><img src="/images/projects/library.png" alt="libraries" width="48" height="48" /></div>
+<h1><?php E_("What is Exo?") ?></h1>
 
-<h2>Exo</h2>
+<p>
+  <?php E_("Exo is an application library for the Xfce desktop environment.") ?>
+</p>
 
-<p>Exo is an application library for the Xfce desktop environment.</p>
+<h2><?php E_("Authors" ?></h2>
 
-<h3>External Links</h3>
+<ul>
+  <li>Benedikt Meurer [benny at xfce.org]</li>
+  <li>Nick Schermer [nick at xfce.org]</li>
+  <li>Jannis Pohlmann [jannis at xfce.org]</li>
+</ul>
 
-<?php include("git.address.php"); ?>
+<h2><?php E_("Useful links") ?></h2>
 
-<h3>Authors</h3>
+<ul>
+  <li>
+    <a href="http://archive.xfce.org/src/xfce/exo/" class="external"><?php E_("Download the source tarballs of Exo releases") ?></a>
+  </li>
+  <li>
+    <a href="http://git.xfce.org/xfce/exo/" class="external"><?php E_("Exo source code on git.xfce.org") ?></a>
+  </li>
+  <li>
+    <a href="http://bugzilla.xfce.org/buglist.cgi?resolution=---;classification=Xfce;query_format=advanced;product=Exo" class="external"><?php E_("Bugs reported against Exo") ?></a>
+  </li>
+</ul>
 
-<?php include("authors.php"); ?>
