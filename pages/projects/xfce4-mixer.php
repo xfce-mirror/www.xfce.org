1fc27a672bd78d5354107d4ed08629819dea3f0c
Jérôme Guelfucci <jeromeg@xfce.org>
2010-12-22 17:08:28 +0100

    Update mixer project page.

--- pages/projects/xfce4-mixer.php
+++ pages/projects/xfce4-mixer.php
@@ -1,23 +1,32 @@
-<div style="float:right; margin:0 0 0.5em 0.5em;"><img src="/images/projects/xfce4-mixer.png" alt="xfce4-mixer" width="48" height="48" /></div>
+<h1><?php E_("What is the mixer?") ?></h1>
 
-<h2>Mixer</h2>
+<p>
+  <?php E_("The Mixer is a volume control application for the Xfce Desktop Environment. It provides both a volume control plugin for the Xfce Panel and a standalone mixer application.") ?>
+</p>
+
+<p>
+  <?php E_('It supports all audio systems supported by the <a href="http://www.gstreamer.net/">GStreamer</a> project.') ?>
+</p>
 
 <p class="center"><img src="/images/projects/screenshots/xfce4-mixer.png" alt="Mixer" /></p>
 
-<p>
-The Mixer is a volume control application for the Xfce Desktop Environment. It provides
-both a volume control plugin for the Xfce Panel and a standalone mixer application.
-</p>
+<h2><?php E_("Authors") ?></h2>
 
-<p>
-It supports all audio systems supported by the <a href="http://www.gstreamer.net/">GStreamer</a> project.
-</p>
+<ul>
+  <li>Jannis Pohlmann [jannis at xfce.org]</li>
+</ul>
 
-<h3>External Links</h3>
+<h2><?php E_("Useful links") ?></h2>
 
-<?php include("git.address.php"); ?>
-
-<h3>Author</h3>
-
-<?php include("authors.php"); ?>
+<ul>
+  <li>
+    <a href="http://archive.xfce.org/src/apps/xfce4-mixer/" class="external"><?php E_("Download the source tarballs of Xfce4 Mixer releases") ?></a>
+  </li>
+  <li>
+    <a href="http://git.xfce.org/apps/xfce4-mixer/" class="external"><?php E_("Xfce4 Mixer source code on git.xfce.org") ?></a>
+  </li>
+  <li>
+    <a href="http://bugzilla.xfce.org/buglist.cgi?resolution=---;classification=Apps;query_format=advanced;product=Xfce4-Mixer" class="external"><?php E_("Bugs reported against Xfce4 Mixer") ?></a>
+  </li>
+</ul>
 
