08de8d061ceef6006ba312e09abf95c1d17c38c9
Nick Schermer <nick@xfce.org>
2011-01-10 19:19:40 +0100

    Improve mirror expand.

--- pages/download/index.php
+++ pages/download/index.php
@@ -21,13 +21,13 @@ $mirrors = array (
   //'archive.tx-us.xfce.org' => R_('United States/Texas'),
 );
 
-function print_mirrors($path)
+function print_mirrors($path, $group)
 {
   global $mirrors;
 
   echo '  <ul>'."\n";
 
-  if (isset($_GET['mirror']) && $_GET['mirror'] == $path)
+  if (isset($_GET['g']) && $_GET['g'] == $group)
   {
     foreach ($mirrors as $m => $l)
     {
@@ -38,7 +38,7 @@ function print_mirrors($path)
   else
   {
     echo '    <li><a href="http://archive.xfce.org/'.$path.'">archive.xfce.org</a> '.
-         '<a href=/download/?mirror='.$path.'>(', R_('mirror list') .'</a>)'."</li>\n";
+         '<a href="?g='.$group.'#'.$group.'">(', R_('mirror list') .'</a>)'."</li>\n";
   }
 
   echo '  </ul>'."\n";
@@ -58,14 +58,14 @@ function print_mirrors($path)
   <?php printf (R_('Xfce %s is the most recent version of the Xfce desktop environment. You can find the changes in this release <a href="/download/changelogs/%s">here</a>.'), $stable_version, $stable_version) ?>
 </p>
 
-<?php print_mirrors ('xfce/'.$stable_version) ?>
+<?php print_mirrors ('xfce/'.$stable_version, 'stable') ?>
 
 <h2 id="individual"><?php E_('Individual releases') ?></h2>
 <p>
   <?php E_('Each package in Xfce can make idividual stable or development releases, including the core packages (as discribed in the <a href="/about/releasemodel">release model</a>). You can follow the announcements of the releases on the <a href="/community/lists">Xfce users mailing list</a>, watch the <a href="http://releases.xfce.org/feeds/project/">release feeds</a> or follow the Xfce <a href="http://identi.ca/xfce">identi.ca channel</a>.') ?>
 </p>
 
-<?php print_mirrors ('src') ?>
+<?php print_mirrors ('src', 'individual') ?>
 
 <?php if ($preview_visible) { ?>
 <h2 id="unstable"><?php printf (R_('Preview release %s'), $preview_version) ?></h2>
@@ -73,6 +73,6 @@ function print_mirrors($path)
   <?php printf (R_('Xfce %s is the latest development release of the Xfce desktop environment. This release should not be used in a production environments. If you are uncertain about downloading this release, you should probably use the <a href="#stable">stable release</a> above. You can find the changes in this release <a href="/download/changelogs/%s">here</a>.'), $preview_version, $preview_version) ?>
 </p>
 
-<?php print_mirrors ('xfce/'.$preview_version) ?>
+<?php print_mirrors ('xfce/'.$preview_version, 'unstable') ?>
 
 <?php } ?>
