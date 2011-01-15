962fbab367b4cbbc2aa9a845b687cfc560885c7d
Nick Schermer <nick@xfce.org>
2011-01-15 21:45:57 +0100

    Add screenshot.

--- pages/about/screenshots.php
+++ pages/about/screenshots.php
@@ -7,7 +7,7 @@
 
 <h2><?php E_('Xfce 4.8') ?></h2>
 <p>
-  <a href="/images/about/screenshots/4.8-1.png"><img src="/images/about/screenshots/4.8-1-small.png" alt="" width="200" height="150" hspace="2" vspace="2" border="0" /></a>
+  <a href="/images/about/screenshots/4.8-1.png"><img src="/images/about/screenshots/4.8-1-small.png" alt="" width="200" height="150" hspace="2" vspace="2" border="0" /></a><a href="/images/about/screenshots/4.8-2.png"><img src="/images/about/screenshots/4.8-2-small.png" alt="" width="200" height="150" hspace="2" vspace="2" border="0" /></a>
 </p>
 
 <h2><?php E_('Xfce 4.6') ?></h2>
