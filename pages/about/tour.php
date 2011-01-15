87642fe4818df06595ba8d9f17e4f307264540e2
Nick Schermer <nick@xfce.org>
2011-01-15 19:07:45 +0100

    Fixup tour stuff.

--- pages/about/tour.php
+++ pages/about/tour.php
@@ -2,25 +2,33 @@
 
 <h1><?php echo $head['title'] ?></h1>
 
-<p><?php E_('This tour will introduce you to all the new major visual features in Xfce 4.8. This is only the visual part of what has been done; for a full list of changes, see the <a href="/download/changelogs/">changelogs</a>. The tour for Xfce 4.6 can be found <a href="/about/tour46">here</a>.') ?></p>
+<p><?php E_("This tour will introduce you to all the new major visual features in Xfce 4.8. This is only the visual part of what has been done; for a full list of changes, see the <a href=\"/download/changelogs/\">changelogs</a>. The tours of <a href=\"/about/tour46\">Xfce 4.6</a> and <a href=\"/about/tour44\">Xfce 4.4</a> are also still available.") ?></p>
 
 <h2><?php E_("Thunar file browser") ?></h2>
 
   <h3><?php E_("Remote shares browsing") ?></h3>
 
-    <p><?php E_("Thunar now has optionnal support for GVFS which will allow you to browse remote shares using Thunar: FTP, Windows Shares, WebDav and SSH servers...") ?></p>
+    <p><?php E_("Thunar now has optional support for GVFS which will allow you to browse remote shares using Thunar: FTP, Windows Shares, WebDav and SSH servers...") ?></p>
 
   <h3><?php E_("New file operations progress dialog") ?></h3>
 
     <p><?php E_("When several file operations (copy or move) are being handled in parallel, Thunar now shows a single progress dialog which displays all the relevant information.") ?></p>
+    
+    <p class="image"><img src="/images/about/tour/4.8/thunar-progess.png" alt="<?php E_("Progress dialog") ?>" /></p>
 
   <h3><?php E_("Eject button for removable devices") ?></h3>
 
     <p><?php E_("It is is now possible to eject removable devices by clicking the eject button near each removable device label in the side pane.") ?></p>
+    
+    <p class="image"><img src="/images/about/tour/4.8/thunar-eject.png" alt="<?php E_("Eject button") ?>" /></p>
 
 <h2><?php E_("Xfce panel") ?></h2>
 
   <p><?php E_("The Xfce panel has been totally rewritten in 4.8, here are the most visible changes.") ?></p>
+  
+  <h3><?php E_("Improved positioning and size handling") ?></h3>
+  
+    <p><?php E_("The panel has much better support for positing panels on the screen. It can be dragged and snapped to screen borders; floating positions are only remembered when moving a panel and the length of the panel can be adjusted as a percentage of the monitor size.") ?></p>
 
   <h3><?php E_("Transparent backgrounds") ?></h3>
 
@@ -29,14 +37,16 @@
   <h3><?php E_("New item editor") ?></h3>
 
     <p><?php E_("The panel now features an item editor which allows to modify, move, add and remove items qucikly. This is particularly useful for items which are hard to right click and gives you an easy way to change your panel layout from a single place.") ?></p>
+    
+    <p class="image"><img src="/images/about/tour/4.8/panel-editor.png" alt="<?php E_("Panel item editor") ?>" /></p>
 
   <h3><?php E_("Drag and drop to create launchers") ?></h3>
 
-    <p><?php E_("You can now drag and drop menu entries from the panel applications menu to the panel to create panel and desktop launchers. This feature should please the huge number of people who had been requesting it for ages.") ?></p>
+    <p><?php E_("You can now drag and drop menu entries from the panel applications menu, application finder or Thunar to the panel to create launchers. This feature should please the huge number of people who had been requesting it for ages. The launcher also has an integrated application finder to add new items.") ?></p>
 
-  <!-- iconbox and tasklist merged in new 'window buttons' plugin -->
-
-  <!-- much improved positioning: show panel only on a single output, snap to screen edges and don't move floating panels -->
+  <h3><?php E_("Window buttons plugin") ?></h3>
+    
+    <p><?php E_("The iconbox and tasklist plugins are new merged into 1 plugin, called window buttons. It implements different sorting modes, improved handling in vertical panels and can draw a similar frame as Xfwm4 Alt+tab when hovering a button.") ?></p>
 
   <h3><?php E_("New fuzzy clock") ?></h3>
 
@@ -45,6 +55,8 @@
   <h3><?php E_("New directory menu plugin") ?></h3>
 
     <p><?php E_("The directory menu plugin provides a menu reproducing the arborescence of a particular folder. This allows you to open a terminal or a file manager in a sub-folder in a very fast and efficient way.") ?></p>
+    
+    <p class="image"><img src="/images/about/tour/4.8/panel-directorymenu.png" alt="<?php E_("Directory menu plugin") ?>" /></p>
 
   <h3><?php E_("Applications menu edition") ?></h3>
 
@@ -56,7 +68,7 @@
 
     <p><?php E_("Xfce can now handle and setup multihead setups for any randr 1.2 compatible driver. Unfortunately, this means it will not work with Nvidia proprietary drivers.") ?></p>
 
-    <p><?php E_("You can enable and disable plugged screens, set their resolution, reflesh rate, rotation and reflexion, clone screens or extend desktops.") ?></p>
+    <p><?php E_("You can enable and disable plugged screens, set their resolution, refresh rate, rotation and reflection, clone screens or extend desktops.") ?></p>
 
     <h4><?php E_("Timed confirmation") ?></h4>
 
@@ -72,4 +84,4 @@
 
   <h3><?php E_("Improved keyboard layout selection") ?></h3>
 
-    <p><?php E_("The layout selection dialog received a lot of love: it no longer displays cryptic language/variants codes but uses localised languages/variants names. Layout edition was also made easier by prefilling the dialogs with the current values.") ?></p>
+    <p><?php E_("The layout selection dialog received a lot of love: it no longer displays cryptic language/variants codes but uses localized languages/variants names. Layout edition was also made easier by prefilling the dialogs with the current values.") ?></p>
