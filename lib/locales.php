06ceb4a58c072ed0363d8fa7f142ffda6da877e3
Nick Schermer <nick@xfce.org>
2011-01-22 15:50:12 +0100

    Enable ja and it.

--- lib/locales.php
+++ lib/locales.php
@@ -2,7 +2,9 @@
 
 /* known languages in this directory */
 $languages = array (
-	'en' => 'English');
+	'en' => 'English',
+	'it' => 'Italian',
+	'ja' => 'Japanese');
 
 /* used in header and language detection functions */
 $language_codes = array_keys ($languages);
