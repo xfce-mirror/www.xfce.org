7fa0c9bf63e7f886ba43dbc57df260fe0c117f77
Nick Schermer <nick@xfce.org>
2011-09-26 18:32:14 +0200

    Enable es and sk.

--- lib/locales.php
+++ lib/locales.php
@@ -3,8 +3,10 @@
 /* known languages in this directory */
 $languages = array (
 	'en' => 'English',
+	'es' => 'Spanish (Castilian)',
 	'it' => 'Italian',
-	'ja' => 'Japanese');
+	'ja' => 'Japanese',
+	'sk' => 'Slovak');
 
 /* used in header and language detection functions */
 $language_codes = array_keys ($languages);
