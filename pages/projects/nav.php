d68a9dbc47c077b785a549252c64dba7de10bfc0
Nick Schermer <nick@xfce.org>
2012-02-29 16:31:51 +0100

    Link project pages to docs.xfce.org.
    The docs contains a better overview (or should) then
    the current pages on the website.
    
    Drop unneeded images.

--- pages/projects/nav.php
+++ pages/projects/nav.php
@@ -4,6 +4,7 @@ $toc['menu'] = array (
 	'projects' => R_('Projects'));
 
 $toc['external'] = array (
-	'http://goodies.xfce.org' => R_('Xfce Goodies'));
+	'http://goodies.xfce.org' => R_('Xfce Goodies'),
+	'http://docs.xfce.org' => R_('Xfce Docs Wiki'));
 
 ?>
