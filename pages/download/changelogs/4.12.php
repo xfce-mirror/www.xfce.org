26f9b4cf0f0a969a5fa9115346feab0b07cb5c1d
Landry Breuil <landry@rhaalovely.net>
2015-02-24 20:35:09 +0100

    add skeleton for 4.12 changelog

---
+++ pages/download/changelogs/4.12.php
@@ -0,0 +1,80 @@
+
+
+<h1>Xfce 4.12 Changelog</h1>
+
+<h2>Dependency changes</h2>
+<ul>
+  <li>GTK+ dependency &gt;= 2.24 and GLib &gt;= 2.30.</li>
+  <li>Garcon has a new libxfce4ui dependency.</li>
+  <li>Libxfce4ui has an optional dependency on GTK+3.</li>
+  <li>Xfce4-panel can be optionally built against GTK+3.</li>
+  <li>Xfce4-appfinder now uses GDbus instead of dbus-glib, and can be potentially built against GTK+3.</li>
+  <li>Xfce4-session has an optional dependency on upower.</li>
+  <li>Xfce4-power-manager has a new dependency on upower, and can build a battery plugin for Xfce4-panel and LXPanel</li>
+  <li>Xfce4-settings has an optional dependency on libinput Xorg driver and upower.</li>
+  <li>Xfwm4 has an optional dependency on libdrm.</li>
+</ul>
+
+<h2>Development Tools <em>(xfce4-dev-tools)</em></h2>
+<ul>
+</ul>
+
+<h2>Utility Library <em>(libxfce4util)</em></h2>
+<ul>
+</ul>
+
+<h2>Widget Library <em>(libxfce4ui)</em></h2>
+<ul>
+</ul>
+
+<h2>Extension Library <em>(exo)</em></h2>
+<ul>
+</ul>
+
+<h2>Menu Library <em>(garcon)</em></h2>
+<ul>
+</ul>
+
+<h2>File Manager <em>(thunar)</em></h2>
+<ul>
+</ul>
+
+<h2>Application Finder <em>(xfce4-appfinder)</em></h2>
+<ul>
+</ul>
+
+<h2>Panel <em>(xfce4-panel)</em></h2>
+<ul>
+</ul>
+
+<h2>Session Manager <em>(xfce4-session)</em></h2>
+<ul>
+</ul>
+
+<h2>Settings Manager <em>(xfce4-settings)</em></h2>
+<ul>
+</ul>
+
+<h2>Configuration Manager <em>(xfconf)</em></h2>
+<ul>
+</ul>
+
+<h2>Desktop Manager <em>(xfdesktop)</em></h2>
+<ul>
+</ul>
+
+<h2>Window Manager <em>(xfwm4)</em></h2>
+<ul>
+</ul>
+
+<h2>Theme Engine <em>(gtk-xfce-engine)</em></h2>
+<ul>
+</ul>
+
+<h2>Power Manager <em>(xfce4-power-manager)</em></h2>
+<ul>
+</ul>
+
+<h2>Volume Manager <em>(thunar-volman)</em></h2>
+<ul>
+</ul>
