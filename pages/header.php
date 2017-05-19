fbafb9ff6bcd47450e2145784d5842e21fb9a797
Pasi Lallinaho <pasi@shimmerproject.org>
2017-05-19 12:07:42 +0300

    Update the viewport meta for mobile devices

--- pages/header.php
+++ pages/header.php
@@ -11,7 +11,7 @@ else
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1" />
 	<title><?php echo $title ?></title>
 	<link rel="icon" href="/style/favicon.png" />
 	<link rel="stylesheet" media="screen" href="/style/css.php" type="text/css" />
