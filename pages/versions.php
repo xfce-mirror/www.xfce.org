e6221926392ad99b28a61f6ad1759b32bacdff53
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2019-08-08 21:38:29 +0200

    Update Xfce stable and dev versions

--- pages/versions.php
+++ pages/versions.php
@@ -5,11 +5,11 @@
    **/
 
   /* latest stable release version */
-  $stable_version = '4.12';
-  $stable_date = '2015-02-28';
+  $stable_version = '4.14';
+  $stable_date = '2019-08-12';
 
   /* latest development version */
-  $preview_version = '4.10pre2';
-  $preview_date = '2012-04-14';
+  $preview_version = '4.13pre3';
+  $preview_date = '2019-07-28';
   $preview_visible = false /* set to true if preview is *newer* then stable */
 ?>
