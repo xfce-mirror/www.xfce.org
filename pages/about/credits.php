491164ccbda70b693ce41565ecfe7ab50639f065
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2019-08-11 21:48:49 +0200

    Update credits / developers

--- pages/about/credits.php
+++ pages/about/credits.php
@@ -52,25 +52,21 @@ function print_translator_credits()
 <h2 id="core"><?php E_('Core developers')?></h2>
 <?php
         print_credits (array (
-                'Andrzej Radecki' => 'ndrwrdck'.ATSIGN.'gmail.com',
-                'Eric Koegel' => 'eric'.ATSIGN.'xfce.org',
-                'Harald Judt' => 'hjudt'.ATSIGN.'xfce.org',
-                'Landry Breuil' => 'landry'.ATSIGN.'xfce.org',
-                'Nick Schermer' => 'nick'.ATSIGN.'xfce.org',
+                'André Miranda' => 'andreldm'.ATSIGN.'xfce.org',
+                'Alexander Schwinn' => 'alexxcons'.ATSIGN.'xfce.org',
                 'Olivier Fourdan' => 'fourdan'.ATSIGN.'xfce.org',
+                'Romain Bouvier' => 'skunnyk'.ATSIGN.'xfce.org',
                 'Sean Davis' => 'bluesabre'.ATSIGN.'xfce.org',
-                'Simon Steinbeiß' => 'simon'.ATSIGN.'xfce.org',
-                'Peter de Ridder' => 'peter'.ATSIGN.'xfce.org'));
+                'Simon Steinbeiß' => 'simon'.ATSIGN.'xfce.org'));
 ?>
 
 <h2 id="active"><?php E_('Active contributors')?></h2>
 <?php
         print_credits (array (
                 'Guido Berhoerster' => 'guido+xfce'.ATSIGN.'berhoerster.name',
-                'Jannis Pohlmann' => 'jannis'.ATSIGN.'xfce.org',
+                'Landry Breuil' => 'landry'.ATSIGN.'xfce.org',
                 'Mike Massonnet' => 'mmassonnet'.ATSIGN.'gmail.com',
                 'Silvio Knizek' => 'killermoehre'.ATSIGN.'gmx.net',
-                'Steve Dodier-Lazaro' => 'sidi'.ATSIGN.'xfce.org',
                 'Yves-Alexis Perez' => 'corsac'.ATSIGN.'debian.org'));
 ?>
 
@@ -105,6 +101,7 @@ function print_translator_credits()
 <?php
         print_credits (array (
                 'Ali Abdallah' => 'aliov'.ATSIGN.'xfce.org',
+                'Andrzej Radecki' => 'ndrwrdck'.ATSIGN.'gmail.com',
                 'Auke Kok' => 'sofar'.ATSIGN.'foo-projects.org',
                 'Benedikt Meurer' => 'benny'.ATSIGN.'xfce.org',
                 'Bernhard Walle' => 'bernhard.walle'.ATSIGN.'gmx.de',
@@ -119,26 +116,29 @@ function print_translator_credits()
                 'Eduard Roccatello' => 'eduard'.ATSIGN.'xfce.org',
                 'Ejvend Nielsen' => 'prophet'.ATSIGN.'sphere-x.net',
                 'Enrico Tröger' => 'enrico.troeger'.ATSIGN.'uvena.de',
+                'Eric Koegel' => 'eric'.ATSIGN.'xfce.org',
                 'Erik Touve' => 'etouve'.ATSIGN.'earthlink.net',
                 'Erik Harrison' => 'erikharrison'.ATSIGN.'xfce.org',
                 'François Le Clainche' => 'fleclainche'.ATSIGN.'wanadoo.fr',
+                'Harald Judt' => 'hjudt'.ATSIGN.'xfce.org',
+                'Jannis Pohlmann' => 'jannis'.ATSIGN.'http://xfce.alteroot.org/xfce.org',
                 'Jasper Huijsmans' => 'jasper'.ATSIGN.'xfce.org',
                 'Jérôme Guelfucci' => 'jeromeg'.ATSIGN.'xfce.org',
                 'Jean-François Wauthy' => 'pollux'.ATSIGN.'fce.org',
                 'Jens Guballa' => 'j.guballa'.ATSIGN.'t-online.de',
                 'Jens Luedicke' => 'jens'.ATSIGN.'irs-net.com',
                 'Joakim Andreasson' => 'joakim.andreasson'.ATSIGN.'gmx.net',
-                'Lionel Le Folgoc' => 'mrpouit'.ATSIGN.'gmail.com',
                 'Karsten Luetkewitz' => 'phrep'.ATSIGN.'plskthx.org',
+                'Lionel Le Folgoc' => 'mrpouit'.ATSIGN.'gmail.com',
                 'Martin Loschwitz' => 'madkiss'.ATSIGN.'debian.org',
                 'Maximilian Schleiss' => 'maximilian'.ATSIGN.'xfce.org',
                 'Michael Mosier' => 'michael'.ATSIGN.'spyonit.com',
                 'Mickaël Graf' => 'korbinus'.ATSIGN.'xfce.org',
+                'Nick Schermer' => 'nick'.ATSIGN.'xfce.org',
                 'Pau Rul·lan Ferragut' => 'paurullan'.ATSIGN.'bulma.net',
                 'Stephan Arts' => 'stephan'.ATSIGN.'xfce.org',
+                'Steve Dodier-Lazaro' => 'sidi'.ATSIGN.'xfce.org',
                 'Thomas Leonard' => 'tal00r'.ATSIGN.'ecs.soton.ac.uk',
                 'Tobias Henle' => 'tobias'.ATSIGN.'page23.de',
                 'Xavier Maillard' => 'zedek'.ATSIGN.'fxgsproject.org'));
 ?>
-
-
