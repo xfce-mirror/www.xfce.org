92e789300cb6a81835d26f1b18dfd78187357765
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2019-08-11 20:48:12 +0200

    4.14. tour coming soon

--- pages/about/tour.php
+++ pages/about/tour.php
@@ -2,4 +2,5 @@
 
 <h1><?php echo $head['title'] ?></h1>
 
-  <p><?php E_("This tour will introduce you to new major features of Xfce 4.14. It only covers improvements made on the surface; for the full list of changes, see the <a href=\"/download/changelogs/\">changelogs</a>.") ?></p>
+  <p><?php E_("Coming soon!") ?></p>
+  <p><?php E_("For the full list of changes, see the <a href=\"/download/changelogs/\">changelogs</a>.") ?></p>
