0d23c28d1b9936f5c07a06d81cb8b1ce9c11f7bd
kevinbowen777 <kevin.bowen@gmail.com>
2020-09-30 14:24:57 -0700

    Update Bugs link to point to open bugs page

--- pages/header.php
+++ pages/header.php
@@ -41,7 +41,7 @@ else
 			<li><a href="http://archive.xfce.org" title="Download location of tarballs">Archive</a></li>
 			<li><a href="https://wiki.xfce.org" title="Community documentation">Wiki</a></li>
 			<li><a href="https://forum.xfce.org" title="Community forums">Forum</a></li>
-			<li><a href="https://bugzilla.xfce.org" title="Report and track bugs">Bugs</a></li>
+			<li><a href="https://docs.xfce.org/contribute/bugs/bug-summary" title="Report and track bugs">Bugs</a></li>
 			<li><a href="https://blog.xfce.org" title="Visit the blog">Blog</a></li>
 			<li><a href="https://xfce.org/getinvolved/translation" title="Help translating the Xfce project">Translate</a></li>
 			<li><a href="https://git.xfce.org" title="Project repositories">GIT</a></li>
