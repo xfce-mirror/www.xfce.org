c5bcb37ff0902007c20d6679c2778cc5c5c70e31
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2020-12-20 00:01:24 +0100

    tour: Add default apps dialog

--- pages/about/tour416.php
+++ pages/about/tour416.php
@@ -8,6 +8,12 @@
 
 <p><?php E_("The Settings Manager itself received a visual refresh of its filter box, which can now be hidden permanently. At the same time the search capabilities of the filter box were improved.") ?>
 
+  <h3><?php E_("Default Applications") ?></h3>
+
+<p><?php E_("This new dialog represents a merger between the previously available 'Mime Settings' and the 'Preferred Applications' dialogs. Consolidating both in one place means users have an easier time setting default applications to handle certain file types.") ?>
+
+  <?php print_figure ("about/tour/4.16/default-applications.png", R_("The new Default Applications dialog")) ?>
+
   <h3><?php E_("Display Dialog") ?></h3>
 
 <p><?php E_("To better support high-density displays - which come in various sizes and densities - we added fractional scaling based on the RandR extension of X11. Furthermore the mechanism for determining a fallback in case of misconfiguration was improved to always fall back to a valid mode.") ?>
