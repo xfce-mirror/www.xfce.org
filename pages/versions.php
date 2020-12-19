4d2c5a9bd716b82caf2fb44f4f1e8b85f90d00df
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2020-12-19 22:54:56 +0100

    Update release dates

--- pages/versions.php
+++ pages/versions.php
@@ -6,10 +6,10 @@
 
   /* latest stable release version */
   $stable_version = '4.16';
-  $stable_date = '2020-12-18';
+  $stable_date = '2020-12-20';
 
   /* latest development version */
-  $preview_version = '4.13pre3';
-  $preview_date = '2019-07-28';
+  $preview_version = '4.16pre2';
+  $preview_date = '2020-11-19';
   $preview_visible = false /* set to true if preview is *newer* then stable */
 ?>
