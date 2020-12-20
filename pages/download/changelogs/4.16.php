823879a444ad0c45c4973a380b75ca4af0545fb9
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2020-12-20 23:39:35 +0100

    changelog: Add exo

--- pages/download/changelogs/4.16.php
+++ pages/download/changelogs/4.16.php
@@ -35,7 +35,59 @@
 
 <h2>Extension Library <em>(exo)</em></h2>
 <ul>
-  <li>TODO</li>
+  <li>Adjust padding only for vertical item orientation (Issue #48)</li>
+  <li>bump glib (and gio, gthread, etc) to 2.50.0</li>
+  <li>Fix GCC warning (false negative)</li>
+  <li>Re-grab tree selection to prevent rare crash (Issue #46)</li>
+  <li>Remove unneeded RTL adjustment (Issue #45)</li>
+  <li>exo-open: Wrap long URI in error dialog (Issue #26)</li>
+  <li>Avoid truncated text in small zoom levels (#42)</li>
+  <li>Make ExoIconView reduce selection on a single click (issue #39)</li>
+  <li>Partially restore 6fcefce9 (Issue #18)</li>
+  <li>Add check for valid GtkTreeSelection (issue #40)</li>
+  <li>Reset cursor on drag end in single-click mode</li>
+  <li>exo-open: Change priority of command line parameters (Fixes #20)</li>
+  <li>Prevent label from overlapping icon in RTL (Fixes #36)</li>
+  <li>Prevent "selection_changed" bursts (#37)</li>
+  <li>Small change to documentation for exo_strdup_strftime</li>
+  <li>desktop-item-edit: Use new XfceTitledDialog API</li>
+  <li>desktop-item-edit: Fix fallback for desktop file type detection</li>
+  <li>desktop-item-edit: Fix sensitivity of save button (Fixes #28)</li>
+  <li>desktop-item-edit: Improve detection of invalid icon names (Fixes #33)</li>
+  <li>desktop-item-edit: Fix sensitivity of save button (Bug #16813)</li>
+  <li>AC_CONFIG_MACRO_DIR → AC_CONFIG_MACRO_DIRS (Closes !9)</li>
+  <li>Remove unused perl module requirement</li>
+  <li>Replace bugzilla occurences with gitlab (fixes #30)</li>
+  <li>icon-chooser-dialog: Allow removing selected icon (fixes #2)</li>
+  <li>Fix build on OpenBSD (!2)</li>
+  <li>Drop exo-csource from exo</li>
+  <li>Rework exo_icon_view_scroll_event to allow overshoot effect (fixes #25)</li>
+  <li>Allow resizing the xfce4-settings window smaller (fixes #19)</li>
+  <li>exo-29: Fix race condition in make install</li>
+  <li>state variable is a GtkStateFlags</li>
+  <li>Fix make warnings since gtk2 drop</li>
+  <li>Drop GTK2 support. This means the following features are no longer part of the API: ExoCellRendererEllipsizedText, ExoIconBar, ExoToolbarsEditor, ExoToolbarsEditorDialog, ExoToolbarsModel, ExoToolbarsView, ExoWrapTable, ExoXsessionClient</li>
+  <li>Fallback without xfce4-mime-helper</li>
+  <li>Drop exo_atomic_{inc,dec}</li>
+  <li>Attach popup window to toplevel parent (Bug #16768)</li>
+  <li>Rename Thunar.desktop to thunar.desktop (Bug #16197)</li>
+  <li>Extend selection on shift+drag (Bug #7526)</li>
+  <li>Do not cancel selection on shift+drag</li>
+  <li>Rename Thunar.desktop to thunar.desktop (Bug #16197)</li>
+  <li>Fix type declaration (Bug #16678)</li>
+  <li>Fix GTimeVal deprecation</li>
+  <li>Replace deprecated G_INLINE_FUNC macro</li>
+  <li>Replace deprecated macro</li>
+  <li>Wrong keyboard navigation after using rubberband in exo-icon-view (Bug #16286)</li>
+  <li>Cursor position in icon view not visible/highlighted when using Ctrl + arrows (Bug #12227)</li>
+  <li>Make sure default applications are properly set in mimeapps.list</li>
+  <li>exo-helper: Add gio-unix-2.0 dependency</li>
+  <li>Fix for the bugs 16075 and 16107</li>
+  <li>Fix cursor missing in search popup widget (bug #16068)</li>
+  <li>Fix prelight deactivation (bug #11806)</li>
+  <li>Fix desktop item creation on symbolic link directories, make user-created launchers executable by default</li>
+  <li>Fix issues with displaying icon view contents (bug #14737)</li>
+  <li>Remove dependency on gio-unix-2.0 (bug #15825)</li>
 </ul>
 
 <h2>Menu Library <em>(garcon)</em></h2>
