29f665d9981fe515c813ccfa0638dba502e1ed8e
Romain Bouvier <skunnyk@alteroot.org>
2021-01-10 22:46:55 +0100

    translations: update the .mo files when .po are updated
    - So gettext/php can read the updated strings

--- lib/tools/compile.sh
+++ lib/tools/compile.sh
@@ -1,48 +1,15 @@
 #!/bin/bash
+# This script generate the $lang.mo file which can be read by PHP gettext
+# The .po and .pot are updated by txcron.sh, with transifex
 
 root=../..
 pofiles=$root/lib/po
 localedir=$root/lib/locale
 
-if test ! -f $pofiles/www.pot; then
-  echo "No pot file found!"
-  exit 1
-fi
-
-tmpfile=$(mktemp -t XXXX.po)
-
-for f in $(find $pofiles -name "*.po")
+for po in $(find $pofiles -name "*.po")
 do
-  file=$(basename $f)
-  lang=${file%.po}
-
-  # Merge po file with latest pot file
-  res=0
-  # TODO: add "--lang $lang" again when earlgrey runs squeeze
-  msgmerge -N -q -o $tmpfile $f $pofiles/www.pot &> /dev/null && res=1
-  if test $res -eq 0; then
-    echo "msgmerge failed for $file"
-    continue
-  fi
-
-  # Check the file
-  res=0
-  msgfmt -c -o /dev/null $tmpfile &> $tmpfile.log && res=1
-  if test $res -eq 0; then
-    echo "$file is not valid"
-    cat $tmpfile.log
-    echo
-    echo
-    rm -f $tmpfile.log
-    continue
-  fi
-
-  rm $tmpfile.log
-
   # Compile catalog
   path=$localedir/$lang/LC_MESSAGES
   mkdir -p $path
-  msgfmt --no-hash -o $path/www.mo $tmpfile
+  msgfmt --no-hash -o $path/www.mo $po
 done
-
-rm -f $tmpfile
