72a1f83ae54a46744fd05a2a0e78376ec8fa0c7f
Romain Bouvier <skunnyk@alteroot.org>
2022-11-03 19:54:23 +0100

    Bootstrap xfce 4.18

--- pages/versions.php
+++ pages/versions.php
@@ -5,11 +5,11 @@
    **/
 
   /* latest stable release version */
-  $stable_version = '4.16';
-  $stable_date = '2020-12-20';
+  $stable_version = '4.18';
+  $stable_date = '2022-12-xx';
 
   /* latest development version */
-  $preview_version = '4.16pre2';
+  $preview_version = '4.18pre1';
   $preview_date = '2020-11-19';
   $preview_visible = false /* set to true if preview is *newer* then stable */
 ?>
