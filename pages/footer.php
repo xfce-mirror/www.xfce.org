6216e85386ffbd1bed564c0f128db6452cac43b8
Akbarkhon Variskhanov <akbarkhon.variskhanov@gmail.com>
2022-12-17 15:33:27 +0500

    Prefer header and footer elements over div

--- pages/footer.php
+++ pages/footer.php
@@ -9,14 +9,11 @@
 	}
 ?>
 	</div> <!-- content-wrap -->
-	<div id="footer">
+	</div> <!-- page-wrap -->
+	<footer>
 		<hr class="hidden" />
-		<p>
 			<div><?php printf (R_('Copyright %s <a href="/about/credits">Xfce Development Team</a>. This page was last modified on %s.'), '2003-'.date ('Y'), $footer['mtime']) ?></div>
 			<div><?php printf (R_('Hosted by <a href="https://gandi.net" target="_blank">Gandi</a>. Additional servers provided by <a href="https://fosshost.org" target="_blank">fosshost.org</a>.')) ?></div>
-		</p>
-	</div>
-</div> <!-- page-wrap -->
-
+	</footer>
 </body>
 </html>
